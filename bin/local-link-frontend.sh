3559e2ce05a3e4835ffcea93fabe57fb0a732207
Shane Smith <voodoogq@gmail.com>
2020-10-06 10:58:00 -0500

    Working through changeover

--- bin/local-link-frontend.sh
+++ bin/local-link-frontend.sh
@@ -2,6 +2,9 @@
 
 sage_repo_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; cd .. ; pwd -P )
 sage_docs_path=$sage_repo_path/docs
+sage_assets_path=$sage_repo_path/packages/sage-assets
+sage_react_path=$sage_repo_path/packages/sage-react
+sage_system_path=$sage_repo_path/packages/sage-system
 
 . $sage_repo_path/bin/local-link-utils.sh
 
@@ -15,6 +18,28 @@ function show_status_of_package() {
   ( ls -l node_modules ; ls -l node_modules/@* ) | grep ^l # <-- This looks up the symlinked node_modules
 }
 
+function link() {
+  (cd $sage_assets_path; yarn link)
+  (cd $sage_react_path; yarn link)
+  (cd $sage_system_path; yarn link)
+  cd $sage_docs_path
+  yarn link @kajabi/sage-assets
+  yarn link @kajabi/sage-react
+  yarn link @kajabi/sage-system
+  cd $sage_repo_path
+}
+
+function unlink() {
+  cd $sage_docs_path
+  yarn unlink @kajabi/sage-assets
+  yarn unlink @kajabi/sage-react
+  yarn unlink @kajabi/sage-system
+  (cd $sage_assets_path; yarn unlink)
+  (cd $sage_react_path; yarn unlink)
+  (cd $sage_system_path; yarn unlink)
+  cd $sage_repo_path
+}
+
 if [ "$1" = "true" ] || [ "$1" = "false" ]; then
 
   cd $sage_repo_path
@@ -22,19 +47,15 @@ if [ "$1" = "true" ] || [ "$1" = "false" ]; then
   # UNINSTALL Local Bindings
   if [ "$1" = "false" ]; then
     echo_custom "[FRONTEND]:" "Removing local Sage package"
-    yarn unlink @kajabi/sage
-    (cd $sage_repo_path; yarn unlink)
-
+    unlink
     yarn_install
     echo_custom "[FRONTEND]: Now Using..." "PRODUCTION SAGE ✅"
 
   # INSTALL Local Bindings
   elif [ "$1" = "true" ]; then
-    echo_custom "[FRONTEND]:" "Use the local Sage frontend package located at: $sage_repo_path"
-    (cd $sage_repo_path; yarn link)
-    yarn link @kajabi/sage
-
-    bundle_and_yarn
+    echo_custom "[FRONTEND]:" "Link the local Sage frontend packages"
+    link
+    yarn_install
     echo_custom "[FRONTEND]: Now Using..." "LOCAL SAGE ✅"
   fi;
 
--- docs/lib/sage-frontend/packs/docs.js
+++ docs/lib/sage-frontend/packs/docs.js
@@ -1,9 +1,9 @@
 // --- Webpacker Dependencies ---
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-import '@kajabi/sage'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "@kajabi/sage";
 
 // --- Sage Docs Assets ----------
-require.context('../../../../assets/images/docs', true)
-require.context('../../../../assets/images/system', true)
-import '../../../../assets/stylesheets/docs/index.scss'
+require.context("../../../../assets/images/docs", true);
+require.context("../../../../assets/images/system", true);
+import "../../../../assets/stylesheets/docs/index.scss";
