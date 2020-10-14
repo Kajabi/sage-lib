ba8d4d974e396b2a46bfc3d175d4efaa46d5c12f
Shane Smith <voodoogq@gmail.com>
2020-10-14 16:25:01 -0500

    feat(monorepo): updated gem-tag script to output results of git commands

--- bin/gem-tag.sh
+++ bin/gem-tag.sh
@@ -9,14 +9,16 @@ function conventional_commit_json() {
   git log --no-merges --oneline --no-decorate $(git rev-parse --abbrev-ref HEAD)...master docs | sed 's/[^ ]* //' | sed 's/$/\n===/' | head -n -1 | npx conventional-commits-parser "==="
 }
 
+echo $(conventional_commit_json)
+
 function uniq_types_from_commits() {
   conventional_commit_json | jq -r '.[] | .type' | uniq
 }
+
+echo $(uniq_types_from_commits)
 
 if [ $(uniq_types_from_commits | grep 'feat') ]; then
   (cd $sage_docs_path && bundle exec bump 'minor' --no-commit)
 elif [ $(uniq_types_from_commits | grep 'fix') ]; then
   (cd $sage_docs_path && bundle exec bump 'patch' --no-commit)
 fi
-
-
