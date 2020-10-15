3d1b4949bd9194027dd281582cc17c7674e9cdfa
Shane Smith <voodoogq@gmail.com>
2020-10-15 15:09:24 -0500

    fix(monorepo): bump gem

--- bin/bump-gem.sh
+++ bin/bump-gem.sh
@@ -8,8 +8,8 @@ sage_docs_path=$sage_repo_path/docs
 echo_custom "[GEM]" "Versioning..."
 
 function commit_bump() {
-  git add $sage_docs_path/lib/sage_rails/lib/sage_rails/version.rb
   (cd $sage_docs_path && bundle install)
+  git add $sage_docs_path/lib/sage_rails/lib/sage_rails/version.rb
   git add $sage_docs_path/Gemfile.lock
   git commit -m "chore(gem): bumping rails gem"
 }
