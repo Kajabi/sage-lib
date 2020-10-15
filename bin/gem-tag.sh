8894706f28bba495f859a8f9743a76bad25360e6
Shane Smith <voodoogq@gmail.com>
2020-10-15 08:41:04 -0500

    fix(monorepo): testing out a full commit message
    Here's some more information for the body

--- bin/gem-tag.sh
+++ bin/gem-tag.sh
@@ -7,15 +7,18 @@ sage_docs_path=$sage_repo_path/docs
 
 echo_custom "[GEM]" "Versioning..."
 
-read -rp "Would you like to bump the Sage Rails gem? (y/n): " BUMP_RAILS_GEM </dev/tty
-
-if [ $BUMP_RAILS_GEM == 'y' ]; then
-  read -rp "What type of bump? (major,minor,patch): " BUMP_TYPE </dev/tty
-  (cd $sage_docs_path && bundle exec bump $BUMP_TYPE --no-commit)
-
+function commit_bump() {
   git add $sage_docs_path/lib/sage_rails/lib/sage_rails/version.rb
   (cd $sage_docs_path && bundle install)
   git add $sage_docs_path/Gemfile.lock
 
   git commit -m "chore(gem): bumping rails gem"
+}
+
+read -rp "Would you like to bump the Sage Rails gem? (y/n): " BUMP_RAILS_GEM </dev/tty
+
+if [ $BUMP_RAILS_GEM == 'y' ]; then
+  read -rp "What type of bump? (major,minor,patch): " BUMP_TYPE </dev/tty
+  (cd $sage_docs_path && bundle exec bump $BUMP_TYPE --no-commit)
+  commit_bump
 fi;
