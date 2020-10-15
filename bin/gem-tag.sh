122a098507a08ef12e8aa81a877361d8e8a0c16c
Shane Smith <voodoogq@gmail.com>
2020-10-15 08:34:50 -0500

    fix(monorepo): verifying version bump

--- bin/gem-tag.sh
+++ bin/gem-tag.sh
@@ -12,6 +12,9 @@ read -rp "Would you like to bump the Sage Rails gem? (y/n): " BUMP_RAILS_GEM </d
 if [ $BUMP_RAILS_GEM == 'y' ]; then
   read -rp "What type of bump? (major,minor,patch): " BUMP_TYPE </dev/tty
   (cd $sage_docs_path && bundle exec bump $BUMP_TYPE --no-commit)
+
   git add $sage_docs_path/lib/sage_rails/lib/sage_rails/version.rb
+  git add $sage_docs_path/Gemfile.lock
+
   git commit -m "chore(gem): bumping rails gem"
 fi;
