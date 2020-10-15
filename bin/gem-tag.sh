8dcab7aeea2714ed12ace762abd7b6cd2cd101ef
Shane Smith <voodoogq@gmail.com>
2020-10-15 08:30:36 -0500

    fix(monorepo): updating gem tag script

--- bin/gem-tag.sh
+++ bin/gem-tag.sh
@@ -7,12 +7,10 @@ sage_docs_path=$sage_repo_path/docs
 
 echo_custom "[GEM]" "Versioning"
 
-echo "Would you like to bump the Sage Rails gem? (y/n): "
-read BUMP
+read -rp "Would you like to bump the Sage Rails gem? (y/n): " BUMP_RAILS_GEM
 
-if [ $BUMP == 'y' ]; then
-  echo "What type of bump? (major,minor,patch): "
-  read BUMP_TYPE
+if [ $BUMP_RAILS_GEM == 'y' ]; then
+  read -rp "What type of bump? (major,minor,patch): " BUMP_TYPE
   (cd $sage_docs_path && bundle exec bump $BUMP_TYPE --no-commit)
   git add $sage_docs_path/lib/sage_rails/lib/sage_rails/version.rb
   git commit -m "chore(gem): bumping rails gem"
