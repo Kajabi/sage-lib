5778895984d1032db9cb710c1747d4a7cfdb7202
Shane Smith <voodoogq@gmail.com>
2020-10-16 12:18:56 -0500

    fix(docs): updating local-link-gem.sh

--- bin/local-link-gem.sh
+++ bin/local-link-gem.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
+# Setup a local link to the internal gem in the directory called
 
+# Paths
 sage_repo_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; cd .. ; pwd -P )
 sage_docs_path=$sage_repo_path/docs
-
+# Utilities
 . $sage_repo_path/bin/utils.sh
 
+# Run a quiet bundle install in the current directory
 function bundle_install() {
   echo_custom "[GEM]:" "bundle install in ${PWD}"
   bundle install --quiet
 }
 
+# Retrieve the current status of all local gems in the current directory
 function show_status_of_gem() {
-  echo_custom "[GEM]:" "Current Bundle Config Entries in ${PWD} 💎"
-  bundle config
+  echo_custom "[GEM]:" "Current local gem entries in ${PWD} 💎"
+  bundle config | grep -e "^local\.*" -A 1
 }
 
+# If true or false is passed
 if [ "$1" = "true" ] || [ "$1" = "false" ]; then
-
-  # UNINSTALL Local Bindings
+  # Uninstall local bindings if passed false
   if [ "$1" = "false" ]; then
     echo_custom "[GEM]:" "Removing local Sage gem in ${PWD}"
     bundle config --delete disable_local_branch_check
     bundle config --delete local.sage_rails
     bundle_install
     echo_custom "[GEM]" "${PWD} is now using PRODUCTION SAGE ✅"
-
-  # INSTALL Local Bindings
+  # Install local bindings if passed true
   elif [ "$1" = "true" ]; then
     echo_custom "[GEM]:" "Use local Sage gem ($sage_repo_path) in ${PWD}"
     bundle config --local disable_local_branch_check true
@@ -39,5 +42,5 @@ if [ "$1" = "true" ] || [ "$1" = "false" ]; then
 elif [ "$1" = "status" ]; then
     show_status_of_gem
 else
-  echo_help
+  echo 'Usage: ./bin/local-link-gem.sh (<boolean> | status)'
 fi
--- bin/utils.sh
+++ bin/utils.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-sage_repo_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; cd .. ; pwd -P )
-sage_docs_path=$sage_repo_path/docs
-sage_bin_path=$sage_repo_path/bin
-
 function echo_custom() {
   printf "\n\n\033[0;34m${1} \033[0m${2}\n\033[0;34m------------------------------------------------\033[0m\n"
 }
 
 function echo_custom_error() {
   printf "\n\n\033[0;31m${1} \033[0m${2}\n\n"
-}
-
-function echo_help() {
-  echo_custom "Run inside the of root the Kajabi-Products repo:"
-  echo "$ <PATH TO SAGE REPO>/bin/sage-local-link <BOOLEAN>"
-  echo "$ <PATH TO SAGE REPO>/bin/sage-local-link status"
-}
-
-function current_rails_version() {
-  (cd $sage_docs_path && bundle exec bump current | cut -d: -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 }
