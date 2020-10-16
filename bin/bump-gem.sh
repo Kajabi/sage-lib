2fa363a1f73452cf35010f6fb6446fa84756800d
Shane Smith <voodoogq@gmail.com>
2020-10-16 09:56:35 -0500

    feat(monorepo): skip ci directive

--- bin/bump-gem.sh
+++ bin/bump-gem.sh
@@ -14,8 +14,8 @@ if [[ $BUMP_TYPE != 'null' ]]; then
   (cd $sage_docs_path && bundle exec bump $BUMP_TYPE --no-commit)
   (cd $sage_docs_path && bundle install)
   git add $sage_docs_path/Gemfile.lock
-  git add $sage_docs_path/lib/sage_rails/version.rb
-  git commit -m "chore(ci): updating rails gem version"
+  git add $sage_docs_path/lib/sage_rails/lib/sage_rails/version.rb
+  git commit -m "chore(ci): updating rails gem version [ci skip]"
 else
   echo 'Nothing to bump!'
 fi;
