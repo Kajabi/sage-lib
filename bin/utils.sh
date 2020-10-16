be268ac228c9f3d1bfd33ae73f41a40022fa255b
Shane Smith <voodoogq@gmail.com>
2020-10-16 12:29:21 -0500

    fix(monorepo): docs for bin/utils.sh

--- bin/utils.sh
+++ bin/utils.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# Utility functions
 
+# Custom echo header
 function echo_custom() {
   printf "\n\n\033[0;34m${1} \033[0m${2}\n\033[0;34m------------------------------------------------\033[0m\n"
 }
 
+# Custom error handler
 function echo_custom_error() {
   printf "\n\n\033[0;31m${1} \033[0m${2}\n\n"
 }
