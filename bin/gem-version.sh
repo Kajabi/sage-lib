d04f051f8959d1609cb4a7d8c7f8658d91eec0b3
Shane Smith <voodoogq@gmail.com>
2020-10-16 11:51:42 -0500

    fix(monorepo): docs bin/gem-version.sh

--- bin/gem-version.sh
+++ bin/gem-version.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# Retrieve the current version of the Rails Gem
 
+# Paths
 sage_repo_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; cd .. ; pwd -P )
 sage_docs_path=$sage_repo_path/docs
 
-. $sage_repo_path/bin/utils.sh
+# Get the current path, trim whitespace
+function current_rails_version() {
+  (cd $sage_docs_path && bundle exec bump current | cut -d: -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+}
 
 echo $(current_rails_version)
