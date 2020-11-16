5aa09a3902da76d8fdf867134fadfb921384b419
Phil Schanely <philschanely@gmail.com>
2020-11-16 06:59:37 -0500

    feat(sage-react): update storybook configs to match starting point
    Storybook in `kajabi-products` used some older addons for Storybook.
    We should consider updating our stories to the newer format
    in the future.

--- packages/sage-react/.storybook/main.js
+++ packages/sage-react/.storybook/main.js
@@ -1,10 +1,15 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../lib/**/*.story.mdx",
+    "../lib/**/*.story.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-notes",
+    "@storybook/addon-knobs",
+    "@storybook/addon-storysource",
   ]
 }
