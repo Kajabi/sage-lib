af84595884b8dac3e6d62ce2f97fda2b3781eadb
joedakroub <joe.d@kajabi.com>
2020-11-16 15:50:07 -0500

    fix(sage-react): removed reference to stylesheet

--- packages/sage-react/lib/Tabs/TabsItem.jsx
+++ packages/sage-react/lib/Tabs/TabsItem.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { SageTokens } from '../configs';
 import { TAB_STYLES, CHOICE_TYPES } from './configs';
-import './TabsItem.scss';
 
 const TabsItem = ({
   alignCenter,
