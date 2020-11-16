edd7ec3ca2f8ae86e779580edb96aa7a2926a3e9
joedakroub <joe.d@kajabi.com>
2020-11-16 12:59:45 -0500

    fix(docs): added choice, tab, tabs_pane and tabs components and views

---
+++ docs/lib/sage_rails/app/sage_components/sage_tab.rb
@@ -0,0 +1,7 @@
+class SageTab < SageComponent
+  attr_accessor :target
+  attr_accessor :text
+  attr_accessor :active
+  attr_accessor :attributes
+  attr_accessor :disabled
+end
---
+++ docs/lib/sage_rails/app/sage_components/sage_tabs.rb
@@ -0,0 +1,9 @@
+class SageTabs < SageComponent
+  attr_accessor :style
+  attr_accessor :stacked
+  attr_accessor :id
+  attr_accessor :items
+  attr_accessor :progressbar
+  attr_accessor :align_items_center
+  attr_accessor :navigational
+end
