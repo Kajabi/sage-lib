93c6dad52e7a227a76c08092e24e6bfa2e7a0f17
Kevin Chang <kevin@kajabi.com>
2020-11-16 14:34:46 -0800

    fix(banner): migrate markup

---
+++ docs/lib/sage_rails/app/sage_components/sage_banner.rb
@@ -0,0 +1,10 @@
+class SageBanner < SageComponent
+  attr_accessor :banner_context
+  attr_accessor :dismissable
+  attr_accessor :type
+  attr_accessor :icon
+  attr_accessor :text
+  attr_accessor :link
+  attr_accessor :id
+  attr_accessor :active
+end
