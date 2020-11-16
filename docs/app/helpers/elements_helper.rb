a5ed72aeb47f306a05abd52fa401e336e5faf84a
Quinton Jason <quinton.jason@gmail.com>
2020-11-16 17:03:14 -0600

    fix: migrate sage checkbox rails component

--- docs/app/helpers/elements_helper.rb
+++ docs/app/helpers/elements_helper.rb
@@ -33,6 +33,16 @@ module ElementsHelper
         a11y: "done"
       },
       {
+        title: "checkbox",
+        description: "Checkboxes provide users with selectable options like toggling a single setting or selecting multiple options from a list.",
+        use_legacy_html_code_source: false,
+        scss: "done",
+        docs: "todo",
+        rails: "no",
+        react: "no",
+        a11y: "todo"
+      },
+      {
         title: "choice",
         description: "A radio button tab for making a choice. To be used inside a tabs object.",
         use_legacy_html_code_source: false,
