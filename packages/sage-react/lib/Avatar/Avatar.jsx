d0820a0fc9f4659c2f2fb1428f36093a1ea6519d
Quinton Jason <quinton.jason@gmail.com>
2020-11-16 14:13:25 -0600

    fix: migrated avatar react component

---
+++ packages/sage-react/lib/Avatar/Avatar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { AVATAR_COLORS } from './configs';
+
+const Avatar = ({
+  className,
+  color,
+  initials,
+  ...rest
+}) => {
+  const classNames = classnames(
+    'sage-avatar',
+    className,
+    {
+      [`sage-avatar--${color}`]: color
+    }
+  );
+
+  return (
+    <div className={classNames} {...rest}>
+      <svg className="sage-avatar__initials" viewBox="0 0 32 32">
+        <text x="16" y="20">{initials}</text>
+      </svg>
+    </div>
+  );
+};
+
+Avatar.COLORS = AVATAR_COLORS;
+
+Avatar.defaultProps = {
+  className: '',
+  color: AVATAR_COLORS.SAGE,
+  initials: ''
+};
+
+Avatar.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.oneOf(Object.values(AVATAR_COLORS)),
+  initials: PropTypes.string
+};
+
+export default Avatar;
