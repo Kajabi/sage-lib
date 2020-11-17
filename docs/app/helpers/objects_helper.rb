db69e0f6cd8c379e1c2e142982e1fe9aa91ed180
Quinton Jason <quinton.jason@gmail.com>
2020-11-17 12:38:11 -0600

    fix: updated objects_help for Avatar

--- docs/app/helpers/objects_helper.rb
+++ docs/app/helpers/objects_helper.rb
@@ -13,13 +13,6 @@ module ObjectsHelper
     [
       # Sage Generated Objects
       {
-        title: "alert",
-        description: "Alerts are used to indicate user-driven notifications.",
-        use_legacy_html_code_source: false,
-        scss: "done",
-        docs: "done",
-        rails: "done",
-        react: "todo",
         title: "avatar",
         description: "The Avatar object shows a user's profile image in a circular frame and allows for a few helpful modifications for different uses.",
         use_legacy_html_code_source: false,
