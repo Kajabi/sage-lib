f0388445f4d70cc6197d1504dfdf09b5a8655acb
joedakroub <joe.d@kajabi.com>
2020-11-17 13:24:13 -0500

    fix(sage-react): added catalog_item and data_card components

---
+++ packages/sage-react/lib/DataCard/DataCardGroup.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { DATA_CARD_COLORS } from './configs';
+
+const DataCardGroup = ({
+  children,
+  className,
+  color,
+  title,
+  titleTag,
+  ...rest
+}) => {
+  const TitleTag = titleTag;
+  const classNames = classnames(
+    'sage-data-card-group',
+    className,
+    {
+      [`sage-data-card-group--${color}`]: color,
+    }
+  );
+
+  return (
+    <div className={classNames} {...rest}>
+      {title ? (
+        <>
+          <TitleTag className="sage-data-card-group__title">{title}</TitleTag>
+          <div className="sage-data-card-group__items">
+            {children}
+          </div>
+        </>
+      ) : children}
+    </div>
+  );
+};
+
+DataCardGroup.defaultProps = {
+  children: null,
+  className: '',
+  color: null,
+  title: null,
+  titleTag: 'h4',
+};
+
+DataCardGroup.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  color: PropTypes.oneOf(Object.values(DATA_CARD_COLORS)),
+  title: PropTypes.string,
+  titleTag: PropTypes.string,
+};
+
+export default DataCardGroup;
---
+++ packages/sage-react/lib/DataCard/configs.js
@@ -0,0 +1,5 @@
+export const DATA_CARD_COLORS = {
+  DEFAULT: null,
+  DANGER: 'danger',
+  MUTED: 'muted',
+};
