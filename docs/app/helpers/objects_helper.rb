9a571df79386f143f7ba1bd74dce0c6cf668c510
Quinton Jason <quinton.jason@gmail.com>
2020-11-17 18:09:11 -0600

    fix: migrate rails page heading component

--- docs/app/helpers/objects_helper.rb
+++ docs/app/helpers/objects_helper.rb
@@ -83,6 +83,16 @@ module ObjectsHelper
         a11y: "done"
       },
       {
+        title: "page_heading",
+        description: "Page headings are used at the top of pages and contain the title of the page along with optional breadcrumbs, help link, action buttons, toolbar, and intro text.",
+        use_legacy_html_code_source: false,
+        scss: "done",
+        docs: "done",
+        rails: "done",
+        react: "done",
+        a11y: "done"
+      },
+      {
         title: "pagination",
         description: "Pagination is used for splitting up results into several pages and provides controls for navigating to the next or previous page.",
         use_legacy_html_code_source: true,
