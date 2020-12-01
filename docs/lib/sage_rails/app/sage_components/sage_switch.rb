ed740755046279093656f49d911bcd6a235c2dc0
Quinton Jason <quinton.jason@gmail.com>
2020-12-01 14:03:29 -0600

    fix(switch): added option to visually hide label text

--- docs/lib/sage_rails/app/sage_components/sage_switch.rb
+++ docs/lib/sage_rails/app/sage_components/sage_switch.rb
@@ -3,12 +3,12 @@ class SageSwitch < SageComponent
   attr_accessor :checked
   attr_accessor :disabled
   attr_accessor :has_error
+  attr_accessor :hide_text
   attr_accessor :id
   attr_accessor :label_text
   attr_accessor :name
   attr_accessor :message
   attr_accessor :required
   attr_accessor :type
-  attr_accessor :hide_text
   attr_accessor :value
 end
