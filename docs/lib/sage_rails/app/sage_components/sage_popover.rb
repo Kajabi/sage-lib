55a20005e56558435443bad9dab789bc60168b2c
Phil Schanely <philschanely@gmail.com>
2020-12-04 14:54:52 -0500

    feat(popover)!: adjust popover to allow default styles and customizations
    - Restores default styling for content in popovers
      but adds a `sage-popover__content--custom` modifier
      that turns off default styling.
    - Adds `custom_content_class` and logic in Rails
      for an open, customizable content panel
      and updates documentation of this setting.
    - Adds `customContentClassName` and logic in React for the same.

--- docs/lib/sage_rails/app/sage_components/sage_popover.rb
+++ docs/lib/sage_rails/app/sage_components/sage_popover.rb
@@ -1,10 +1,11 @@
 class SagePopover < SageComponent
-  attr_accessor :id
-  attr_accessor :title
   attr_accessor :body
-  attr_accessor :link
+  attr_accessor :custom_content_class
   attr_accessor :icon
+  attr_accessor :id
+  attr_accessor :link
+  attr_accessor :popover_position
+  attr_accessor :title
   attr_accessor :trigger_icon_only
   attr_accessor :trigger_value
-  attr_accessor :popover_position
 end
