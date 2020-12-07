4df1eaf732c056c46d95d193958f8b20f5d2d116
Quinton Jason <quinton.jason@gmail.com>
2020-12-07 10:17:43 -0600

    fix(loader): updated react component

--- packages/sage-react/lib/Loader/Loader.jsx
+++ packages/sage-react/lib/Loader/Loader.jsx
@@ -23,12 +23,28 @@ const Loader = ({
   );
 
   const ariaAttrs = {
-    'aria-label': loading ? label : null,
     'aria-hidden': !loading,
+    'aria-busy': true,
+    'aria-live': "polite"
   };
 
   return (
-    <div className={classNames} data-loading={loading} {...ariaAttrs} {...rest} />
+    <div className={classNames} data-loading={loading} {...ariaAttrs} {...rest}>
+      {
+        (shape === LOADER_SHAPES.SPINNER )
+        && (
+        <svg class="spinner" viewBox="25 25 50 50" >
+          <defs>
+            <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="0%"   stop-color="#0072EF"/>
+              <stop offset="100%" stop-color="#4fc9c5"/>
+            </linearGradient>
+          </defs>
+          <circle class="spinner-path" cx="50" cy="50" r="20" fill="none" stroke="url(#linear)" stroke-width="4" />
+        </svg>
+      )}
+      <span class="visually-hidden">{label}</span>
+    </div>
   );
 };
 
