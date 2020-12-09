c24a06fa48a3083bc705aa75723bf7f87e3cfd1a
Andrew McIntee <contact@andrew.mn>
2020-12-08 21:49:36 -0500

    feat(typeahead): focus trap typeahead field

--- packages/sage-react/lib/Typeahead/Typeahead.jsx
+++ packages/sage-react/lib/Typeahead/Typeahead.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { createFocusTrap } from 'focus-trap';
 import uuid from 'react-uuid';
 
 import { SageTokens } from '../configs';
@@ -11,48 +12,76 @@ const Typeahead = ({
   ...rest
 }) => {
   const [value, setValue] = useState('');
-  const [results, setResults] = useState(null);
+  const [open, setOpen] = useState(false);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!open) return;
+
+    const focusTrap = createFocusTrap(containerRef.current, {escapeDeactivates: false});
+    const handleEscape = (evt) => {
+      if (evt.keyCode === 27) setOpen(false);
+    };
+
+    focusTrap.activate();
+    document.addEventListener("keydown", handleEscape, false);
+    return () => {
+      focusTrap.deactivate();
+      document.addEventListener("keydown", handleEscape, false);
+    };
+  }, [open, containerRef.current]);
 
   return (
-    <div className="sage-typeahead">
+    <div
+      className="sage-typeahead"
+      ref={containerRef}
+    >
       <Search
         contained={true}
-        onChange={e => setValue(e.target.value)}
-        onClear={() => setValue('')}
+        onChange={e => {
+          setValue(e.target.value);
+          e.target.value.length ? setOpen(true) : setOpen(false);
+        }}
+        onClear={() => {
+          setValue('');
+          setOpen(false);
+        }}
         value={value}
       />
-      <ul className="sage-typeahead__panel">
-        {items.map(item => (
-          <li
-            className="sage-typeahead__item"
-            key={uuid()}
-          >
-            <button className="sage-typeahead__item-trigger">
-              <i
-                className={`sage-icon-${item.icon}`}
-                style={{'grid-area': 'icon'}}
-              />
-              <div
-                className="t-sage-heading-6"
-                style={{'grid-area': 'title'}}
-              >
-                {item.title}
+      {open &&
+        <ul className="sage-typeahead__panel">
+          {items.map(item => (
+            <li
+              className="sage-typeahead__item"
+              key={uuid()}
+            >
+              <button className="sage-typeahead__item-trigger">
+                <i
+                  className={`sage-icon-${item.icon}`}
+                  style={{gridArea: 'icon'}}
+                />
+                <div
+                  className="t-sage-heading-6"
+                  style={{gridArea: 'title'}}
+                >
+                  {item.title}
+                </div>
+                <div
+                  className="t-sage-body-small"
+                  style={{gridArea: 'subTitle'}}
+                >
+                  {item.subTitle}
+                </div>
+              </button>
+              <div className="sage-typeahead__item-actions">
+                {item.actions.map(action => (
+                  React.cloneElement(action, {key: uuid()})
+                ))}
               </div>
-              <div
-                className="t-sage-body-small"
-                style={{'grid-area': 'subTitle'}}
-              >
-                {item.subTitle}
-              </div>
-            </button>
-            <div className="sage-typeahead__item-actions">
-              {item.actions.map(action => (
-                React.cloneElement(action, {key: uuid()})
-              ))}
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      }
     </div>
   );
 };
