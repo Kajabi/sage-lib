1f5cd48b712a2ef092b7edd8b46b141e050e6cf3
Quinton Jason <quinton.jason@gmail.com>
2020-12-09 08:40:27 -0600

    fix(feature-toggle): updated properties

--- docs/lib/sage_rails/app/sage_components/sage_feature_toggle.rb
+++ docs/lib/sage_rails/app/sage_components/sage_feature_toggle.rb
@@ -1,8 +1,10 @@
 class SageFeatureToggle < SageComponent
-  attr_accessor :image
-  attr_accessor :toggle_name
-  attr_accessor :link_text
-  attr_accessor :link_location
   attr_accessor :description
+  attr_accessor :image
+  attr_accessor :link_location
+  attr_accessor :link_text
   attr_accessor :title
+  attr_accessor :toggle_id
+  attr_accessor :toggle_name
+  attr_accessor :toggle_value
 end
