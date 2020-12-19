d59bae993e1d48dd81a909d9cafd010223664b98
Kevin Chang <kevin@kajabi.com>
2020-12-18 17:39:59 -0800

    docs: revised url paths, rearranged links

--- docs/config/initializers/load_sage_version.rb
+++ docs/config/initializers/load_sage_version.rb
@@ -1,15 +1,28 @@
-frontend_package_contents = JSON.parse(
+react_package_contents = JSON.parse(
                               File.read(
-                                File.join(Rails.root, 'package.json')
+                                File.join("../packages/sage-react/", "package.json")
                               )
                             )
-
+assets_package_contents = JSON.parse(
+                              File.read(
+                                File.join("../packages/sage-assets/", "package.json")
+                              )
+                            )
+system_package_contents = JSON.parse(
+                              File.read(
+                                File.join("../packages/sage-system/", "package.json")
+                              )
+                            )
 $SAGE_VERSION_GEM = SageRails::VERSION
-$SAGE_VERSION_FRONTEND = frontend_package_contents["version"]
+$SAGE_VERSION_REACT = react_package_contents["version"]
+$SAGE_VERSION_ASSETS = assets_package_contents["version"]
+$SAGE_VERSION_SYSTEM = system_package_contents["version"]
 
 $SAGE_GITHUB_URL = "https://github.com/Kajabi/sage-lib"
+$SAGE_GITHUB_PACKAGE_URL = "#{$SAGE_GITHUB_URL}/tree/@kajabi/sage@#{$SAGE_VERSION_GEM}/packages"
 
-$SAGE_VERSION_GEM_URL = "#{$SAGE_GITHUB_URL}/tree/v#{$SAGE_VERSION_GEM}/lib/sage_rails"
-$SAGE_VERSION_FRONTEND_URL = "#{$SAGE_GITHUB_URL}/tree/v#{$SAGE_VERSION_FRONTEND}/lib/sage-frontend"
-
-$SAGE_RELEASE_URL = "#{$SAGE_GITHUB_URL}/releases/tag/v#{$SAGE_VERSION_FRONTEND}"
+$SAGE_VERSION_GEM_URL = "#{$SAGE_GITHUB_PACKAGE_URL}/sage-system"
+$SAGE_VERSION_REACT_URL = "#{$SAGE_GITHUB_PACKAGE_URL}/sage-react"
+$SAGE_VERSION_ASSETS_URL = "#{$SAGE_GITHUB_PACKAGE_URL}/sage-assets"
+$SAGE_VERSION_SYSTEM_URL = "#{$SAGE_GITHUB_PACKAGE_URL}/sage-system"
+$SAGE_RELEASE_URL = "#{$SAGE_GITHUB_URL}/releases/tag/@kajabi/sage@#{$SAGE_VERSION_GEM}"
