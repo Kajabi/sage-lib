3ecb5a7053ab26488022b6f0ba8a2dad23cda4df
Phil Schanely <philschanely@gmail.com>
2021-01-13 15:38:58 -0500

    feat(property): add react Property component

---
+++ packages/sage-react/lib/Property/Property.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import {
+  Icon,
+  SageTokens,
+} from '..';
+
+const Property = ({
+  children,
+  className,
+  icon,
+  ...rest
+}) => {
+  const classNames = classnames(
+    'sage-property',
+    className,
+  );
+
+  return (
+    <span className={classNames} {...rest}>
+      {icon && (
+        <Icon icon={icon} className="sage-property__icon" />
+      )}
+      {children && (
+        <span className="sage-property__value">
+          {children}
+        </span>
+      )}
+    </span>
+  );
+};
+
+Property.defaultProps = {
+  className: null,
+  children: null,
+  icon: null,
+};
+
+Property.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+  icon: PropTypes.oneOf(Object.values(SageTokens.ICONS)),
+};
+
+export default Property;
