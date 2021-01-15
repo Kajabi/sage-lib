2b881ad9db214f7fe4c3203534e75be1d3e3b769
Kevin Chang <kevin@kajabi.com>
2021-01-14 16:25:10 -0800

    feat(checkbox): conditionally output attributes, name, and value properties

--- docs/lib/sage_rails/app/sage_components/sage_checkbox.rb
+++ docs/lib/sage_rails/app/sage_components/sage_checkbox.rb
@@ -1,5 +1,6 @@
 class SageCheckbox < SageComponent
   set_attribute_schema({
+    attributes: [:optional, Hash],
     checked: [:optional, TrueClass],
     css_classes: [:optional, String],
     disabled: [:optional, TrueClass],
