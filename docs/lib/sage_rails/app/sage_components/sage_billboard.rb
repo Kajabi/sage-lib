4e8e748dba064d54c3b0dda59456302ff0597d7a
Quinton Jason <quinton.jason@gmail.com>
2021-01-21 09:43:22 -0600

    fix(billboard): updated props and added title_tag

--- docs/lib/sage_rails/app/sage_components/sage_billboard.rb
+++ docs/lib/sage_rails/app/sage_components/sage_billboard.rb
@@ -2,6 +2,7 @@ class SageBillboard < SageComponent
   set_attribute_schema({
     img: [:optional, String],
     title: [:optional, String],
+    title_tag: [:optional, String],
     message: [:optional, String],
   })
 end
