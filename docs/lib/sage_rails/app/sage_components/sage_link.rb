1c41d5b8aab20f0c58b2f3904a3b44936ccb018c
Quinton Jason <quinton.jason@gmail.com>
2021-01-25 13:59:39 -0600

    fix(link): add rails component and updated docs

---
+++ docs/lib/sage_rails/app/sage_components/sage_link.rb
@@ -0,0 +1,10 @@
+class SageLink < SageComponent
+  set_attribute_schema({
+    external: [:optional, TrueClass],
+    help_link: [:optional, TrueClass],
+    label: [:optional, String],
+    launch: [:optional, TrueClass],
+    show_label: [:optional, TrueClass],
+    url: [:optional, String],
+  })
+end
