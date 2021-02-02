d19f6ef9299d6e409605d406bf99c94ab9e8851e
Phil Schanely <philschanely@gmail.com>
2021-02-02 15:45:47 -0500

    feat(modal): add spacing features to `modal__content`

--- docs/lib/sage_rails/app/sage_components/sage_modal_content.rb
+++ docs/lib/sage_rails/app/sage_components/sage_modal_content.rb
@@ -1,6 +1,7 @@
 class SageModalContent < SageComponent
   set_attribute_schema({
     title: [:optional, String],
+    spacing: [:optional, NilClass, Set.new(["panel", "card"])],
   })
 
   def sections
