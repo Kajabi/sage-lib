43a9a7a518ce8234bb2fb4d317a433137f318c71
joedakroub <joe.d@kajabi.com>
2021-02-03 17:42:07 -0500

    fix(sage_feature_toggle): added links prop to feature toggle and updated docs

--- docs/lib/sage_rails/app/sage_components/sage_feature_toggle.rb
+++ docs/lib/sage_rails/app/sage_components/sage_feature_toggle.rb
@@ -3,10 +3,11 @@ class SageFeatureToggle < SageComponent
     alt_text: [:optional, String],
     description: [:optional, String],
     image: [:optional, String],
-    link_location: [:optional, String],
-    link_text: [:optional, String],
-    secondary_link_location: [:optional, String],
-    secondary_link_text: [:optional, String],
+    links: [:optional, [[{
+      icon: [:optional, NilClass, String],
+      location: [:optional, String],
+      text: [:optional, String],
+    }]]],
     title: [:optional, String],
     title_tag: [:optional, String],
   })
