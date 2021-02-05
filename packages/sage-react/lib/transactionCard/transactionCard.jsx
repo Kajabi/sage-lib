491781a92dee532dd4f528bed3ebf9c85f7d4e98
Phillip Lovelace <phillip@pixelflips.com>
2021-02-05 14:31:43 -0800

    fix(transaction card): lint fixes

--- packages/sage-react/lib/transactionCard/transactionCard.jsx
+++ packages/sage-react/lib/transactionCard/transactionCard.jsx
@@ -1,19 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import { SageTokens } from '../configs';
-import { Property } from "../Property";
-import { Icon } from "../Icon";
-import { Label } from "../Label";
-import { Dropdown } from "../Dropdown";
-import { AMOUNT_COLORS } from "./configs";
-
+import { Property } from '../Property';
+import { Icon } from '../Icon';
+import { Label } from '../Label';
+import { Dropdown } from '../Dropdown';
+import { AMOUNT_COLORS } from './configs';
 
 export const TransactionCard = ({
   name,
   amount,
   amountColor,
-  className,
-  ...rest
 }) => (
 
   <div className="sage-transaction-card">
@@ -32,20 +29,21 @@ export const TransactionCard = ({
             label: 'View Contact Profile',
             icon: SageTokens.ICONS.USER_CIRCLE,
           }]
-        } />
+        }
+        />
       </Dropdown>
     </div>
     <div className="sage-transaction-card__body">
       <h4 className="sage-transaction-card__name">{name}</h4>
       {amount && (
-        <div className={`sage-transaction-card__amount${amountColor ? " sage-transaction-card__amount--" + amountColor : ""}`}>
+        <div className={`sage-transaction-card__amount${amountColor ? ` sage-transaction-card__amount--${amountColor}` : ''}`}>
           {amount}
         </div>
       )}
     </div>
     <div className="sage-transaction-card__footer">
       <Property icon={Icon.ICONS.TAG}>Total Product Blueprint</Property>
-      <Property children="10:32pm" />
+      <Property>10:32pm</Property>
     </div>
   </div>
 
@@ -54,14 +52,13 @@ export const TransactionCard = ({
 TransactionCard.AMOUNT_COLORS = AMOUNT_COLORS;
 
 TransactionCard.defaultProps = {
-  name: null,
-  amount: "0.00",
+  name: '',
+  amount: '0.00',
   amountColor: null,
 };
 
 TransactionCard.propTypes = {
-  name: PropTypes.string.isRequired,
+  name: PropTypes.string,
   amount: PropTypes.string,
   amountColor: PropTypes.oneOf(Object.values(TransactionCard.AMOUNT_COLORS)),
 };
-
--- packages/sage-react/lib/transactionCard/transactionCard.story.jsx
+++ packages/sage-react/lib/transactionCard/transactionCard.story.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { withKnobs, text, select } from "@storybook/addon-knobs";
-import { TransactionCard } from "./transactionCard";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
+import { TransactionCard } from './TransactionCard';
 
-storiesOf("Sage/TransactionCard", module)
+storiesOf('Sage/TransactionCard', module)
   .addDecorator(withKnobs)
   .add(
-    "Default",
+    'Default',
     () => (
       <TransactionCard
-        name={text("Name", "Lilly Jones")}
-        amount={text("Amount", "+ $148.00")}
-        amountColor={select("Amount Color", TransactionCard.AMOUNT_COLORS)}
-      ></TransactionCard>
+        name={text('Name', 'Lilly Jones')}
+        amount={text('Amount', '+ $148.00')}
+        amountColor={select('Amount Color', TransactionCard.AMOUNT_COLORS)}
+      />
     ),
     {}
   );
