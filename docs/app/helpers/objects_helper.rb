0d43e10ea1a3aa907d87520ddcf10edac4be508f
Phillip Lovelace <phillip@pixelflips.com>
2021-02-08 09:04:11 -0800

    docs(transaction card): add description

--- docs/app/helpers/objects_helper.rb
+++ docs/app/helpers/objects_helper.rb
@@ -292,7 +292,7 @@ module ObjectsHelper
       },
       {
         title: "transaction_card",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description: "The Transaction Card displays customer transaction details in a compact view.",
         scss: "todo",
         rails: "todo",
         react: "todo",
