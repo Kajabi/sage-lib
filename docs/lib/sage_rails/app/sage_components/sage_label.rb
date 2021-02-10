50d8e4c2d8fa0be205d3677afbbc5e4c15b8fe97
Phil Schanely <philschanely@gmail.com>
2021-02-10 13:01:01 -0500

    feat(label): integrate secondary button into main label in Rails

--- docs/lib/sage_rails/app/sage_components/sage_label.rb
+++ docs/lib/sage_rails/app/sage_components/sage_label.rb
@@ -1,10 +1,14 @@
 class SageLabel < SageComponent
   set_attribute_schema({
     color: Set.new(["danger", "draft", "info", "locked", "published", "success", "warning"]),
-    html_tag: [:optional, String],
     icon: [:optional, String],
     interactive_type: [:optional, Set.new([:dropdown, :default, :secondary_button])],
-    secondary_button: [:optional, String],
+    secondary_button: [:optional, String, TrueClass, {
+      attributes:       [:optional, NilClass, Hash],
+      css_classes:      [:optional, String],
+      icon:             [:optional, String],
+      value:            [:optional, String],
+    }],
     style: [:optional, Set.new(["subtle", "bold"])],
     value: String,
   })
