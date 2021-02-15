de4330e326d27367b4cef243a9b99486b2512425
Quinton Jason <quinton.jason@gmail.com>
2021-02-15 14:51:19 -0600

    fix(modal): add prop to modal to remove contents on close

--- packages/sage-system/lib/modal.js
+++ packages/sage-system/lib/modal.js
@@ -5,7 +5,9 @@ Sage.modal = (function() {
   // ==================================================
 
   const SELECTOR_MODAL = 'data-js-modal';
+  const SELECTOR_MODAL_CONTAINER = '.sage-modal__container';
   const SELECTOR_MODAL_DISABLE_CLOSE = 'data-js-modal-disable-close';
+  const SELECTOR_MODAL_REMOVE_CONTENTS_ON_CLOSE = 'data-js-modal-remove-contents-on-close';
   const SELECTOR_MODAL_CLOSE = 'data-js-modal-close';
   const SELECTOR_MODALTRIGGER = 'data-js-modaltrigger';
   const SELECTOR_FOCUSABLE_ELEMENTS = 'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])';
@@ -94,6 +96,12 @@ Sage.modal = (function() {
     el.removeAttribute("open");
     el.removeEventListener('keydown', focusTrap);
     SELECTOR_LAST_FOCUSED.focus();
+    removeModalContents(el);
+  }
+
+  function removeModalContents(el) {
+    let elContainer = el.querySelector(`${SELECTOR_MODAL_REMOVE_CONTENTS_ON_CLOSE}`);
+    elContainer.innerHTML = "";
   }
 
   function eventHandlerCloseAll() {
