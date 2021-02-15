1ecaa01cdf7ae6ff89aad5f8774cac5e2eae7be6
Phil Schanely <philschanely@gmail.com>
2021-02-15 16:44:10 -0500

    fix(nav): update nav component

--- docs/lib/sage_rails/app/sage_components/sage_nav.rb
+++ docs/lib/sage_rails/app/sage_components/sage_nav.rb
@@ -1,5 +1,23 @@
 class SageNav < SageComponent
   set_attribute_schema({
     aria_label: [:optional, String],
+    items: [:optional, [[
+      active: [:optional, TrueClass],
+      link: [:optional, String],
+      text: [:optional, String],
+      type: [:optional, String],
+      icon: [:optional, String],
+      no_active: [:optional, TrueClass],
+      method: [:optional, Symbol],
+      items: [:optional, [[
+        active: [:optional, TrueClass],
+        link: [:optional, String],
+        text: [:optional, String],
+        type: [:optional, String],
+        icon: [:optional, String],
+        no_active: [:optional, TrueClass],
+        method: [:optional, Symbol],
+      ]]],
+    ]]]
   })
 end
