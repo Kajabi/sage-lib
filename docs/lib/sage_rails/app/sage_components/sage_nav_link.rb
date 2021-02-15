1849ca43d289f736ff925b778839d8e0e6575f9a
Phil Schanely <philschanely@gmail.com>
2021-02-15 16:43:15 -0500

    fix(nav-link): update nav link component

--- docs/lib/sage_rails/app/sage_components/sage_nav_link.rb
+++ docs/lib/sage_rails/app/sage_components/sage_nav_link.rb
@@ -1,7 +1,17 @@
 class SageNavLink < SageComponent
   set_attribute_schema({
-    icon: [:optional, String],
+    active: [:optional, TrueClass],
     link: [:optional, String],
+    icon: [:optional, String],
+    items: [:optional, [[
+      active: [:optional, TrueClass],
+      link: [:optional, String],
+      text: [:optional, String],
+      type: [:optional, String],
+      icon: [:optional, String],
+      method: [:optional, Symbol],
+      no_active: [:optional, TrueClass],
+    ]]],
     method: [:optional, Symbol],
     no_active: [:optional, TrueClass],
     text: [:optional, String],
