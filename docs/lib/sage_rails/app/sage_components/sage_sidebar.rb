0588782ade7966e3dbe1af33fc9c9a9cd82a468e
Phillip Lovelace <phillip@pixelflips.com>
2021-02-15 15:07:18 -0800

    fix(sidebar): update attributes and add sections

--- docs/lib/sage_rails/app/sage_components/sage_sidebar.rb
+++ docs/lib/sage_rails/app/sage_components/sage_sidebar.rb
@@ -1,5 +1,9 @@
 class SageSidebar < SageComponent
   set_attribute_schema({
+    id: [:optional, String],
     size: [:optional, Set.new(["md", "lg"])],
   })
+  def sections
+    %w(sidebar_footer)
+  end
 end
