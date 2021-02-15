ce1967890f1db30a74d550408880db882339d2bf
Phil Schanely <philschanely@gmail.com>
2021-02-15 14:27:40 -0500

    feat(sage-react): add searchNews service call

--- packages/sage-react/lib/services/newsapi.js
+++ packages/sage-react/lib/services/newsapi.js
@@ -1,18 +1,37 @@
 import axios from 'axios';
 
+const headers = {
+  'x-rapidapi-key': 'API_KEY_GOES_HERE', // Get an api key at https://rapidapi.com/newscatcher-api-newscatcher-api-default/api/newscatcher
+  'x-rapidapi-host': 'newscatcher.p.rapidapi.com'
+};
+
+const standardError = (error) => {
+  console.warn(error); // eslint-disable-line
+  console.warn('You likely need to set up an API key with Newscatcher.'); // eslint-disable-line
+};
+
 export const getNews = (page = 1) => {
   const options = {
+    headers,
     method: 'GET',
-    url: 'https://newscatcher.p.rapidapi.com/v1/search_free',
     params: { q: 'Star Wars', lang: 'en', page, page_size: '25', media: 'True' },
-    headers: {
-      'x-rapidapi-key': 'API_KEY_GOES_HERE', // Get an api key at https://rapidapi.com/newscatcher-api-newscatcher-api-default/api/newscatcher
-      'x-rapidapi-host': 'newscatcher.p.rapidapi.com'
-    }
+    url: 'https://newscatcher.p.rapidapi.com/v1/search_free',
   };
 
-  return axios.request(options).then((response) => response.data).catch((error) => {
-    console.warn(error); // eslint-disable-line
-    console.warn('You likely need to set up an API key with Newscatcher.'); // eslint-disable-line
-  });
+  return axios.request(options).then((response) => response.data).catch(standardError);
+};
+
+export const searchNews = (q, page = 1) => {
+  if (!q) {
+    return [];
+  }
+
+  const options = {
+    headers,
+    method: 'GET',
+    params: { q, page, page_size: '25', lang: 'en', media: 'True' },
+    url: 'https://newscatcher.p.rapidapi.com/v1/search_free',
+  };
+
+  return axios.request(options).then((response) => response.data).catch(standardError);
 };
