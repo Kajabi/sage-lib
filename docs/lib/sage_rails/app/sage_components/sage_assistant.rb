80be192dfcd8dd667f96f98315e6d4c5e32f7ff0
Phil Schanely <philschanely@gmail.com>
2021-02-18 11:15:48 -0500

    feat(assistant): implement props to control logo image

--- docs/lib/sage_rails/app/sage_components/sage_assistant.rb
+++ docs/lib/sage_rails/app/sage_components/sage_assistant.rb
@@ -1,6 +1,9 @@
 class SageAssistant < SageComponent
   set_attribute_schema({
     menu_button: [:optional, TrueClass],
+    logo: [:optional, {
+      path: String,
+      logo_method: [:optional, NilClass, Set.new(["pack"])],
+    }]
   })
-
 end
