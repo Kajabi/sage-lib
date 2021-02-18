4b5349337b413a054711f67e01a1c104e824701d
Phil Schanely <philschanely@gmail.com>
2021-02-18 11:46:41 -0500

    feat(assistant): adds additional fallback protection for menu button

--- docs/lib/sage_rails/app/sage_components/sage_assistant.rb
+++ docs/lib/sage_rails/app/sage_components/sage_assistant.rb
@@ -1,6 +1,7 @@
 class SageAssistant < SageComponent
   set_attribute_schema({
     menu_button: [:optional, TrueClass],
+    menu_button_controls: [:optional, String],
     logo: [:optional, {
       path: String,
       method: [:optional, NilClass, Set.new(["pack"])],
