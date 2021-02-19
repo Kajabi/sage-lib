fe5aad6efd6d4d9632d222da79a9b866ff44d4d2
Quinton Jason <quinton.jason@gmail.com>
2021-02-19 07:43:10 -0600

    fix(radio): added bordered variation with updated focus states

--- docs/lib/sage_rails/app/sage_components/sage_radio.rb
+++ docs/lib/sage_rails/app/sage_components/sage_radio.rb
@@ -1,15 +1,18 @@
 class SageRadio < SageComponent
   set_attribute_schema({
     attributes: [:optional, Hash],
+    caption: [:optional, String],
     checked: [:optional, TrueClass],
     css_classes: [:optional, String],
     disabled: [:optional, TrueClass],
+    has_border: [:optional, TrueClass],
     has_error: [:optional, TrueClass],
     id: String,
     label_text: String,
     name: String,
     required: [:optional, TrueClass],
     standalone: [:optional, TrueClass],
+    toggle_position: [:optional, String],
     value: String,
   })
 end
