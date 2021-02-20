d0507afe113cee010fa6d6e861dce82af9fd2835
Kevin Chang <kevin@kajabi.com>
2021-02-19 18:13:36 -0800

    feat(input group): apply JS for browser control button overlap

--- packages/sage-system/lib/inputgroup.js
+++ packages/sage-system/lib/inputgroup.js
@@ -3,9 +3,10 @@ Sage.inputgroup = (function() {
   // ==================================================
   // Functions
   // ==================================================
+  const inputPaddingOffset = 16;
 
   function togglePasswordDisplay(evt) {
-    var parentEle = evt.target.parentElement,
+    const parentEle = evt.target.parentElement,
        field = parentEle.querySelector(".sage-input__field"),
        activeClassName = "sage-input-group--visible";
 
@@ -21,8 +22,22 @@ Sage.inputgroup = (function() {
   }
 
 
+  function addButtonPadding() {
+    const inputGroupBtns = Sage.util.nodelistToArray(document.querySelectorAll(".sage-input-group__button"));
+
+    inputGroupBtns.forEach(function(btn) {
+      const parentGroup = btn.closest(".sage-input-group");
+      const field = parentGroup.querySelector(".sage-input__field");
+
+      field.style.paddingRight = `${btn.offsetWidth + inputPaddingOffset }px`;
+
+      console.info("field", field.style.paddingRight);
+    });
+  }
+
+
   function bindPWEvents() {
-    var pwShowBtn = Sage.util.nodelistToArray(document.querySelectorAll("[data-js-mask='password']"));
+    const pwShowBtn = Sage.util.nodelistToArray(document.querySelectorAll("[data-js-mask='password']"));
 
     // show/hide password text; assumes multiple password fields
     pwShowBtn.forEach(function(btn) {
@@ -34,6 +49,10 @@ Sage.inputgroup = (function() {
 
 
   function init() {
+    if (document.querySelector(".sage-input-group__button").length !== null) {
+      addButtonPadding();
+    }
+
     if (document.querySelector(".sage-input-group__toggle").length !== null) {
       bindPWEvents();
     }
