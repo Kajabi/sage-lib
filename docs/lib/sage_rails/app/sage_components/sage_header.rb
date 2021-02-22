f549c20fd99f22264691e025ec5ee47447f6e5b1
Phil Schanely <philschanely@gmail.com>
2021-02-22 15:07:45 -0500

    fix: temp

--- docs/lib/sage_rails/app/sage_components/sage_header.rb
+++ docs/lib/sage_rails/app/sage_components/sage_header.rb
@@ -1,2 +1,14 @@
 class SageHeader < SageComponent
+  set_attribute_schema({
+    branding: [{
+      text: String,
+      hide_text: [:optional, TrueClass],
+      logo: [:optional, {
+        path: String,
+        method: [:optional, NilClass, Set.new(["pack"])],
+      }]
+    }],
+    center_body: [:optional, TrueClass],
+    menu_button_controls: [:optional, NilClass, String],
+  })
 end
