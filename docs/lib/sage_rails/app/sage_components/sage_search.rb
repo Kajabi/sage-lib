b93a94053e45d962bb5d632d3829ca7acbb01045
Quinton Jason <quinton.jason@gmail.com>
2021-02-22 10:58:54 -0600

    fix(docs): updated to element docs

--- docs/lib/sage_rails/app/sage_components/sage_search.rb
+++ docs/lib/sage_rails/app/sage_components/sage_search.rb
@@ -1,9 +1,9 @@
 class SageSearch < SageComponent
   set_attribute_schema({
-    id: [:optional, String],
-    value: [:optional, String, NilClass],
-    placeholder: [:optional, String],
     contained: [:optional, TrueClass],
+    id: [:optional, String],
     label_text: [:optional, String],
+    placeholder: [:optional, String],
+    value: [:optional, String, NilClass],
   })
 end
