af335c84cfa3eb7922c0b1466e7377b559195217
Kevin Chang <kevin@kajabi.com>
2021-02-23 10:22:55 -0800

    refactor(input group): refactor component props using hash structure per review feedback

--- docs/lib/sage_rails/app/sage_components/sage_input_group.rb
+++ docs/lib/sage_rails/app/sage_components/sage_input_group.rb
@@ -1,17 +1,19 @@
 class SageInputGroup < SageComponent
   set_attribute_schema({
     disabled: [:optional, TrueClass],
-    group_button_display_on_focus: [:optional, TrueClass],
-    group_button_icon: [:optional, String],
-    group_button_text: [:optional, String],
-    group_button_text_hidden: [:optional, TrueClass],
-    group_button_tooltip: [:optional, TrueClass],
-    group_button_tooltip_position: [:optional, String, Set.new(["top", "right", "bottom", "left"])],
-    group_button_tooltip_size: [:optional, String, Set.new(["small", "large"])],
-    group_button_tooltip_text: [:optional, String],
-    group_button_type: [:optional, String],
+    group_buttons: [:optional, [[{
+      display_on_focus: [:optional, TrueClass],
+      icon: [:optional, String],
+      text: [:optional, String],
+      text_hidden: [:optional, TrueClass],
+      tooltip: [:optional, TrueClass],
+      tooltip_position: [:optional, String, Set.new(["top", "right", "bottom", "left"])],
+      tooltip_size: [:optional, String, Set.new(["small", "large"])],
+      tooltip_text: [:optional, String],
+    }]]],
     group_id: [:optional, String],
     has_button: [:optional, TrueClass],
     has_hint: [:optional, TrueClass],
+    input_type: [:optional, String],
   })
 end
