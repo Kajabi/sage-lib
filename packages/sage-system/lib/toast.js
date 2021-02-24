aa907c82090eff67cc19db37d7d29a3b2920d9db
Andrew McIntee <contact@andrew.mn>
2021-02-24 14:01:12 -0500

    feat(rails toast): toast: Integrate a global container, animate out tooltip polish
    BUILD-792

--- packages/sage-system/lib/toast.js
+++ packages/sage-system/lib/toast.js
@@ -1,76 +1,113 @@
 Sage.toast = (function () {
+
   // ==================================================
   // Variables
   // ==================================================
-  const DATA_ATTR = "data-js-toast";
-  const TOAST_CLASS = "sage-toast";
-  const SELECTOR_TOAST_CLOSE = "[data-js-toast-close]";
+
+  const APP_TOAST_CONTAINER_ID = 'SageToastContainer';
+  const DEFAULT_CONFIG = {
+    icon: 'check',
+    type: 'notice',
+    timer: 4000,
+  };
+
+  const DATA_ATTR = 'data-js-toast';
+  const DATA_ATTR_CLOSE_BUTTON = 'data-js-toast-close';
+  const TOAST_CLASS = 'sage-toast';
+  const TOAST_CLASS_REMOVE = 'sage-toast--animate-out';
+
+  // ==================================================
+  // Template
+  // ==================================================
+
+  const toastTemplate = ({id, type, icon, message}) => (`
+    <dialog
+      class="${TOAST_CLASS} sage-toast--style-${type}"
+      ${DATA_ATTR}="${id}"
+    >
+      <i class="sage-toast__icon sage-icon-${icon}"></i>
+      <output
+        class="sage-toast__value"
+        aria-live="assertive"
+        aria-atomic="true"
+      >
+        ${message}
+      </output>
+      <button
+        class="sage-toast__close sage-btn sage-btn--subtle sage-btn--secondary sage-btn--icon-only-remove"
+        type="button"
+        ${DATA_ATTR_CLOSE_BUTTON}
+      >
+        <span class="visually-hidden">Close</span>
+      </button
+    </dialog>
+  `);
 
   // ==================================================
   // Functions
   // ==================================================
 
   function init(el) {
-    el.querySelector(SELECTOR_TOAST_CLOSE).addEventListener("click", handleCloseToast);
+    el.querySelector(`[${DATA_ATTR_CLOSE_BUTTON}]`).addEventListener('click', handleCloseToast);
   }
 
   function unbind(el) {
-    el.querySelector(SELECTOR_TOAST_CLOSE).removeEventListener("click", handleCloseToast);
+    el.querySelector(`[${DATA_ATTR_CLOSE_BUTTON}]`).removeEventListener('click', handleCloseToast);
   }
 
   function trigger(args) {
+    const config = {
+      id: Sage.util.generateId(),
+      ...DEFAULT_CONFIG,
+      ...args,
+    };
 
-    const defaultConfig = {
-      icon: "check",
-      type: "notice",
-    }
+    const elToast = Sage.util.stringToHtmlFragment(toastTemplate(config));
+    getAppToastContainer().appendChild(elToast);
+    setToastTimer(config)
 
-    const config = Object.assign(defaultConfig, args)
-
-    let toast = document.createElement("dialog");
-
-    toast.setAttribute(DATA_ATTR, "");
-    toast.className = TOAST_CLASS;
-    toast.classList.add('sage-toast--style-'+ config.type);
-    toast.innerHTML = `
-    <i class="sage-toast__icon sage-icon-${config.icon}"></i>
-    <output class="sage-toast__value" aria-live="assertive" aria-atomic="true">${config.message}</output>
-    <button type="button" data-js-toast-close class="sage-toast__close sage-btn sage-btn--subtle sage-btn--secondary sage-btn--icon-only-remove"><span class="visually-hidden">Close</span></button>`;
-
-    document.body.appendChild(toast);
-    setToastTimer(toast, config)
+    return config.id;
   }
 
-  // sets default or customer timer
-  function setToastTimer(toast, config) {
-    if (config.timer) {
-      setTimeout(() => {
-        removeToast(toast);
-      }, config.timer);
-    } else {
-      setTimeout(() => {
-        removeToast(toast)
-      }, 4000);
-    }
-  }
+  function dismiss(toastId) {
+    const elToast = document.querySelector(`[${DATA_ATTR}='${toastId}']`);
+    if (!elToast) return false;
 
-  // removes toast when timer is up
-  function removeToast(toast) {
-    window.requestAnimationFrame(function () {
-      toast.remove();
-      clearTimeout(toast);
+    window.requestAnimationFrame(() => {
+      elToast.classList.add(TOAST_CLASS_REMOVE);
     });
+
+    // Remove DOM node on CSS close transition completion
+    elToast.addEventListener('transitionend', elToast.remove, { once: true });
+
+    return true;
   }
 
-  // handles click on toast close btn
+  // Get global app container OR create if not present
+  function getAppToastContainer() {
+    let elAppToastContainer = document.getElementById(APP_TOAST_CONTAINER_ID);
+
+    if (!elAppToastContainer) {
+      elAppToastContainer = document.createElement('div');
+      elAppToastContainer.setAttribute('id', APP_TOAST_CONTAINER_ID);
+      document.body.appendChild(elAppToastContainer);
+    }
+
+    return elAppToastContainer;
+  }
+
+  function setToastTimer(config) {
+    if (!config.timer) return;
+    setTimeout(() => dismiss(config.id), config.timer);
+  }
+
   function handleCloseToast(evt) {
-    window.requestAnimationFrame(function () {
-      document.body.removeChild(evt.target.parentElement);
-    });
+    dismiss(evt.target.parentElement.getAttribute(DATA_ATTR));
   }
 
   return {
     trigger: trigger,
+    dismiss: dismiss,
     init: init,
     unbind: unbind,
   };
