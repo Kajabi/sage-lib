97fdc4c25062054935cfc8de77bd36f1022c0820
Quinton Jason <quinton.jason@gmail.com>
2021-02-24 07:00:50 -0600

    fix(docs): undesired save point due to local sage issue

--- packages/sage-react/lib/HelpLink/HelpLink.story.jsx
+++ packages/sage-react/lib/HelpLink/HelpLink.story.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
-import { centerXY } from '../story-support/decorators';
 import { HelpLink } from './HelpLink';
 
-storiesOf('Sage/HelpLink', module)
-  .addDecorator(withKnobs)
-  .addDecorator(centerXY)
-  .add('Default', () => (
-    <HelpLink href="http://example.com" target="_blank" referrer="no-referrer" labelIsVisible={boolean('Show label', false)}>
-      {text('Text', 'Learn something!')}
-    </HelpLink>
-  ));
+export default {
+  title: 'Sage/HelpLink',
+  component: HelpLink,
+};
+const Template = (args) => <HelpLink target="_blank" referrer="no-referrer" {...args} />;
+
+// Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = {
+  labelIsVisible: false,
+  text: 'Learn something'
+};
+
+// storiesOf('Sage/HelpLink', module)
+//   .addDecorator(withKnobs)
+//   .addDecorator(centerXY)
+//   .add('Default', () => (
+//     <HelpLink href="http://example.com" target="_blank" referrer="no-referrer" labelIsVisible={boolean('Show label', false)}>
+//       {text('Text', 'Learn something!')}
+//     </HelpLink>
+//   ));
--- packages/sage-react/lib/Popover/Popover.story.jsx
+++ packages/sage-react/lib/Popover/Popover.story.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
-import { centerXY } from '../story-support/decorators';
-import { SageTokens } from '../configs';
 import { Popover } from './Popover';
 
-storiesOf('Sage/Popover', module)
-  .addDecorator(withKnobs)
-  .addDecorator(centerXY)
-  .add('Default', () => (
-    <Popover
-      label={text('Trigger Label', 'Learn more')}
-      icon={select('Trigger icon', SageTokens.ICONS, SageTokens.ICONS.QUESTION_CIRCLE)}
-      iconOnly={boolean('Icon only trigger', true)}
-      moreLinkURL="//example.com"
-      title="Amazing popover"
-    >
-      <p>Testing...</p>
-    </Popover>
-  ));
+export default {
+  title: 'Sage/Popover',
+  component: Popover,
+};
+const Template = (args) => <Popover {...args} />;
+
+// Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Learn more',
+  icon: 'add',
+  iconOnly: true,
+  moreLinkURL: '//example.com',
+  title: 'Amazing popover'
+};
+
+// storiesOf('Sage/Popover', module)
+//   .addDecorator(withKnobs)
+//   .addDecorator(centerXY)
+//   .add('Default', () => (
+//     <Popover
+//       label={text('Trigger Label', 'Learn more')}
+//       icon={select('Trigger icon', SageTokens.ICONS, SageTokens.ICONS.QUESTION_CIRCLE)}
+//       iconOnly={boolean('Icon only trigger', true)}
+//       moreLinkURL="//example.com"
+//       title="Amazing popover"
+//     >
+//       <p>Testing...</p>
+//     </Popover>
+//   ));
