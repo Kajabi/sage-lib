bf49ce47ac636596ebeac62b972c89ba7d3870f5
Quinton Jason <quinton.jason@gmail.com>
2021-02-25 15:09:19 -0600

    fix(docs): updated to popover, icon, iconcard, helplink react docs

--- packages/sage-react/lib/Icon/Icon.story.jsx
+++ packages/sage-react/lib/Icon/Icon.story.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { selectArgs } from '../story-support/helpers';
+import { SageTokens } from '../configs';
 import { Icon } from './Icon';
 
 export default {
   title: 'Sage/Icon',
   component: Icon,
+  argTypes: {
+    ...selectArgs({
+      color: Icon.COLORS,
+      icon: SageTokens.ICONS,
+      size: Icon.SIZES
+    }),
+  },
+  args: {
+    color: Icon.COLORS.CHARCOAL,
+    icon: Icon.ICONS.CHECK_CIRCLE,
+    label: 'Click me',
+    size: Icon.SIZES.MD
+  }
 };
 const Template = (args) => <Icon {...args} />;
 
-// Each story then reuses that template
 export const Default = Template.bind({});
-Default.args = {
-  color: Icon.COLORS.CHARCOAL,
-  icon: Icon.ICONS.CHECK_CIRCLE,
-  label: 'Click me',
-  size: Icon.SIZES.MD
-};
--- packages/sage-react/lib/Popover/Popover.story.jsx
+++ packages/sage-react/lib/Popover/Popover.story.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
+import { selectArgs } from '../story-support/helpers';
+import { SageTokens } from '../configs';
 import { Popover } from './Popover';
 
 export default {
   title: 'Sage/Popover',
   component: Popover,
+  decorators:  [(Story) => <div style={{ minHeight: 150 }}><Story/></div>],
+  argTypes: {
+    ...selectArgs({
+      icon: SageTokens.ICONS,
+    }),
+  },
+  args: {
+    label: 'Learn more',
+    icon: SageTokens.ICONS.QUESTION_CIRCLE,
+    iconOnly: true,
+    moreLinkURL: '//example.com',
+    title: 'Amazing popover'
+  }
 };
 const Template = (args) => <Popover {...args} />;
 
-// Each story then reuses that template
 export const Default = Template.bind({});
-Default.args = {
-  label: 'Learn more',
-  icon: 'add',
-  iconOnly: true,
-  moreLinkURL: '//example.com',
-  title: 'Amazing popover'
-};
-
-// storiesOf('Sage/Popover', module)
-//   .addDecorator(withKnobs)
-//   .addDecorator(centerXY)
-//   .add('Default', () => (
-//     <Popover
-//       label={text('Trigger Label', 'Learn more')}
-//       icon={select('Trigger icon', SageTokens.ICONS, SageTokens.ICONS.QUESTION_CIRCLE)}
-//       iconOnly={boolean('Icon only trigger', true)}
-//       moreLinkURL="//example.com"
-//       title="Amazing popover"
-//     >
-//       <p>Testing...</p>
-//     </Popover>
-//   ));
