3da93a4625e996301218e27b8b032c19d89ead03
Quinton Jason <quinton.jason@gmail.com>
2021-02-25 15:09:19 -0600

    fix(docs): updated to popover, icon, iconcard, helplink react docs

--- packages/sage-react/lib/HelpLink/HelpLink.story.jsx
+++ packages/sage-react/lib/HelpLink/HelpLink.story.jsx
@@ -4,21 +4,11 @@ import { HelpLink } from './HelpLink';
 export default {
   title: 'Sage/HelpLink',
   component: HelpLink,
+  args: {
+    labelIsVisible: false,
+    text: 'Learn something'
+  }
 };
-const Template = (args) => <HelpLink target="_blank" referrer="no-referrer" {...args} />;
+const Template = (args) => <HelpLink href="http://example.com" target="_blank" referrer="no-referrer" {...args} />;
 
-// Each story then reuses that template
 export const Default = Template.bind({});
-Default.args = {
-  labelIsVisible: false,
-  text: 'Learn something'
-};
-
-// storiesOf('Sage/HelpLink', module)
-//   .addDecorator(withKnobs)
-//   .addDecorator(centerXY)
-//   .add('Default', () => (
-//     <HelpLink href="http://example.com" target="_blank" referrer="no-referrer" labelIsVisible={boolean('Show label', false)}>
-//       {text('Text', 'Learn something!')}
-//     </HelpLink>
-//   ));
--- packages/sage-react/lib/IconCard/IconCard.story.jsx
+++ packages/sage-react/lib/IconCard/IconCard.story.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import { withKnobs, select } from '@storybook/addon-knobs';
-// import { centerXY } from '../story-support/decorators';
+import { selectArgs } from '../story-support/helpers';
+import { SageTokens } from '../configs';
 import { IconCard } from './IconCard';
 
 export default {
   title: 'Sage/IconCard',
   component: IconCard,
-  decorators: [(Story) => <div style={{ marginTop: 50 }}><Story/></div>]
+  decorators:  [(Story) => <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}><Story/></div>],
+  argTypes: {
+    ...selectArgs({
+      color: IconCard.COLORS,
+      icon: SageTokens.ICONS,
+      size: IconCard.SIZES
+    }),
+  },
+  args: {
+    color: IconCard.COLORS.DRAFT,
+    icon: IconCard.ICONS.CHECK_CIRCLE,
+    size: IconCard.SIZES.MD
+  }
 };
 const Template = (args) => <IconCard {...args} />;
 
-// Each story then reuses that template
 export const Default = Template.bind({});
-Default.args = {
-  color: IconCard.COLORS.DRAFT,
-  icon: IconCard.ICONS.CHECK_CIRCLE,
-  size: IconCard.SIZES
-};
-
-// storiesOf('Sage/Icon Card', module)
-//   .addDecorator(withKnobs)
-//   .addDecorator(centerXY)
-//   .add('Default', () => (
-//     <div style={{ marginTop: 50 }}>
-//       <IconCard
-//         color={select('Color', IconCard.COLORS, IconCard.COLORS.DRAFT)}
-//         icon={select('Icon', IconCard.ICONS, IconCard.ICONS.CHECK_CIRCLE)}
-//         size={select('Size', IconCard.SIZES)}
-//       />
-//     </div>
-//   ));
