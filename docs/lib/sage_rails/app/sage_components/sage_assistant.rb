8d30440834679c0b29b9b74ce8ab2ee313ef28a0
Phil Schanely <philschanely@gmail.com>
2021-03-01 11:03:51 -0500

    fix(assistant): improve with use of sections

--- docs/lib/sage_rails/app/sage_components/sage_assistant.rb
+++ docs/lib/sage_rails/app/sage_components/sage_assistant.rb
@@ -7,4 +7,8 @@ class SageAssistant < SageComponent
     menu_button: [:optional, TrueClass],
     menu_button_controls: [:optional, String],
   })
+
+  def sections
+    %w(:assistant_actions)
+  end
 end
