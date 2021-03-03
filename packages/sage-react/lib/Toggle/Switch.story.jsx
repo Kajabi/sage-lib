ca4c3df94e7faf61dac363b513692ab6ea7e1bd6
Quinton Jason <quinton.jason@gmail.com>
2021-03-03 08:44:35 -0600

    fix(docs): incorrect file committed

--- packages/sage-react/lib/Toggle/Switch.story.jsx
+++ packages/sage-react/lib/Toggle/Switch.story.jsx
@@ -4,14 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean, text, radios } from '@storybook/addon-knobs';
 import { centerXY } from '../story-support/decorators';
 import { Switch } from './Switch';
-import { Toggle } from './Toggle';
 
-export default {
-  title: 'Sage/Switch',
-  component: Switch,
-};
-
-export const Default = () => {
+const SwitchWithState = () => {
   const [on, toggleOn] = useState(false);
   return (
     <div>
@@ -31,7 +25,7 @@ export const Default = () => {
   );
 };
 
-export const MultiplesExample = (type) => {
+const SwitchesWithState = ({ type }) => {
   const items = [
     {
       label: 'Option 1',
@@ -85,97 +79,16 @@ export const MultiplesExample = (type) => {
   );
 };
 
-MultiplesExample.args = {
-  type: "radio"
+SwitchesWithState.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
-
-
-
-// const SwitchWithState = () => {
-//   const [on, toggleOn] = useState(false);
-//   return (
-//     <div>
-//       <Switch
-//         checked={on}
-//         disabled={boolean('Disabled', false)}
-//         hasError={boolean('Has Error', false)}
-//         id="switch-demo"
-//         label={text('Label', 'Switch label')}
-//         message={text('Message', 'Subtext appears')}
-//         name="switch-demo"
-//         onChange={(val, isOn) => toggleOn(!isOn)}
-//         type={radios('Type', ['radio', 'checkbox'], 'radio')}
-//         value="Demo"
-//       />
-//     </div>
-//   );
-// };
-
-// const SwitchesWithState = ({ type }) => {
-//   const items = [
-//     {
-//       label: 'Option 1',
-//       value: 'option-1',
-//       checked: false,
-//     }, {
-//       label: 'Option 2',
-//       value: 'option-2',
-//       checked: false,
-//     }, {
-//       label: 'Option 3',
-//       value: 'option-3',
-//       checked: false,
-//     }, {
-//       label: 'Option 4',
-//       value: 'option-4',
-//       checked: false,
-//     }
-//   ];
-
-//   const [itemState, changeItemState] = useState(items);
-
-//   const onChange = (newValue) => {
-//     const stateItems = itemState.map(({ label, value, checked }) => {
-//       if (newValue === value) {
-//         checked = type === 'radio' ? true : !checked;
-//       } else if (type === 'radio') {
-//         checked = false;
-//       }
-
-//       return { label, value, checked };
-//     });
-
-//     changeItemState(stateItems);
-//   };
-
-//   return (
-//     <ul className="sage-list">
-//       {itemState.map((configs, i) => (
-//         <Switch
-//           id={`switch-${i}`}
-//           itemInList={true}
-//           key={i.toString()}
-//           name="group-1"
-//           onChange={onChange}
-//           type={type}
-//           {...configs}
-//         />
-//       ))}
-//     </ul>
-//   );
-// };
-
-// SwitchesWithState.propTypes = {
-//   type: PropTypes.string.isRequired,
-// };
-
-// storiesOf('Sage/Switch', module)
-//   .addDecorator(centerXY)
-//   .addDecorator(withKnobs)
-//   .add('Default', () => (
-//     <SwitchWithState />
-//   ))
-//   .add('Multiples example', () => (
-//     <SwitchesWithState type={radios('Type', ['radio', 'checkbox'], 'radio')} />
-//   ));
+storiesOf('Sage/Switch', module)
+  .addDecorator(centerXY)
+  .addDecorator(withKnobs)
+  .add('Default', () => (
+    <SwitchWithState />
+  ))
+  .add('Multiples example', () => (
+    <SwitchesWithState type={radios('Type', ['radio', 'checkbox'], 'radio')} />
+  ));
