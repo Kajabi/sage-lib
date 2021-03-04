82db5a1e91c04282981cedfea1ba304e89990314
Quinton Jason <quinton.jason@gmail.com>
2021-03-04 17:30:13 -0600

    fix(docs): updated sortable story

--- packages/sage-react/lib/Sortable/Sortable.story.jsx
+++ packages/sage-react/lib/Sortable/Sortable.story.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { selectArgs } from '../story-support/helpers';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs';
 import { centerXY } from '../story-support/decorators';
@@ -6,8 +7,17 @@ import { Button } from '../Button';
 import { Grid } from '../Grid';
 import { SageTokens } from '../configs';
 import { Sortable } from './Sortable';
+import { SortableItem } from './SortableItem';
 
-const SortableWithState = () => {
+export default {
+  title: 'Sage/Sortable',
+  component: Sortable,
+  subcomponents: {
+    'Sortable.Item': Sortable.Item
+  }
+};
+
+export const Default = (args) => {
   const [state, setState] = useState([
     {
       id: '1',
@@ -65,7 +75,7 @@ const SortableWithState = () => {
       title={item.title}
       subtitle={item.subtitle}
       actionItems={item.actionItems}
-      type={select('Sortable Item Type', Sortable.Item.TYPES, Sortable.Item.TYPES.DEFAULT)}
+      type={args.type}
     />
   );
 
@@ -80,8 +90,92 @@ const SortableWithState = () => {
     </Grid>
   );
 };
+Default.args = {
+  type: Sortable.Item.TYPES.CARD
+};
+Default.argTypes = {
+  ...selectArgs({
+    type: Sortable.Item.TYPES
+  }),
+};
 
-storiesOf('Sage/Sortable', module)
-  .addDecorator(withKnobs)
-  .addDecorator(centerXY)
-  .add('Controlled Component Demo', () => <SortableWithState />);
+
+
+// const SortableWithState = () => {
+//   const [state, setState] = useState([
+//     {
+//       id: '1',
+//       title: 'Title 1',
+//       subtitle: 'Subtitle',
+//       actionItems: [
+//         <Button
+//           onClick={() => {}}
+//           color={Button.COLORS.SECONDARY}
+//           subtle={true}
+//           icon={SageTokens.ICONS.REMOVE}
+//           iconOnly={true}
+//         >
+//           Delete
+//         </Button>
+//       ],
+//     },
+//     {
+//       id: '2',
+//       title: 'Title 2',
+//       subtitle: 'Subtitle',
+//       actionItems: [
+//         <Button
+//           onClick={() => {}}
+//           color={Button.COLORS.SECONDARY}
+//           subtle={true}
+//           icon={SageTokens.ICONS.REMOVE}
+//           iconOnly={true}
+//         >
+//           Delete
+//         </Button>
+//       ],
+//     },
+//     {
+//       id: '3',
+//       title: 'Title 3',
+//       subtitle: 'Subtitle',
+//       actionItems: [
+//         <Button
+//           onClick={() => {}}
+//           color={Button.COLORS.SECONDARY}
+//           subtle={true}
+//           icon={SageTokens.ICONS.REMOVE}
+//           iconOnly={true}
+//         >
+//           Delete
+//         </Button>
+//       ],
+//     },
+//   ]);
+
+//   const renderItem = (item) => (
+//     <Sortable.Item
+//       key={item.id}
+//       title={item.title}
+//       subtitle={item.subtitle}
+//       actionItems={item.actionItems}
+//       type={select('Sortable Item Type', Sortable.Item.TYPES, Sortable.Item.TYPES.DEFAULT)}
+//     />
+//   );
+
+//   return (
+//     <Grid container={Grid.CONTAINER_SIZES.XS}>
+//       <Sortable
+//         list={state}
+//         setList={setState}
+//         onEnd={() => {}}
+//         renderItem={renderItem}
+//       />
+//     </Grid>
+//   );
+// };
+
+// storiesOf('Sage/Sortable', module)
+//   .addDecorator(withKnobs)
+//   .addDecorator(centerXY)
+//   .add('Controlled Component Demo', () => <SortableWithState />);
