e2280914ef6d5ebe762b70e8666e8ab61f46b5ba
Quinton Jason <quinton.jason@gmail.com>
2021-03-04 15:14:38 -0600

    fix(dropdown): updated dropdown story

--- packages/sage-react/lib/Dropdown/Dropdown.story.jsx
+++ packages/sage-react/lib/Dropdown/Dropdown.story.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withKnobs, select, text, boolean } from '@storybook/addon-knobs';
+import { selectArgs } from '../story-support/helpers';
 import { Button } from '../Button';
 import { FormSection } from '../FormSection';
 import { Input } from '../Input';
 import { SageTokens } from '../configs';
-import { centerXY } from '../story-support/decorators';
 import { Dropdown } from './Dropdown';
 import { OptionsDropdown } from './OptionsDropdown';
 import { defaultOptionsItems, sampleMenuItems } from './stories/story-helper';
@@ -13,52 +11,103 @@ import { MultiMenuStory } from './stories/MultiMenuStory';
 import { SelectDropdownDemo } from './stories/SelectDropdownDemo';
 import { BulkActionsStory } from './stories/BulkActionsStory';
 
-storiesOf('Sage/Dropdown', module)
-  .addDecorator(withKnobs)
-  .addDecorator(centerXY)
-  .add('Default', () => (
-    <Dropdown
-      label={text('Label', 'Feature')}
-      icon={select('Icon', SageTokens.ICONS, SageTokens.ICONS.GEAR)}
-      isDisabled={boolean('Disabled?', false)}
-      isLabelVisible={boolean('Is Label visible?', true)}
-      exitPanelHandler={(data) => {
-        if (data.handler) {
-          data.handler();
-        }
+export default {
+  title: 'Sage/Dropdown',
+  component: Dropdown,
+  decorators: [(Story) => <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}><Story /></div>],
+  subcomponents: {
+    'Dropdown.ItemList': Dropdown.ItemList
+  },
+  argTypes: {
+    ...selectArgs({
+      icon: SageTokens.ICONS,
+      panelSize: Dropdown.PANEL_SIZES
+    }),
+  },
+  args: {
+    align: null,
+    className: false,
+    closePanelOnExit: true,
+    contained: false,
+    customized: false,
+    disabled: false,
+    icon: null,
+    isLabelVisible: true,
+    isPinned: false,
+    label: 'Switch label',
+    panelMaxWidth: null,
+    panelModifier: Dropdown.PANEL_SIZES.DEFAULT,
+    triggerButtonSubtle: false
+  }
+};
+
+export const Default = (args) => (
+  <Dropdown
+    label={args.label}
+    icon={args.icon}
+    isDisabled={args.isDisabled}
+    isLabelVisible={args.isLabelVisible}
+    exitPanelHandler={(data) => {
+      if (data.handler) {
+        data.handler();
+      }
+    }}
+  >
+    <Dropdown.ItemList items={sampleMenuItems} />
+  </Dropdown>
+);
+Default.args = {
+  icon: SageTokens.ICONS.GEAR,
+  isDisabled: false,
+  isLabelVisible: true,
+  label: 'Feature',
+};
+
+export const OptionMenu = (args) => (
+  <OptionsDropdown options={defaultOptionsItems} isPinned={args.isPinned} />
+);
+OptionMenu.args = {
+  isPinned: false
+};
+
+export const DropdownMenuWithHeadings = () => (
+  <BulkActionsStory />
+);
+
+export const Select = () => (
+  <SelectDropdownDemo />
+);
+
+export const Multiselect = () => (
+  <MultiMenuStory />
+);
+
+export const MenuWithCustomPanel = (args) => (
+  <Dropdown
+    align={args.align}
+    label={args.label}
+    icon={args.icon}
+    isLabelVisible={args.isLabelVisible}
+  >
+    <FormSection
+      style={{
+        width: '500px',
+        padding: '24px'
       }}
+      title="Sign in"
+      subtitle="You must sign in order to use this feature."
     >
-      <Dropdown.ItemList items={sampleMenuItems} />
-    </Dropdown>
-  ))
-  .add('Option menu', () => (
-    <OptionsDropdown options={defaultOptionsItems} isPinned={false} />
-  ))
-  .add('Dropdown menu with headings', () => (
-    <BulkActionsStory />
-  ))
-  .add('Select', () => <SelectDropdownDemo />)
-  .add('Multiselect', () => <MultiMenuStory />)
-  .add('Menu with custom panel', () => (
-    <Dropdown
-      align="right"
-      label="Login"
-      icon={SageTokens.ICONS.USERS}
-      isLabelVisible={true}
-    >
-      <FormSection
-        style={{
-          width: '500px',
-          padding: '24px'
-        }}
-        title="Sign in"
-        subtitle="You must sign in order to use this feature."
-      >
-        <Input type="text" label="Username" />
-        <Input type="password" label="Password" />
-        <Button color={Button.COLORS.PRIMARY} alignEnd={true}>
-          Log in
-        </Button>
-      </FormSection>
-    </Dropdown>
-  ));
+      <Input type="text" label="Username" />
+      <Input type="password" label="Password" />
+      <Button color={Button.COLORS.PRIMARY} alignEnd={true}>
+        Log in
+      </Button>
+    </FormSection>
+  </Dropdown>
+);
+MenuWithCustomPanel.args = {
+  align: 'right',
+  icon: SageTokens.ICONS.USERS,
+  isLabelVisible: true,
+  label: 'Login',
+};
