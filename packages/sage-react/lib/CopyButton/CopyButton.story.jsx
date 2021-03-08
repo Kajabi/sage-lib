dd68bcf0fedb79dccfc2e6ee52e008ce84de1563
Quinton Jason <quinton.jason@gmail.com>
2021-03-08 08:53:29 -0600

    fix(docs): updated copyButton story

--- packages/sage-react/lib/CopyButton/CopyButton.story.jsx
+++ packages/sage-react/lib/CopyButton/CopyButton.story.jsx
@@ -4,11 +4,10 @@ import { CopyButton } from './CopyButton';
 export default {
   title: 'Sage/CopyButton',
   component: CopyButton,
+  args: {
+    children: 'https://www.example.com',
+  }
 };
-const Template = (args) => <CopyButton {...args} />;
 
-// Each story then reuses that template
+const Template = (args) => <CopyButton {...args} />;
 export const Default = Template.bind({});
-Default.args = {
-  children: 'https://www.example.com',
-};
