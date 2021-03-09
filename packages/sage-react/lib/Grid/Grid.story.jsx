ae8ebcf45a855b46ab0edbd69a37efa2dd6b02a0
Quinton Jason <quinton.jason@gmail.com>
2021-03-09 09:00:27 -0600

    fix(docs): updated grid story

--- packages/sage-react/lib/Grid/Grid.story.jsx
+++ packages/sage-react/lib/Grid/Grid.story.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { selectArgs } from '../story-support/helpers';
 import { Grid } from './Grid';
+import { Card } from '../Card';
 
 export default {
   title: 'Sage/Grid',
   component: Grid,
+  args: {
+    container: Grid.CONTAINER_SIZES.MD,
+    withRow: false
+  },
+  argTypes: {
+    ...selectArgs({
+      container: Grid.CONTAINER_SIZES
+    })
+  }
 };
 
 const GridDemo = ({ children }) => (
-  <div style={{
-    border: '1px solid lightgrey',
-    borderRadius: '4px',
-    padding: '16px',
-    width: '100%',
-    textAlign: 'center',
-  }}
-  >
+  <Card style={{ textAlign: 'center' }}>
     {children}
-  </div>
+  </Card>
 );
 
 GridDemo.propTypes = {
@@ -27,127 +30,116 @@ GridDemo.propTypes = {
 
 const columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-export const Default = (args) => (
-  <Grid container={args.container}>
-    <Grid.Row spacerBelow="sm">
-      {columns.map((col) => (
-        <Grid.Col size="1" key={col.toString()} aria-label="Single column">
+const Template = (args) => <Grid {...args}/>
+export const Default = Template.bind({});
+
+Default.args = {
+  children: (
+    <>
+      <Grid.Row spacerBelow="sm">
+        {columns.map((col) => (
+          <Grid.Col size="1" key={col.toString()} aria-label="Single column">
+            <GridDemo>
+              1 col
+            </GridDemo>
+          </Grid.Col>
+        ))}
+      </Grid.Row>
+      <Grid.Row spacerBelow="sm">
+        {columns.map((col) => col % 2 === 0 && (
+          <Grid.Col size="2" key={col.toString()}>
+            <GridDemo>
+              2 col
+            </GridDemo>
+          </Grid.Col>
+        ))}
+      </Grid.Row>
+      <Grid.Row spacerBelow="sm">
+        {columns.map((col) => col % 3 === 0 && (
+          <Grid.Col size="3" key={col.toString()}>
+            <GridDemo>
+              3 col
+            </GridDemo>
+          </Grid.Col>
+        ))}
+      </Grid.Row>
+      <Grid.Row spacerBelow="sm">
+        {columns.map((col) => col % 4 === 0 && (
+          <Grid.Col size="4" key={col.toString()}>
+            <GridDemo>
+              4 col
+            </GridDemo>
+          </Grid.Col>
+        ))}
+      </Grid.Row>
+      <Grid.Row spacerBelow="sm">
+        {columns.map((col) => col % 6 === 0 && (
+          <Grid.Col size="6" key={col.toString()}>
+            <GridDemo>
+              6 col
+            </GridDemo>
+          </Grid.Col>
+        ))}
+      </Grid.Row>
+      <Grid.Row spacerBelow="sm">
+        <Grid.Col size="7">
           <GridDemo>
-            1 col
+            7 col
           </GridDemo>
         </Grid.Col>
-      ))}
-    </Grid.Row>
-    <Grid.Row spacerBelow="sm">
-      {columns.map((col) => col % 2 === 0 && (
-        <Grid.Col size="2" key={col.toString()}>
+        <Grid.Col size="5">
           <GridDemo>
-            2 col
+            5 col
           </GridDemo>
         </Grid.Col>
-      ))}
-    </Grid.Row>
-    <Grid.Row spacerBelow="sm">
-      {columns.map((col) => col % 3 === 0 && (
-        <Grid.Col size="3" key={col.toString()}>
+      </Grid.Row>
+      <Grid.Row spacerBelow="sm">
+        <Grid.Col size="8">
           <GridDemo>
-            3 col
+            8 col
           </GridDemo>
         </Grid.Col>
-      ))}
-    </Grid.Row>
-    <Grid.Row spacerBelow="sm">
-      {columns.map((col) => col % 4 === 0 && (
-        <Grid.Col size="4" key={col.toString()}>
+        <Grid.Col size="4">
           <GridDemo>
             4 col
           </GridDemo>
         </Grid.Col>
-      ))}
-    </Grid.Row>
-    <Grid.Row spacerBelow="sm">
-      {columns.map((col) => col % 6 === 0 && (
-        <Grid.Col size="6" key={col.toString()}>
+      </Grid.Row>
+      <Grid.Row spacerBelow="sm">
+        <Grid.Col size="9">
           <GridDemo>
-            6 col
+            9 col
           </GridDemo>
         </Grid.Col>
-      ))}
-    </Grid.Row>
-    <Grid.Row spacerBelow="sm">
-      <Grid.Col size="7">
-        <GridDemo>
-          7 col
-        </GridDemo>
-      </Grid.Col>
-      <Grid.Col size="5">
-        <GridDemo>
-          5 col
-        </GridDemo>
-      </Grid.Col>
-    </Grid.Row>
-    <Grid.Row spacerBelow="sm">
-      <Grid.Col size="8">
-        <GridDemo>
-          8 col
-        </GridDemo>
-      </Grid.Col>
-      <Grid.Col size="4">
-        <GridDemo>
-          4 col
-        </GridDemo>
-      </Grid.Col>
-    </Grid.Row>
-    <Grid.Row spacerBelow="sm">
-      <Grid.Col size="9">
-        <GridDemo>
-          9 col
-        </GridDemo>
-      </Grid.Col>
-      <Grid.Col size="3">
-        <GridDemo>
-          3 col
-        </GridDemo>
-      </Grid.Col>
-    </Grid.Row>
-    <Grid.Row>
-      <Grid.Col size="10">
-        <GridDemo>
-          10 col
-        </GridDemo>
-      </Grid.Col>
-      <Grid.Col size="2">
-        <GridDemo>
-          2 col
-        </GridDemo>
-      </Grid.Col>
-    </Grid.Row>
-  </Grid>
-);
-
-Default.args = {
-  className: null,
-  container: Grid.CONTAINER_SIZES.NONE,
-  withRow: false,
-};
-Default.argTypes = {
-  ...selectArgs({
-    container: Grid.CONTAINER_SIZES
-  }),
+        <Grid.Col size="3">
+          <GridDemo>
+            3 col
+          </GridDemo>
+        </Grid.Col>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Col size="10">
+          <GridDemo>
+            10 col
+          </GridDemo>
+        </Grid.Col>
+        <Grid.Col size="2">
+          <GridDemo>
+            2 col
+          </GridDemo>
+        </Grid.Col>
+      </Grid.Row>
+    </>
+  )
 };
 
-export const Containers = (args) => (
-  <Grid container={args.container}>
-    <GridDemo>
-      Empty Container
-    </GridDemo>
-  </Grid>
-);
+export const Containers = Template.bind({});
 Containers.args = {
-  container: Grid.CONTAINER_SIZES.NONE,
-};
-Containers.argTypes = {
-  ...selectArgs({
-    container: Grid.CONTAINER_SIZES
-  }),
+  children: (
+    <>
+      <GridDemo>
+        Empty Container
+      </GridDemo>
+    </>
+  )
 };
