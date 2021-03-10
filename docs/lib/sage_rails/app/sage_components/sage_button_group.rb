a512154e73b51bde7f6851ce67c766c7cebbcfbc
Quinton Jason <quinton.jason@gmail.com>
2021-03-10 16:54:59 -0600

    fix(button-group): added space-between modifier

--- docs/lib/sage_rails/app/sage_components/sage_button_group.rb
+++ docs/lib/sage_rails/app/sage_components/sage_button_group.rb
@@ -1,6 +1,6 @@
 class SageButtonGroup < SageComponent
   set_attribute_schema({
-    align: [:optional, Set.new(["end"])],
+    align: [:optional, Set.new(["space-between", "end"])],
     gap: [:optional, Set.new([:xs, :sm, :md, :lg])]
   })
 end
