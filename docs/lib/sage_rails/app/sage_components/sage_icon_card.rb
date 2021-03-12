5272b8bdcf7396ccfd357a6447ef7570b537af06
joedakroub <joe.d@kajabi.com>
2021-03-12 11:14:46 -0500

    feat(icon-card): added `locked` color

--- docs/lib/sage_rails/app/sage_components/sage_icon_card.rb
+++ docs/lib/sage_rails/app/sage_components/sage_icon_card.rb
@@ -2,7 +2,7 @@ class SageIconCard < SageComponent
   set_attribute_schema({
     attributes: [:optional, NilClass, Hash],
     background_color: [:optional, String],
-    color: [:optional, Set.new(["draft", "published", "info", "warning", "danger"])],
+    color: [:optional, Set.new(["draft", "published", "info", "locked", "warning", "danger"])],
     css_classes: [:optional, String],
     foreground_color: [:optional, String],
     icon: String,
