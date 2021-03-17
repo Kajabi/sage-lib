d21517d8551825acfa186e72d90e8188da95a745
Phil Schanely <philschanely@gmail.com>
2021-03-17 12:11:55 -0400

    feat(expandable-card): adjust padding, button setting, and sage type

--- docs/lib/sage_rails/app/sage_components/sage_expandable_card.rb
+++ docs/lib/sage_rails/app/sage_components/sage_expandable_card.rb
@@ -1,6 +1,7 @@
 class SageExpandableCard < SageComponent
   set_attribute_schema({
     body_bordered: [:optional, TrueClass],
+    sage_type: [:optional, TrueClass],
     trigger_label: [:optional, String],
   })
 end
