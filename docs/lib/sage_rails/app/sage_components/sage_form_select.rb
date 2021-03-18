6239417ae9673c2537cd9d02080f68e056d50c52
Phillip Lovelace <phillip@pixelflips.com>
2021-03-18 11:57:43 -0700

    revert(form select): reverts changes to form select

--- docs/lib/sage_rails/app/sage_components/sage_form_select.rb
+++ docs/lib/sage_rails/app/sage_components/sage_form_select.rb
@@ -3,7 +3,6 @@ class SageFormSelect < SageComponent
     has_error: [:optional, TrueClass],
     message: [:optional, String],
     name: [:optional, String],
-    css_classes: [:optional, String],
     select_options: [:optional, [[{
       text: String,
       value: [:optional, NilClass, String],
