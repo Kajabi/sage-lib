6492bc0bccf27287f6771a0531799b02082ccf83
Quinton Jason <quinton.jason@gmail.com>
2021-03-18 12:00:09 -0500

    fix(panel-controls): updated inner pagination

--- docs/lib/sage_rails/app/sage_components/sage_panel_controls.rb
+++ docs/lib/sage_rails/app/sage_components/sage_panel_controls.rb
@@ -5,6 +5,9 @@ class SagePanelControls < SageComponent
       value: String,
     }]]],
     item_count_label: [:optional, String],
+    # pagination: [:optional, SagePagination]
+    items: [:optional, -> (v) { SageSchemaHelper.can_paginate?(v) }],
+    pagination_additional_params: [:optional, Hash],
     show_bulk_actions: [:optional, TrueClass],
     show_checkboxes: [:optional, TrueClass],
     show_expand_collapse: [:optional, TrueClass],
@@ -21,6 +24,20 @@ class SagePanelControls < SageComponent
   def sections
     %w(panel_controls_toolbar panel_controls_tabs)
   end
+
+  # def page_count(collection)
+    
+  #   name = "Record"
+  #   entry_name = name.titleize.pluralize(collection.total_count)
+
+  #   if collection.total_pages < 2
+  #     "<strong>#{collection.total_count}</strong> #{entry_name}"
+  #   else
+  #     first = collection.offset_value + 1
+  #     last = collection.last_page? ? collection.total_count : collection.offset_value + collection.limit_value
+  #     "<strong>#{first}</strong> - <strong>#{last}</strong> of <strong>#{collection.total_count}</strong> #{entry_name}"
+  #   end.html_safe
+  # end
 end
 
 
@@ -29,6 +46,7 @@ end
  :show_expand_collapse=>true,
  :show_pagination=>true,
  :show_sort=>true,
+ :pagination_additional_params=>{},
  :bulk_action_items=>
   [{:value=>"Delete",
     :attributes=>{:href=>"#", :"data-js-list-action"=>"delete_selected"}},
