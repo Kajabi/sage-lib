5a8e358217b9f885efd5971f15a88943c75dc87d
Quinton Jason <quinton.jason@gmail.com>
2021-03-24 12:44:44 -0500

    fix(panel-controls): self review

--- docs/lib/sage_rails/app/sage_components/sage_panel_controls.rb
+++ docs/lib/sage_rails/app/sage_components/sage_panel_controls.rb
@@ -5,7 +5,6 @@ class SagePanelControls < SageComponent
       value: String,
     }]]],
     item_count_label: [:optional, String],
-    # pagination: [:optional, SagePagination]
     items: [:optional, -> (v) { SageSchemaHelper.can_paginate?(v) }],
     pagination_additional_params: [:optional, Hash],
     show_bulk_actions: [:optional, TrueClass],
@@ -24,20 +23,6 @@ class SagePanelControls < SageComponent
   def sections
     %w(panel_controls_pagination panel_controls_toolbar panel_controls_tabs)
   end
-
-  # def page_count(collection)
-    
-  #   name = "Record"
-  #   entry_name = name.titleize.pluralize(collection.total_count)
-
-  #   if collection.total_pages < 2
-  #     "<strong>#{collection.total_count}</strong> #{entry_name}"
-  #   else
-  #     first = collection.offset_value + 1
-  #     last = collection.last_page? ? collection.total_count : collection.offset_value + collection.limit_value
-  #     "<strong>#{first}</strong> - <strong>#{last}</strong> of <strong>#{collection.total_count}</strong> #{entry_name}"
-  #   end.html_safe
-  # end
 end
 
 
