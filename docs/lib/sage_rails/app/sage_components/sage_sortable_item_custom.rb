42e0febf7078448a42bb1ed656b741f1937f7b58
Quinton Jason <quinton.jason@gmail.com>
2021-04-02 12:48:05 -0500

    fix(sortable-item-custom): removed prop until followup

--- docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
+++ docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
@@ -1,6 +1,5 @@
 class SageSortableItemCustom < SageComponent
   set_attribute_schema({
-    icon: [:optional, String],
     id: [:optional, Integer, String],
     url_update: [:optional, String],
   })
