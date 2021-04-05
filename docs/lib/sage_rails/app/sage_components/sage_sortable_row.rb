115a88fc39f112b13d5fc829d5575437010a8ee8
Quinton Jason <quinton.jason@gmail.com>
2021-04-05 10:45:17 -0500

    fix(sortable-row): updated naming and added grid_template props

--- docs/lib/sage_rails/app/sage_components/sage_sortable_row.rb
+++ docs/lib/sage_rails/app/sage_components/sage_sortable_row.rb
@@ -1,5 +1,6 @@
-class SageSortableItemCustom < SageComponent
+class SageSortableRow < SageComponent
   set_attribute_schema({
+    grid_template: String,
     id: [:optional, Integer, String],
     url_update: [:optional, String],
   })
