33070047b6e535e603888413e18f705c12f7a7ef
Phillip Lovelace <phillip@pixelflips.com>
2021-04-26 12:37:00 -0700

    feat(transaction card): removes dropdown, adds optional href, and adds status text/colors (react)

--- packages/sage-react/lib/TransactionCard/TransactionCard.jsx
+++ packages/sage-react/lib/TransactionCard/TransactionCard.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Property } from '../Property';
+import { Link } from '../Link';
 import { Icon } from '../Icon';
 import { Label } from '../Label';
-import { OptionsDropdown } from '../Dropdown/OptionsDropdown';
-import { AMOUNT_COLORS, LABEL_COLORS } from './configs';
+import { AMOUNT_COLORS, LABEL_COLORS, STATE_COLORS } from './configs';
 
 export const TransactionCard = ({
   labelColor,
   labelText,
+  stateColor,
+  stateText,
   name,
+  href,
   amount,
   amountColor,
   productName,
   transactionTime,
-  dropdownOptions
 }) => (
   <div className="sage-transaction-card">
     <div className="sage-transaction-card__header">
       <Label value={labelText} color={labelColor} />
-      {dropdownOptions && (
-        <OptionsDropdown
-          align="right"
-          options={dropdownOptions}
-        />
-      )}
+      <div className={`sage-transaction-card__state${stateColor ? ` sage-transaction-card__state--${stateColor}` : ''}`}>
+        {stateText}
+      </div>
     </div>
     <div className="sage-transaction-card__body">
-      <h4 className="sage-transaction-card__name">{name}</h4>
+      <h4 className="sage-transaction-card__name">
+        {href ? (
+          <Link href={href} className="sage-transaction-card__name-link">{name}</Link>
+        ) : name}
+      </h4>
       {amount && (
         <div className={`sage-transaction-card__amount${amountColor ? ` sage-transaction-card__amount--${amountColor}` : ''}`}>
           {amount}
@@ -43,26 +46,30 @@ export const TransactionCard = ({
 
 TransactionCard.LABEL_COLORS = LABEL_COLORS;
 TransactionCard.AMOUNT_COLORS = AMOUNT_COLORS;
+TransactionCard.STATE_COLORS = STATE_COLORS;
 
 TransactionCard.defaultProps = {
   labelText: '--',
   labelColor: LABEL_COLORS.PUBLISHED,
+  stateText: null,
+  stateColor: null,
   name: '--',
+  href: null,
   amount: '0.00',
   amountColor: null,
   productName: '--',
   transactionTime: '--',
-  dropdownOptions: null,
 };
 
 TransactionCard.propTypes = {
   labelText: PropTypes.string,
   labelColor: PropTypes.oneOf(Object.values(TransactionCard.LABEL_COLORS)),
+  stateText: PropTypes.string,
+  stateColor: PropTypes.oneOf(Object.values(TransactionCard.STATE_COLORS)),
   name: PropTypes.string,
+  href: PropTypes.string,
   amount: PropTypes.string,
   amountColor: PropTypes.oneOf(Object.values(TransactionCard.AMOUNT_COLORS)),
   productName: PropTypes.string,
   transactionTime: PropTypes.string,
-  dropdownOptions: PropTypes.arrayOf(PropTypes.shape({}))
-
 };
--- packages/sage-react/lib/TransactionCard/TransactionCard.story.jsx
+++ packages/sage-react/lib/TransactionCard/TransactionCard.story.jsx
@@ -9,22 +9,18 @@ export default {
     ...selectArgs({
       amountColor: TransactionCard.AMOUNT_COLORS,
       labelColor: TransactionCard.LABEL_COLORS,
+      stateColor: TransactionCard.STATE_COLORS,
     }),
   },
   args: {
     amount: '+ $148.00',
     amountColor: TransactionCard.AMOUNT_COLORS.DEFAULT,
-    dropdownOptions: [
-      {
-        id: 1,
-        label: 'View contact profile',
-        icon: 'user-circle',
-        href: '#',
-      },
-    ],
-    labelColor: TransactionCard.LABEL_COLORS.INFO,
-    labelText: 'New',
+    labelColor: TransactionCard.LABEL_COLORS.PUBLISHED,
+    labelText: 'Multi-Payment',
+    stateColor: TransactionCard.STATE_COLORS.INFO,
+    stateText: 'New',
     name: 'Lilly Jones',
+    href: "https://kajabi.com",
     productName: 'Total Product Blueprint',
     transactionTime: '10:32pm'
   }
