af08e7ff1219e741182d727af32c3f9f7bb81451
Phillip Lovelace <phillip@pixelflips.com>
2021-04-26 12:34:28 -0700

    feat(transaction card): removes dropdown, adds optional href, and adds status text/colors (rails)

--- docs/lib/sage_rails/app/sage_components/sage_transaction_card.rb
+++ docs/lib/sage_rails/app/sage_components/sage_transaction_card.rb
@@ -2,15 +2,13 @@ class SageTransactionCard < SageComponent
   set_attribute_schema({
     amount: [:optional, String],
     amount_color: [:optional, Set.new(["sage", "red"])],
-    dropdown_options: [:optional, [[
-      attributes: [:optional, Hash],
-      icon: [:optional, String],
-      value: [:optional, String],
-    ]]],
     label_color: [:optional, Set.new(["published", "draft", "danger", "info", "warning", "locked"])],
     label_text: [:optional, String],
     name: [:optional, String],
+    href: [:optional, String],
     product_name: [:optional, String],
+    state: [:optional, String],
+    state_color: [:optional, Set.new(["info", "red"])],
     transaction_time: [:optional, String],
   })
 end
