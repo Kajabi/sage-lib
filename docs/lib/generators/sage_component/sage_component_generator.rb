e3914ba8a49eafa144b68d93e73e450598ef5ffb
Phil Schanely <philschanely@gmail.com>
2021-04-27 16:31:32 -0400

    chore(docs): reorganize to flatten elements and objects in documentation

--- docs/lib/generators/sage_component/sage_component_generator.rb
+++ docs/lib/generators/sage_component/sage_component_generator.rb
@@ -1,22 +1,22 @@
-class SageElementGenerator < Rails::Generators::NamedBase
+class SageComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
-  def create_sage_element
+  def create_sage_component
 
     # Style Variables
-    style_file = "../packages/sage-assets/lib/stylesheets/patterns/elements/_#{file_name}.scss"
+    style_file = "../packages/sage-assets/lib/stylesheets/patterns/components/_#{file_name}.scss"
     style_include_file = "../packages/sage-assets/lib/stylesheets/index.scss"
-    style_include_line = "// Elements"
+    style_include_line = "// Components"
     # Create Style File
     template "style.scss", style_file
     # Include Style File
     gsub_file style_include_file, /(#{Regexp.escape(style_include_line)})/mi do |match|
-      "#{match}\n@import \"patterns/elements/#{file_name}\";"
+      "#{match}\n@import \"patterns/components/#{file_name}\";"
     end
 
     # Markup Variables
-    markup_file = "app/views/examples/elements/#{file_name}/_preview.html.erb"
-    markup_include_file = "app/helpers/elements_helper.rb"
-    markup_include_line = "# Sage Generated Elements"
+    markup_file = "app/views/examples/components/#{file_name}/_preview.html.erb"
+    markup_include_file = "app/helpers/components_helper.rb"
+    markup_include_line = "# Sage Generated Components"
     # Create Markup File
     template "markup.html.erb", markup_file
     # Include Markup File
@@ -25,17 +25,17 @@ class SageElementGenerator < Rails::Generators::NamedBase
     end
 
     # Props Variables
-    markup_file = "app/views/examples/elements/#{file_name}/_props.html.erb"
+    markup_file = "app/views/examples/components/#{file_name}/_props.html.erb"
     # Create Props File
     template "props.html.erb", markup_file
 
     # Rules Do Variables
-    markup_file = "app/views/examples/elements/#{file_name}/_rules_do.html.erb"
+    markup_file = "app/views/examples/components/#{file_name}/_rules_do.html.erb"
     # Create Rules Do File
     template "rules_do.html.erb", markup_file
 
     # Rules Dont Variables
-    markup_file = "app/views/examples/elements/#{file_name}/_rules_dont.html.erb"
+    markup_file = "app/views/examples/components/#{file_name}/_rules_dont.html.erb"
     # Create Rules Dont File
     template "rules_dont.html.erb", markup_file
 
