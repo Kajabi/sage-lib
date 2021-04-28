8901162af9be05616bf973614ecad17a29b7d2b4
Phil Schanely <philschanely@gmail.com>
2021-04-28 10:36:08 -0400

    feat(docs): further update component and token generator

--- docs/lib/generators/sage_component/sage_component_generator.rb
+++ docs/lib/generators/sage_component/sage_component_generator.rb
@@ -1,43 +1,54 @@
 class SageComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+  argument :attributes, type: :array, default: [], banner: "attribute"
+
   def create_sage_component
 
-    # Style Variables
-    style_file = "../packages/sage-assets/lib/stylesheets/patterns/components/_#{file_name}.scss"
-    style_include_file = "../packages/sage-assets/lib/stylesheets/index.scss"
-    style_include_line = "// Components"
-    # Create Style File
-    template "style.scss", style_file
-    # Include Style File
-    gsub_file style_include_file, /(#{Regexp.escape(style_include_line)})/mi do |match|
-      "#{match}\n@import \"patterns/components/#{file_name}\";"
-    end
+    # Generate the rails component
 
     # Markup Variables
-    markup_file = "app/views/examples/components/#{file_name}/_preview.html.erb"
-    markup_include_file = "app/helpers/components_helper.rb"
-    markup_include_line = "# Sage Generated Components"
-    # Create Markup File
-    template "markup.html.erb", markup_file
-    # Include Markup File
-    gsub_file markup_include_file, /(#{Regexp.escape(markup_include_line)})/mi do |match|
-      "#{match}\n        {\n          title: \"#{file_name}\",\n          description: \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\", \n          scss: \"todo\",\n          rails: \"todo\",\n          react: \"todo\",\n          a11y: \"todo\",\n        },"
+    template "markup.html.erb", "lib/sage_rails/app/views/sage_components/_sage_#{file_name}.html.erb"
+    # Component Variables
+    template "component.rb", "lib/sage_rails/app/sage_components/sage_#{file_name}.rb"
+
+
+    # Generate the stylesheet
+
+    # Create Style File
+    template "style.scss", "../packages/sage-assets/lib/stylesheets/components/_#{file_name}.scss"
+    # Include Style File
+    gsub_file "../packages/sage-assets/lib/stylesheets/index.scss", /(#{Regexp.escape("// Components")})/mi do |match|
+      "#{match}\n@import \"components/#{file_name}\";"
     end
 
-    # Props Variables
-    markup_file = "app/views/examples/components/#{file_name}/_props.html.erb"
-    # Create Props File
-    template "props.html.erb", markup_file
 
-    # Rules Do Variables
-    markup_file = "app/views/examples/components/#{file_name}/_rules_do.html.erb"
-    # Create Rules Do File
-    template "rules_do.html.erb", markup_file
+    # Generate the Preview and documentation files
 
-    # Rules Dont Variables
-    markup_file = "app/views/examples/components/#{file_name}/_rules_dont.html.erb"
-    # Create Rules Dont File
-    template "rules_dont.html.erb", markup_file
+    # Include component in helper
+    gsub_file "app/helpers/components_helper.rb", /(#{Regexp.escape("# Sage Generated Components")})/mi do |match|
+      "#{match}\n        {\n          title: \"#{file_name}\",\n          description: \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\", \n          scss: \"todo\",\n          rails: \"todo\",\n          react: \"todo\",\n          a11y: \"todo\",\n        },"
+    end
+    # Preview for documentation
+    template "preview.html.erb", "app/views/examples/components/#{file_name}/_preview.html.erb"
+    # Props
+    template "props.html.erb", "app/views/examples/components/#{file_name}/_props.html.erb"
+    # Rules Do
+    template "rules_do.html.erb", "app/views/examples/components/#{file_name}/_rules_do.html.erb"
+    # Rules Dont
+    template "rules_dont.html.erb", "app/views/examples/components/#{file_name}/_rules_dont.html.erb"
+  end
 
+  private
+
+  def initialize_attributes
+    attributes.map { |attr| "#{attr.name}: [:optional, String]" }.join(",\n    ")
+  end
+
+  def use_attributes
+    attributes.map { |attr| "#{attr.name}: \"#{attr.name}\"" }.join(",\n  ")
+  end
+
+  def implement_attributes
+    attributes.map { |attr| "<%= component.#{attr.name} %>" }.join("\n  ")
   end
 end
