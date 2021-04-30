218593d55af65ef7e324c934e2c8d5f6e7bedc9b
Court McFadzean <court@kajabi.com>
2021-04-29 17:28:57 -0700

    feat(icon): adds a card_color option to icon
    Lets you set a card color that will turn the icon into an icon card
    
    re #449

--- docs/lib/sage_rails/app/sage_components/sage_icon.rb
+++ docs/lib/sage_rails/app/sage_components/sage_icon.rb
@@ -1,6 +1,7 @@
 class SageIcon < SageComponent
   set_attribute_schema({
     color: [:optional, NilClass, SageSchemas::COLOR_SLIDER],
+    card_color: [:optional, Set.new(["draft", "published", "info", "locked", "warning", "danger"])],
     css_classes: [:optional, String],
     icon: SageSchemas::ICON,
     label: [:optional, NilClass, String],
