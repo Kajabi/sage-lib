d0f08e5d15e66440a265c30c66d75934dd87b8c7
Court McFadzean <court@kajabi.com>
2021-04-30 08:07:14 -0700

    style(spacing): updating to the spacing

--- docs/lib/sage_rails/app/sage_components/sage_icon.rb
+++ docs/lib/sage_rails/app/sage_components/sage_icon.rb
@@ -2,7 +2,6 @@ class SageIcon < SageComponent
   set_attribute_schema({
     color: [:optional, NilClass, SageSchemas::COLOR_SLIDER],
     card_color: [:optional, Set.new(["draft", "published", "info", "locked", "warning", "danger"])],
-    css_classes: [:optional, String],
     icon: SageSchemas::ICON,
     label: [:optional, NilClass, String],
     size: [:optional, NilClass, SageSchemas::ICON_SIZE]
