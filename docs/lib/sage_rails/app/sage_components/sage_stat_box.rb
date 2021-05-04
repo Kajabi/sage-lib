d20aa86abe28cb8878c5e19bf32ce163071a87fd
Ashley E <ashleyne92@gmail.com>
2021-05-04 16:34:19 -0500

    feat(stat box): adds initial attributes and some styling

--- docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
+++ docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
@@ -1,5 +1,9 @@
 class SageStatBox < SageComponent
   set_attribute_schema({
-    
+    label_color: [:optional, Set.new(["danger", "info", "warning"])],
+    label_icon: [:optional, String],
+    label_text: [:optional, String],
+    status: String,
+    timeframe: [:optional, String],
   })
 end
