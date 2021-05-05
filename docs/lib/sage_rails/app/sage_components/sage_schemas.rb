37fb15b37ab7cfe13f259ea1b40e4992bac20d06
Ashley E <ashleyne92@gmail.com>
2021-05-05 17:32:00 -0500

    feat(stat box): adds more conditional markup logic and attributes

--- docs/lib/sage_rails/app/sage_components/sage_schemas.rb
+++ docs/lib/sage_rails/app/sage_components/sage_schemas.rb
@@ -13,6 +13,21 @@ module SageSchemas
   
   STATUSES = Set.new(SageTokens::STATUSES)
 
+  LABEL = {
+    container_attributes: [:optional, Hash],
+    color: Set.new(["danger", "draft", "info", "locked", "published", "success", "warning"]),
+    icon: [:optional, String],
+    interactive_type: [:optional, Set.new([:dropdown, :default, :secondary_button])],
+    secondary_button: [:optional, String, TrueClass, {
+      attributes:       [:optional, NilClass, Hash],
+      css_classes:      [:optional, String],
+      icon:             [:optional, String],
+      value:            [:optional, String],
+    }],
+    style: [:optional, Set.new(["subtle", "bold"])],
+    value: String,
+  }
+
   SPACER = {
     top: [:optional, Set.new(SageTokens::SPACER_SIZES)],
     right: [:optional, Set.new(SageTokens::SPACER_SIZES)],
--- docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
+++ docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
@@ -1,9 +1,13 @@
 class SageStatBox < SageComponent
   set_attribute_schema({
-    label_color: [:optional, Set.new(["danger", "info", "warning"])],
-    label_icon: [:optional, String],
-    label_text: [:optional, String],
+    custom_label: [:optional, NilClass, SageSchemas::LABEL],
+    data: Integer,
+    change_type: [:optional, Set.new(["up", "down", "neutral"])],
+    change_value: [:optional, String],
+    link_href: [:optional, String],
+    link_value: [:optional, String],
     status: String,
     timeframe: [:optional, String],
+    title_icon: [:optional, TrueClass]
   })
 end
