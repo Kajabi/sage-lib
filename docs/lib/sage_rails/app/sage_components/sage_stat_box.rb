c947ba91a589ede4d72a037946fb8d2a1a300b51
Ashley E <ashleyne92@gmail.com>
2021-05-11 15:34:23 -0500

    feat(stat box): cleans up styling and logic for React and Rails component

--- docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
+++ docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
@@ -1,14 +1,17 @@
 class SageStatBox < SageComponent
   set_attribute_schema({
-    advanced: [:optional, TrueClass],
-    custom_label: [:optional, NilClass, SageSchemas::LABEL],
+    custom_label: [:optional, NilClass, String],
     data: Integer,
-    change_type: [:optional, Set.new(["up", "down", "neutral"])],
-    change_value: [:optional, String],
-    link_href: [:optional, String],
-    link_value: [:optional, String],
+    change: [:optional, {
+      type: Set.new(["up", "down", "neutral"]),
+      value: String,
+    }],
+    link: [:optional, {
+      href: String,
+      value: String,
+    }],
     timeframe: [:optional, String],
     title: String,
-    title_icon: [:optional, TrueClass]
+    popover: [:optional, String],
   })
 end
--- packages/sage-react/lib/StatBox/StatBox.jsx
+++ packages/sage-react/lib/StatBox/StatBox.jsx
@@ -2,68 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from '../Icon';
 import { Label } from '../Label';
-import { Popover } from '../Popover';
 import { LABEL_COLORS, TYPE } from './configs'; // component configurations as needed
 
 export const StatBox = ({
-  advanced,
   customLabel,
   data,
-  changeType,
-  changeValue,
-  linkHref,
-  linkValue,
+  change,
+  link,
+  popover,
   timeframe,
   title,
-  titleIcon
 }) => {
-
   const renderLabelStatus = () => {
-    let valueConfig = changeValue ? changeValue : "No Change";
-    let typeConfig = changeType ? changeValue : "neutral";
-    let icon, 
-        color;
+    let icon = null;
+    let color;
 
-    switch (typeConfig) {
+    switch (change.type) {
       case StatBox.TYPE.UP:
-        console.log("up");
         icon = Icon.ICONS.CARET_UP;
         color = StatBox.LABEL_COLORS.PUBLISHED;
         break;
       case StatBox.TYPE.DOWN:
-        console.log("down");
         icon = Icon.ICONS.CARET_DOWN;
         color = StatBox.LABEL_COLORS.DANGER;
         break;
-      case StatBox.TYPE.DEFAULT:
+      default:
         color = StatBox.LABEL_COLORS.DRAFT;
-        console.log("neutral");
         break;
     }
 
-    return <Label color={color} value={valueConfig} icon={icon} />
-  }
+    return (
+      <Label color={color} value={change.value} icon={icon} />
+    );
+  };
 
   return (
     <article className="sage-stat-box">
       <header className="sage-stat-box__header">
         <h2 className="sage-stat-box__title">{title}</h2>
-        {titleIcon ? (
-          <Popover icon="info-circle" />
-        ) : ''}
+        {popover}
       </header>
       <main className="sage-stat-box__body sage-grid-template-ete">
         <p className="sage-stat-box__data">{data}</p>
         {timeframe && (
           <span className="sage-stat-box__timeframe">{timeframe}</span>
         )}
-        {changeValue && (
-          renderLabelStatus()
-        )}
+        {customLabel || (change && (renderLabelStatus()))}
       </main>
-      {linkValue && (
+      {link && (
         <footer className="sage-stat-box__footer">
-          <a className="sage-stat-box__link" href={linkHref}>{linkValue}</a>
+          <a className="sage-stat-box__link" href={link.href}>{link.value}</a>
         </footer>
       )}
     </article>
@@ -74,23 +62,31 @@ StatBox.LABEL_COLORS = LABEL_COLORS;
 StatBox.TYPE = TYPE;
 
 StatBox.defaultProps = {
-  advanced: false,
-  data: 0,
-  changeType: StatBox.TYPE.DEFAULT,
-  linkHref: null,
-  linkValue: 'View More',
-  status: 'In Progress',
+  change: {
+    type: StatBox.TYPE.DOWN,
+    value: '38%',
+  },
+  customLabel: null,
+  link: {
+    href: null,
+    value: 'View More',
+  },
+  popover: null,
   timeframe: 'Current',
 };
 
 StatBox.propTypes = {
-  advanced: PropTypes.bool,
-  data: PropTypes.number,
-  changeType: PropTypes.oneOf(Object.values(StatBox.TYPE)),
-  changeValue: PropTypes.string,
-  linkHref: PropTypes.string,
-  linkValue: PropTypes.string,
-  status: PropTypes.string,
+  customLabel: PropTypes.node,
+  data: PropTypes.number.isRequired,
+  change: PropTypes.shape({
+    type: PropTypes.oneOf(Object.values(StatBox.TYPE)).isRequired,
+    value: PropTypes.string.isRequired,
+  }),
+  link: PropTypes.shape({
+    href: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  }),
+  popover: PropTypes.node,
   timeframe: PropTypes.string,
-  titleIcon: PropTypes.bool,
+  title: PropTypes.string.isRequired,
 };
