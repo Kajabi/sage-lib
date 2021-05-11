8bb84d9fda66794572b8ff35221424e5ab90963e
Ashley E <ashleyne92@gmail.com>
2021-05-11 16:47:32 -0500

    docs(stat box): adds properties to docs

--- docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
+++ docs/lib/sage_rails/app/sage_components/sage_stat_box.rb
@@ -1,11 +1,11 @@
 class SageStatBox < SageComponent
   set_attribute_schema({
     custom_label: [:optional, NilClass, String],
-    data: Integer,
     change: [:optional, {
       type: Set.new(["up", "down", "neutral"]),
       value: String,
     }],
+    data: Integer,
     link: [:optional, {
       href: String,
       value: String,
