a215f2344be2fdec11c4049bc75a617bd12dbe99
Phil Schanely <philschanely@gmail.com>
2021-05-12 13:05:23 -0400

    feat(react): add color palette values to tokens

--- packages/sage-react/lib/configs/tokens/index.js
+++ packages/sage-react/lib/configs/tokens/index.js
@@ -1,10 +1,11 @@
-import { TOKENS_COLORS, TOKENS_COLOR_SLIDERS } from './colors';
+import { TOKENS_COLORS, TOKENS_COLOR_PALETTE, TOKENS_COLOR_SLIDERS } from './colors';
 import { TOKENS_GRID_TEMPLATES } from './grid-templates';
 import { TOKENS_ICONS } from './icons';
 import { TOKENS_SPACERS } from './spacers';
 
 export const SageTokens = {
   COLORS: TOKENS_COLORS,
+  COLOR_PALETTE: TOKENS_COLOR_PALETTE,
   COLOR_SLIDERS: TOKENS_COLOR_SLIDERS,
   GRID_TEMPLATES: TOKENS_GRID_TEMPLATES,
   ICONS: TOKENS_ICONS,
