15a98f96c39a4386223ab769e486b2b02f1fbd0e
Quinton Jason <quinton.jason@gmail.com>
2021-05-14 09:18:06 -0500

    fix(catalog-item): add image_aspect_ratio prop and styles

--- docs/lib/sage_rails/app/sage_components/sage_catalog_item.rb
+++ docs/lib/sage_rails/app/sage_components/sage_catalog_item.rb
@@ -2,6 +2,7 @@ class SageCatalogItem < SageComponent
   set_attribute_schema({
     href: [:optional, String],
     image: [:optional, String],
+    image_aspect_ratio: [:optional, NilClass, Set.new(["square"])],
     title: [:optional, String],
     icon: [:optional, String],
   })
