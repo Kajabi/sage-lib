19b86436e1ec759b6527ddbaf7f252766f500e82
Phil Schanely <philschanely@gmail.com>
2021-05-14 13:01:22 -0400

    feat(chart): add POC Bar charts

--- packages/sage-react/lib/Chart/configs.js
+++ packages/sage-react/lib/Chart/configs.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
 export const CHART_TYPES = {
+  BAR: 'bar',
   DONUT: 'donut',
 };
 
 export const dataPropTypes = {
   name: PropTypes.string.isRequired,
-  value: PropTypes.number.isRequired,
+  value: PropTypes.number,
 };
