11513a30efa8ea6be4fa700822a58dd4c2a9ac45
Brendon Murphy <xternal1+github@gmail.com>
2021-05-21 14:35:55 -0700

    Disable activerecord that was accidentally added during upgrade merge

--- docs/config/environments/production.rb
+++ docs/config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 end
