a57851376731cd2b537622addd528b31019d611c
Brendon Murphy <xternal1+github@gmail.com>
2021-05-25 10:46:08 -0700

    Update rails and ensure rack is updated as well (#533)
    * Update rails version
    
    * Update docs/config/boot.rb for Rails 5
    
    * Update config/application.rb for Rails 5
    
    * update config/environment.rb for Rails 5
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * More rails 5 updates
    
    * Fix bad merge for docs/config/secrets.yml
    
    * Disable activerecord that was accidentally added during upgrade merge

--- docs/config/environments/production.rb
+++ docs/config/environments/production.rb
@@ -49,16 +49,15 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  # config.active_job.queue_name_prefix = "sage_docs_#{Rails.env}"
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -74,6 +73,15 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Use a different logger for distributed setups.
+  # require 'syslog/logger'
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
 end
