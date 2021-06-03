9df974ca39c773b6c02fc6471257370d458861d3
Ben Hays <ben@kajabi.com>
2021-06-03 16:06:23 -0700

    feat(hint): generated rails-y things and stylesheet for Hint component

--- packages/sage-react/lib/Hint/Hint.jsx
+++ packages/sage-react/lib/Hint/Hint.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-export const Hint = ({ text = 'Hello, world!' }) => (
-  <div className="hint">
-    <h1 className="hint__text">{text} This is a hint, dude!</h1>
-  </div>
-);
+export const Hint = ({
+  className,
+  text,
+  ...rest
+}) => {
+  const classNames = classnames(
+    'sage-hint',
+    className,
+  );
+  return (
+    <div className={classNames} {...rest}>
+      <h1 className="sage-hint__text">{text} This is a hint, dude!</h1>
+    </div>
+  );
+};
 
 Hint.defaultProps = {
+  className: '',
 };
 
 Hint.propTypes = {
-  text: PropTypes.string.isRequired
+  className: PropTypes.string,
+  text: PropTypes.string.isRequired,
 };
