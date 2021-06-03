ecc079837e88290fa988962b3127354502add688
Ben Hays <ben@kajabi.com>
2021-06-03 13:18:51 -0700

    feat(hint): added basic structure for Hint component

---
+++ packages/sage-react/lib/Hint/Hint.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Hint = ({ text }) => {
+
+};
+
+Hint.defaultProps = {
+};
+
+Hint.propTypes = {
+  text: PropTypes.string.isRequired
+};
