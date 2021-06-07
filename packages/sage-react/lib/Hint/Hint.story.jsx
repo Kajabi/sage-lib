f4ef96c40286cdc1fffa0fe35ba33eb5e499bd1e
Ben Hays <ben@kajabi.com>
2021-06-07 14:12:10 -0700

    feat(hint): added all Sage icons as options. No icon by default

--- packages/sage-react/lib/Hint/Hint.story.jsx
+++ packages/sage-react/lib/Hint/Hint.story.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { selectArgs } from '../story-support/helpers';
+import { SageTokens } from '../configs';
 import { Hint } from './Hint';
 
 export default {
   title: 'Sage/Hint',
   component: Hint,
+  argTypes: {
+    ...selectArgs({
+      icon: SageTokens.ICONS,
+    }),
+  },
   args: {
-    content: 'Avoid using the following spam trigger: "free"'
+    content: 'Avoid using the following spam trigger: "free"',
   }
 };
 
