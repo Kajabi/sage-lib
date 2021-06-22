5eed0c1fea6c61f90293de95b49cbc1f1c3d45c3
Phil Schanely <philschanely@gmail.com>
2021-06-22 13:51:41 -0400

    feat(empty-state): update to spec and consolidate null view

--- docs/lib/sage_rails/app/sage_components/sage_empty_state.rb
+++ docs/lib/sage_rails/app/sage_components/sage_empty_state.rb
@@ -1,9 +1,15 @@
 class SageEmptyState < SageComponent
   set_attribute_schema({
+    center_vertical: [:optional, TrueClass],
     compact: [:optional, TrueClass],
     graphic: [:optional, String],
     icon: [:optional, String],
+    scope: [:optional, Set.new(["page", "compact", nil])],
     title: [:optional, String],
+    title_tag: [:optional, Set.new(["h1", "h2", "h3", "h4", "h5", "h5", "h6"])],
     text: [:optional, String],
   })
+  def sections
+    %w(sage_empty_state_actions sage_empty_state_text)
+  end
 end
