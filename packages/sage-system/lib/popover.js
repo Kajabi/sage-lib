8c96b0dc876f8947bb06d060f829b3d79c546503
Phil Schanely <philschanely@gmail.com>
2021-07-08 11:41:33 -0400

    feat(popover): sync rails side and tweak styles

--- packages/sage-system/lib/popover.js
+++ packages/sage-system/lib/popover.js
@@ -6,7 +6,6 @@ Sage.popover = (function() {
   const SELECTOR_PARENT = 'data-js-popover';
   const SELECTOR_OVERLAY = 'data-js-popover--overlay';
   const SELECTOR_TRIGGER = 'data-js-popover--trigger';
-  const SELECTOR_POSITION = 'data-js-popover--position';
 
   const CLASS_ACTIVE = 'sage-popover--is-expanded';
   const ATTRIBUTE_ARIA_EXPANDED = 'aria-expanded';
@@ -32,54 +31,18 @@ Sage.popover = (function() {
     const elParent = evt.currentTarget;
     const elChild = elParent.children[0];
     const popoverPanel = elChild.parentNode.querySelector(".sage-popover__panel");
-    const options = {
-      triggerWidth: elChild.offsetWidth,
-      triggerHeight: elChild.offsetHeight,
-      popoverPanelHeight: popoverPanel.offsetHeight,
-      popoverPanelWidth: popoverPanel.offsetWidth,
-      position: elChild.parentNode.getAttribute(SELECTOR_POSITION)
-    };
 
     if (evt.target.hasAttribute(SELECTOR_TRIGGER) || evt.target.parentNode.hasAttribute(SELECTOR_TRIGGER)) {
       if (isExpanded(elParent) || isExpanded(elChild.parentNode)) {
         closePopoverPanel(elParent);
       } else {
         openPopoverPanel(elParent);
-        positionPopoverPanel(popoverPanel, options)
       }
     }
     // if the clicked element is the overlay, close the previously opened popover
     if (evt.target.hasAttribute(SELECTOR_OVERLAY)) {
       closePopoverPanel(elParent);
     }
-  }
-
-  function positionPopoverPanel(popoverPanel, options) {
-
-    let dist = 8,
-        top,
-        left;
-
-    switch (options.position) {
-      case "left":
-        top = 0;
-        left = -options.popoverPanelWidth - (dist * 2);
-      break;
-      case "top":
-        top = -options.popoverPanelHeight - dist;
-        left = 0;
-      break;
-      case "bottom":
-        top = options.triggerHeight + dist;
-        left = 0;
-        break;
-      default:
-      case "right":
-        top = 0;
-        left = options.triggerWidth + (dist * 2);
-    }
-    popoverPanel.style.left = left + "px";
-    popoverPanel.style.top = top + "px";
   }
 
   function handleKeydown(evt) {
