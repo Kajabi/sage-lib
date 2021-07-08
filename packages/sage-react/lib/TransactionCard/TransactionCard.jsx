5e155b063693f0ad6b4f6abc5fb43731c9df69eb
Quinton Jason <quinton.jason@gmail.com>
2021-07-08 15:00:03 -0500

    fix(interactive pattern): updates per phil review

--- packages/sage-react/lib/TransactionCard/TransactionCard.jsx
+++ packages/sage-react/lib/TransactionCard/TransactionCard.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Property } from '../Property';
-import { Icon } from '../Icon';
-import { Button } from '../Button';
 import { Link } from '../Link';
 import { Label } from '../Label';
-import { SageTokens } from '../configs';
 import { AMOUNT_COLORS, LABEL_COLORS, STATE_COLORS } from './configs';
-
+import { SageTokens } from '../configs';
+import { Button } from '../Button';
 export const TransactionCard = ({
   amount,
   amountColor,
@@ -24,14 +22,18 @@ export const TransactionCard = ({
   transactionTime,
 }) => {
   const NameTag = nameTag;
-
+  const amountClassNames = classnames(
+    'sage-transaction-card__amount',
+    {
+      [`sage-transaction-card__amount--${amountColor}`]: amountColor,
+    }
+  );
   const stateClassNames = classnames(
     'sage-transaction-card__state',
     {
       [`sage-transaction-card__state--${transactionStateColor}`]: transactionStateColor,
     }
   );
-
   return (
     <article className="sage-transaction-card">
       <div className="sage-transaction-card__header">
@@ -44,41 +46,44 @@ export const TransactionCard = ({
         <div className="sage-transaction-card__body-group">
           <NameTag className="sage-transaction-card__name">
             {nameHref ? (
-              <Link href={nameHref} className="sage-link sage-transaction-card__name-link">
-                <span className="t-sage--truncate">{name}</span>
+              <Link
+                href={nameHref}
+                className="sage-link sage-transaction-card__name-link"
+              >
+                {name}
               </Link>
             ) : name}
           </NameTag>
           {amount && (
-            <div className={`sage-transaction-card__amount${amountColor ? ` sage-transaction-card__amount--${amountColor}` : ''}`}>
+            <div className={amountClassNames}>
               {amount}
             </div>
           )}
         </div>
-      </div>
-      <div className="sage-transaction-card__footer">
-        {relatedPropertyHref ? (
-          <Button
-            color={Button.COLORS.SECONDARY}
-            href={relatedPropertyHref}
-            icon={SageTokens.ICONS.TAG}
-            iconPosition={Button.ICON_POSITIONS.LEFT}
-            small={true}
-            subtle={true}
-          >
-            {relatedProperty}
-          </Button>
-        ) : <Property icon={Icon.ICONS.TAG}>{relatedProperty}</Property>}
-        <Property>{transactionTime}</Property>
+        <div className="sage-transaction-card__body-group">
+          {relatedPropertyHref ? (
+            <Button
+              className="sage-transaction-card__product"
+              color={Button.COLORS.SECONDARY}
+              href={relatedPropertyHref}
+              icon={SageTokens.ICONS.TAG}
+              small={true}
+              subtle={true}
+            >
+              {relatedProperty}
+            </Button>
+          ) : (
+            <Property className="sage-transaction-card__product" icon={SageTokens.ICONS.TAG}>{relatedProperty}</Property>
+          )}
+          <Property className="sage-transaction-card__time">{transactionTime}</Property>
+        </div>
       </div>
     </article>
   );
 };
-
 TransactionCard.LABEL_COLORS = LABEL_COLORS;
 TransactionCard.AMOUNT_COLORS = AMOUNT_COLORS;
 TransactionCard.STATE_COLORS = STATE_COLORS;
-
 TransactionCard.defaultProps = {
   amount: '0.00',
   amountColor: null,
@@ -93,7 +98,6 @@ TransactionCard.defaultProps = {
   transactionStateColor: null,
   transactionTime: '--',
 };
-
 TransactionCard.propTypes = {
   amount: PropTypes.string,
   amountColor: PropTypes.oneOf(Object.values(TransactionCard.AMOUNT_COLORS)),
