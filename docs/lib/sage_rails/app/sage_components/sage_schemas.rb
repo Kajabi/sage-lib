231157fd8aae10c1b3a91fd4400b3a38c94835bf
Phil Schanely <philschanely@gmail.com>
2021-07-16 14:31:51 -0400

    chore(media-tiles): remove aspect ratio presets and centralize panel figure schema reference

--- docs/lib/sage_rails/app/sage_components/sage_schemas.rb
+++ docs/lib/sage_rails/app/sage_components/sage_schemas.rb
@@ -31,6 +31,14 @@ module SageSchemas
     value: String,
   }
 
+  PANEL_FIGURE = {
+    aspect_ratio: [:optional, NilClass, Integer, Float],
+    background_color: [:optional, NilClass, String],
+    bleed: [:optional, Set.new(["top", "bottom", "sides"])],
+    is_wistia: [:optional, TrueClass],
+    padded: [:optional, NilClass, TrueClass],
+  }
+
   MEDIA_TILE = {
     actions_dropdown_items: [:optional, [[SageSchemas::DROPDOWN_ITEM]]],
     actions_custom: [:optional, NilClass, String],
@@ -38,12 +46,7 @@ module SageSchemas
     caption: [:optional, NilClass, String],
     footer: [:optional, NilClass, String],
     media: [:optional, NilClass, String],
-    media_configs: [:optional, NilClass, {
-      aspect_ratio: [:optional, NilClass, Integer, Float, Set.new(["1x", "2x", "3x"])],
-      is_wistia: [:optional, TrueClass],
-      key_color: [:optional, NilClass, String],
-      padded: [:optional, NilClass, TrueClass],
-    }],
+    media_configs: [:optional, NilClass, SageSchemas::PANEL_FIGURE],
     title: [:optional, NilClass, String],
   }
 
