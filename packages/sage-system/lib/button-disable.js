5048849f47221e95eff594e33693b41b8607990f
Quinton Jason <quinton.jason@gmail.com>
2021-07-19 11:18:38 -0500

    fix(button): more updates to loading state

--- packages/sage-system/lib/button-disable.js
+++ packages/sage-system/lib/button-disable.js
@@ -3,7 +3,7 @@ Sage.buttonDisable = (function () {
   // Variables
   // ==================================================
 
-  const SELECTOR_BUTTON_DISABLED_WITH = "data-js-sage-disabled-with"
+  const SELECTOR_BUTTON_DISABLE_ON_SUBMIT = "data-js-sage-disable-on-submit"
   const BUTTON_LOADING_SPINNER = `<svg class='sage-loader__spinner sage-loader__spinner--loading-button' viewBox='25 25 50 50' aria-hidden='true'><circle class='sage-loader__spinner-path sage-loader__spinner-path--loading-button' cx='50' cy='50' r='20' fill='none' stroke='0072EF' stroke-width='4'></circle></svg>`;
   const ATTRIBUTE_ARIA_LABEL = 'aria-label';
 
@@ -11,24 +11,33 @@ Sage.buttonDisable = (function () {
   // Functions
   // ==================================================
 
-  const disabledWithButton = document.querySelector(`[${SELECTOR_BUTTON_DISABLED_WITH}]`);
+  function renderDisabledButtonSpinner(evt) {
+    console.log('evt', evt);
+    let currentTarget = evt.currentTarget;
+    let spinnerLabel = currentTarget.getAttribute(SELECTOR_BUTTON_DISABLE_ON_SUBMIT);
 
-  renderDisabledButtonSpinner(disabledWithButton);
+    currentTarget.classList.add('sage-btn--is-loading');
+    currentTarget.setAttribute(ATTRIBUTE_ARIA_LABEL, spinnerLabel)
 
-  function renderDisabledButtonSpinner(node) {
-    if (disabledWithButton == null) return false;
+    let element = document.createElement('div');
+    element.innerHTML = BUTTON_LOADING_SPINNER;
 
-    let spinnerLabel = node.getAttribute(SELECTOR_BUTTON_DISABLED_WITH);
+    currentTarget.appendChild(element)
 
-    node.classList.add('sage-btn--is-loading');
-    node.setAttribute(ATTRIBUTE_ARIA_LABEL, spinnerLabel)
-
-    return node.innerHTML = BUTTON_LOADING_SPINNER;
+    // return currentTarget.innerHTML = BUTTON_LOADING_SPINNER;
+    return
   }
 
-  function init() {}
+  function init(el) {
+    el.addEventListener("click", renderDisabledButtonSpinner);
+  }
+
+  function unbind(el) {
+    el.removeEventListener("click", renderDisabledButtonSpinner);
+  }
 
   return {
     init: init,
+    unbind: unbind
   };
 })();
