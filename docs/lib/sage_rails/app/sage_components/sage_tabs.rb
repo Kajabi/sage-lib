5ac2338f6168cf46c0625c47bb50b53fd211fe40
Phil Schanely <philschanely@gmail.com>
2021-07-22 11:07:52 -0400

    chore(tabs): centralize schemas for more efficient use

--- docs/lib/sage_rails/app/sage_components/sage_tabs.rb
+++ docs/lib/sage_rails/app/sage_components/sage_tabs.rb
@@ -2,26 +2,7 @@ class SageTabs < SageComponent
   set_attribute_schema({
     align_items_center: [:optional, TrueClass],
     id: [:optional, String],
-    items: [:optional, [[
-      active: [:optional, NilClass, TrueClass],
-      align_center: [:optional, TrueClass],
-      attributes: [:optional, NilClass, Hash],
-      data: [:optional, Hash],
-      disabled: [:optional, NilClass, TrueClass],
-      graphic: [:optional, NilClass, String],
-      icon: [:optional,NilClass, String],
-      link_text: [:optional, NilClass, String],
-      subtext: [:optional, NilClass, String],
-      target: [:optional, NilClass, String],
-      text: [:optional, NilClass, String],
-      type: [:optional, Set.new(["arrow", "graphic", "icon", "radio"])],
-      vertical_align_icon: [:optional, NilClass, Set.new(["start"])],
-      radio_configs: [:optional, NilClass, {
-        value: String,
-        name: String,
-        id: String,
-      }]
-    ]]],
+    items: [:optional, [[SageSchemas::CHOICE]], [[SageSchemas::TAB]]],
     navigational: [:optional, TrueClass],
     progressbar: [:optional, TrueClass],
     stacked: [:optional, TrueClass],
