bfb7d408264d711741b920a664cbc0c77c88efb2
Ben Hays <ben@kajabi.com>
2021-07-28 15:53:47 -0700

    feat(upsell): getting started on Upsell component

--- packages/sage-react/lib/Upsell/Upsell.jsx
+++ packages/sage-react/lib/Upsell/Upsell.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { Button } from '../Button';
+// import { Choice } from '../Choice';
 import { Icon } from '../Icon';
-import { SageTokens } from '../configs';
+import { Label } from '../Label';
+import { Modal } from '../Modal';
+import { Panel } from '../Panel';
+import { SageClassnames, SageTokens } from '../configs';
+
+const Benefit = ({ icon, benefitAmount, benefitTitle }) => (
+  <div className="sage-upsell__icon">
+    <Icon icon={icon} size="lg" />
+    <span className="sage-upsell__allowance-text">
+      <strong>{benefitAmount}</strong>&nbsp;{benefitTitle}
+    </span>
+  </div>
+);
+
+Benefit.propTypes = {
+  icon: PropTypes.string.isRequired,
+  benefitAmount: PropTypes.string.isRequired,
+  benefitTitle: PropTypes.string.isRequired,
+};
 
 export const Upsell = ({
   className,
-  content,
-  icon,
   ...rest
 }) => {
   const classNames = classnames(
@@ -16,24 +34,45 @@ export const Upsell = ({
   );
   return (
     <div className={classNames} {...rest}>
-      {icon && (
-        <Icon
-          className="sage-upsell__icon"
-          icon={icon}
-        />
-      )}
-      <span className="sage-upsell__content">{content}</span>
+      <Modal active="true">
+        <Modal.Body>
+          <Label
+            color="published"
+            interactiveType
+            value="Upgrade today!"
+          />
+          <h5 className={SageClassnames.TYPE.HEADING_4}>
+            Kajabi Growth Plan
+          </h5>
+          <p>Uprage your account to do your best work</p>
+          <hr />
+        </Modal.Body>
+        <Modal.Body>
+          <Panel.Row gridTemplate={SageTokens.GRID_TEMPLATES.M}>
+            <div className="sage-upsell__icons">
+              <Benefit icon={SageTokens.ICONS.PRODUCT} benefitAmount="15" benefitTitle="Products" />
+              <Benefit icon={SageTokens.ICONS.MEGAPHONE} benefitAmount="15" benefitTitle="Pipelines" />
+              <Benefit icon={SageTokens.ICONS.MONITOR} benefitAmount="Unlimited" benefitTitle="Pages" />
+              <Benefit icon={SageTokens.ICONS.MAIL} benefitAmount="Unlimited" benefitTitle="Emails" />
+              <Benefit icon={SageTokens.ICONS.USER_CIRCLE} benefitAmount="25K" benefitTitle="Contacts" />
+              <Benefit icon={SageTokens.ICONS.USERS} benefitAmount="10K" benefitTitle="Members" />
+            </div>
+            <div className="sage-upsell__choices">
+              {/* <Choice text="Growth - $159/mo" subtext="Billed monthly" /> */}
+              {/* <Choice text="Pro - $319/mo" subtext="Billed monthly" /> */}
+              <Button fullWidth="true">This is a button</Button>
+            </div>
+          </Panel.Row>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
 
 Upsell.defaultProps = {
   className: '',
-  icon: null,
 };
 
 Upsell.propTypes = {
   className: PropTypes.string,
-  content: PropTypes.string.isRequired,
-  icon: PropTypes.oneOf(Object.values(SageTokens.ICONS)),
 };
