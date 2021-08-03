0a6ad798f3c69b26e9721607e7d023a012d839ae
Ben Hays <ben@kajabi.com>
2021-08-03 15:09:53 -0700

    feat(choice): added type and icon props

--- packages/sage-react/lib/Choice/Choice.jsx
+++ packages/sage-react/lib/Choice/Choice.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { SageTokens } from '../configs';
+import { CHOICE_TYPES } from './config';
 
 export const Choice = ({
   className,
+  icon,
   isActive,
+  type,
   ...rest
 }) => {
   const baseClass = 'sage-choice';
+  const isIconType = type && type === CHOICE_TYPES.ICON;
+  // const isRadioType = type && type === CHOICE_TYPES.RADIO;
+
   const classNames = classnames(
     baseClass,
     className,
+    'sage-choice--radio',
     {
       [`${baseClass}--active`]: isActive,
+      [`${baseClass}--${type}`]: type && !isIconType,
+      [`${baseClass}--icon-${icon}`]: isIconType && icon,
     }
   );
   return (
@@ -22,12 +32,18 @@ export const Choice = ({
   );
 };
 
+Choice.CHOICE_TYPES = CHOICE_TYPES;
+
 Choice.defaultProps = {
   className: '',
+  icon: null,
   isActive: false,
+  type: null,
 };
 
 Choice.propTypes = {
   className: PropTypes.string,
+  icon: PropTypes.oneOf(Object.values(SageTokens.ICONS)),
   isActive: PropTypes.bool,
+  type: PropTypes.oneOf(Object.values(Choice.CHOICE_TYPES)),
 };
