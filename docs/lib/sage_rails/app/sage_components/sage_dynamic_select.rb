4819e50a9ad69e6fcda90e28b44d790c06b9dc5a
Jamell Jones <jamell@kajabi.com>
2021-08-12 08:59:48 -0700

    feat(dynamic-select): add markup and ruby class

---
+++ docs/lib/sage_rails/app/sage_components/sage_dynamic_select.rb
@@ -0,0 +1,18 @@
+class SageDynamicSelect < SageComponent
+  set_attribute_schema({
+    url: String,
+    label: String,
+    name: String,
+    id: String,
+    has_error: [:optional, TrueClass],
+    message: [:optional, String],
+    default_value: [:optional, String, Integer],
+    default_text: [:optional, String],
+    paginate: [:optional, TrueClass],
+    search: [:optional, TrueClass],
+    clear: [:optional, TrueClass],
+    placeholder: [:optional, String],
+    theme: [:optional, String],
+    required: [:optional, TrueClass],
+  })
+end
