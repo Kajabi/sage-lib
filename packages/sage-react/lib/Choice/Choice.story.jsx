11102f4df096494dabd4af81923671d119fe2c98
Ben Hays <ben@kajabi.com>
2021-08-12 14:18:58 -0700

    refactor(choice): removed imports for CONSTANTS for existing ones

--- packages/sage-react/lib/Choice/Choice.story.jsx
+++ packages/sage-react/lib/Choice/Choice.story.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { selectArgs } from '../story-support/helpers';
 import { SageTokens } from '../configs';
 import { Choice } from './Choice';
-import { ICON_ALIGNMENTS, TYPES } from './configs';
 
 export default {
   title: 'Sage/Choice',
@@ -10,8 +9,8 @@ export default {
   argTypes: {
     ...selectArgs({
       icon: SageTokens.ICONS,
-      type: TYPES,
-      verticalAlignIcon: ICON_ALIGNMENTS,
+      type: Choice.TYPES,
+      verticalAlignIcon: Choice.ICON_ALIGNMENTS,
     }),
   },
   args: {
@@ -19,7 +18,7 @@ export default {
     radioConfigs: { name: 'alpha', id: 'bravo', value: 'charlie' },
     subtext: 'Description of Option 1',
     text: 'Option 1',
-    verticalAlignIcon: ICON_ALIGNMENTS.DEFAULT,
+    verticalAlignIcon: Choice.ICON_ALIGNMENTS.DEFAULT,
   }
 };
 
