b569205bb90e8dcbbe6bf6c59ee86780996e6b6a
Kevin Chang <kevin@kajabi.com>
2021-08-12 18:24:09 -0700

    feat(button group): adds wrap option to button group
    #690

--- docs/lib/sage_rails/app/sage_components/sage_button_group.rb
+++ docs/lib/sage_rails/app/sage_components/sage_button_group.rb
@@ -2,6 +2,7 @@ class SageButtonGroup < SageComponent
   set_attribute_schema({
     align: [:optional, Set.new(["space-between", "end"])],
     borderTop: [:optional, TrueClass],
-    gap: [:optional, Set.new([:xs, :sm, :md, :lg])]
+    gap: [:optional, Set.new([:xs, :sm, :md, :lg])],
+    wrap: [:optional, TrueClass]
   })
 end
