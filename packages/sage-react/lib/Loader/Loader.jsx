8832e3e4ff3eceff355b80a19af5d3b1bf7c4f1d
Ashley E <ashleyne92@gmail.com>
2021-08-23 14:51:02 -0500

    feat(loader): updates React component

--- packages/sage-react/lib/Loader/Loader.jsx
+++ packages/sage-react/lib/Loader/Loader.jsx
@@ -41,6 +41,12 @@ export const Loader = ({
           <circle className="sage-loader__spinner-path" cx="50" cy="50" r="20" fill="none" stroke="url(#linear)" strokeWidth="4" />
         </svg>
       )}
+      {(type === LOADER_TYPES.SUCCESS) && (
+        <svg className="sage-loader__success" viewBox="25 25 50 50">
+          <circle className="sage-loader__success-path" cx="50" cy="50" r="20" fill="none" stroke="0072EF" strokeWidth="4" />
+          <path className="sage-loader__success-check" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" d="M22.7071 1.29291C23.0976 1.68344 23.0976 2.3166 22.7071 2.70713L9.7071 15.7071C9.31658 16.0977 8.68341 16.0977 8.29289 15.7071L1.29289 8.70713C0.902365 8.3166 0.902365 7.68344 1.29289 7.29291C1.68341 6.90239 2.31658 6.90239 2.7071 7.29291L9 13.5858L21.2929 1.29291C21.6834 0.902388 22.3166 0.902388 22.7071 1.29291Z" strokeWidth="2" />
+        </svg>
+      )}
       <span className="visually-hidden">{label}</span>
     </div>
   );
--- packages/sage-react/lib/Loader/configs.js
+++ packages/sage-react/lib/Loader/configs.js
@@ -1,4 +1,5 @@
 export const LOADER_TYPES = {
   BAR: 'bar',
   SPINNER: 'spinner',
+  SUCCESS: 'success',
 };
