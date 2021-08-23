46116298bf02888f5f7ac969d5a7cf8547257ec4
Ashley E <ashleyne92@gmail.com>
2021-08-23 10:07:45 -0500

    feat(loader): adds new 'success' variant

--- docs/lib/sage_rails/app/sage_components/sage_loader.rb
+++ docs/lib/sage_rails/app/sage_components/sage_loader.rb
@@ -1,6 +1,6 @@
 class SageLoader < SageComponent
   set_attribute_schema({
     fill: [:optional, TrueClass],
-    type: Set.new(["bar", "spinner"]),
+    type: Set.new(["bar", "spinner", "success"]),
   })
 end
