a4d510e80b979e7fa9688036faa040032f2afb28
Phil Schanely <philschanely@gmail.com>
2021-08-25 14:58:08 -0400

    feat(icon): add adjacentType on React component

--- packages/sage-react/lib/Icon/Icon.story.jsx
+++ packages/sage-react/lib/Icon/Icon.story.jsx
@@ -1,61 +1,106 @@
 import React from 'react';
-import { selectArgs } from '../story-support/helpers';
-import { SageClassnames, SageTokens } from '../configs';
-import { Icon } from './Icon';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { SageTokens } from '../configs';
+import { ICON_ADJACENT_TYPES, ICON_CARD_COLORS, ICON_SIZES } from './configs';
 
-export default {
-  title: 'Sage/Icon',
-  component: Icon,
-  argTypes: {
-    ...selectArgs({
-      adjacentType: Icon.ADJACENT_TYPES,
-      cardColor: Icon.CARD_COLORS,
-      color: Icon.COLORS,
-      icon: SageTokens.ICONS,
-      size: Icon.SIZES
-    }),
-  },
-  args: {
-    cardColor: null,
-    circular: false,
-    color: Icon.COLORS.CHARCOAL,
-    icon: Icon.ICONS.CHECK_CIRCLE,
-    label: 'Click me',
-    size: Icon.SIZES.MD
+export const Icon = ({
+  adjacentType,
+  backgroundHeight,
+  backgroundWidth,
+  cardColor,
+  circular,
+  className,
+  color,
+  icon,
+  label,
+  size,
+  ...rest
+}) => {
+  const classNames = classnames(
+    className,
+    {
+      [`sage-icon-${icon}`]: icon && (!size || size === ICON_SIZES.MD),
+      [`sage-icon-${icon}-${size}`]: icon && size,
+      [`t-sage--color-${color}`]: color,
+      [`sage-icon--adjacent-type-${adjacentType}`]: adjacentType,
+    }
+  );
+
+  const wrapperClassNames = classnames(
+    'sage-icon-background',
+    {
+      'sage-icon-background--custom-size': backgroundHeight || backgroundWidth,
+      [`sage-icon-background--${cardColor}`]: cardColor,
+      [`sage-icon-background--${size}`]: size,
+      'sage-icon-background--circular': circular,
+    },
+  );
+
+  const attributes = {};
+  if (!label || label === '') {
+    attributes['aria-hidden'] = true;
+  } else {
+    attributes['aria-label'] = label;
   }
-};
-const Template = (args) => <Icon {...args} />;
 
-export const Default = Template.bind({});
-export const AdjacentType = Template.bind({});
-AdjacentType.decorators = [
-  (Story) => (
-    <>
-      <div style={{ display: 'flex' }}>
-        <Story />
-        <p className={`${SageClassnames.TYPE.HEADING_2} ${SageClassnames.SPACERS.XS_LEFT}`}>
-          I am a Heading 2 type spec
-        </p>
-      </div>
-      <p>
-        Watch how the icon becomes more aligned when you set
-        the <code>adjacentType</code> property to match
-        the type spec to which it is adjacent.
-      </p>
-    </>
-  )
-];
-export const CustomBackgroundSize = Template.bind({});
-CustomBackgroundSize.args = {
-  cardColor: 'draft',
-  backgroundWidth: '96px',
-  backgroundHeight: '48px',
-  size: Icon.SIZES.LG
+  const renderIcon = () => (
+    <i className={classNames} {...attributes} {...rest} />
+  );
+
+  const setBackgroundDimensions = () => {
+    const props = {};
+
+    backgroundHeight = backgroundHeight || backgroundWidth;
+    backgroundWidth = backgroundWidth || backgroundHeight;
+
+    if (circular) {
+      backgroundWidth = backgroundHeight;
+    }
+
+    props['--sage-icon-background-height'] = backgroundHeight;
+    props['--sage-icon-background-width'] = backgroundWidth;
+
+    return props;
+  };
+
+  return cardColor ? (
+    <div
+      style={setBackgroundDimensions()}
+      className={wrapperClassNames}
+    >
+      {renderIcon()}
+    </div>
+  ) : renderIcon();
 };
-export const CustomBackgroundSizeCircular = Template.bind({});
-CustomBackgroundSizeCircular.args = {
-  cardColor: 'draft',
-  circular: true,
-  backgroundHeight: '48px',
-  size: Icon.SIZES.LG
+
+Icon.ADJACENT_TYPES = ICON_ADJACENT_TYPES;
+Icon.CARD_COLORS = ICON_CARD_COLORS;
+Icon.COLORS = SageTokens.COLOR_SLIDERS;
+Icon.ICONS = SageTokens.ICONS;
+Icon.SIZES = ICON_SIZES;
+
+Icon.defaultProps = {
+  adjacentType: null,
+  backgroundHeight: null,
+  backgroundWidth: null,
+  cardColor: null,
+  circular: false,
+  className: '',
+  color: SageTokens.COLOR_SLIDERS.INHERIT,
+  label: '',
+  size: null,
+};
+
+Icon.propTypes = {
+  adjacentType: PropTypes.oneOf(Object.values(Icon.ADJACENT_TYPES)),
+  backgroundHeight: PropTypes.string,
+  backgroundWidth: PropTypes.string,
+  cardColor: PropTypes.oneOf(Object.values(Icon.CARD_COLORS)),
+  circular: PropTypes.bool,
+  className: PropTypes.string,
+  color: PropTypes.oneOf(Object.values(Icon.COLORS)),
+  icon: PropTypes.oneOf(Object.values(Icon.ICONS)).isRequired,
+  label: PropTypes.string,
+  size: PropTypes.oneOf(Object.values(Icon.SIZES)),
 };
