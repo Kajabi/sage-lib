4340bc094784dbd8307ceb08e90d0c15617e347e
Ben Hays <ben@kajabi.com>
2021-08-30 17:08:51 -0700

    feat(panel): added custom class for PanelSubheading components

--- packages/sage-react/lib/Panel/PanelSubheader.jsx
+++ packages/sage-react/lib/Panel/PanelSubheader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { PanelSubtext } from './PanelSubtext';
 import { PanelSubtitle } from './PanelSubtitle';
 
@@ -10,17 +11,36 @@ export const PanelSubheader = ({
   title,
   titleTag,
   ...rest
-}) => (
-  <div className={`sage-panel__subheader ${className || ''}`} {...rest}>
-    {title && (
-      <PanelSubtitle tag={titleTag}>{title}</PanelSubtitle>
-    )}
-    {children}
-    {subtext && (
-      <PanelSubtext>{subtext}</PanelSubtext>
-    )}
-  </div>
-);
+}) => {
+  let hasSubtextChild = false;
+  let childCount = 0;
+  React.Children.forEach(children, (child) => {
+    childCount += 1;
+    if (child.type.name === 'PanelSubtext') hasSubtextChild = true;
+  });
+  let isCustom = false;
+  if ((childCount === 1 && (!hasSubtextChild || !subtext)) || childCount > 1) isCustom = true;
+
+  const classNames = classnames(
+    'sage-panel__subheader',
+    className,
+    {
+      'sage-panel__subheader--custom': isCustom
+    }
+  );
+
+  return (
+    <header className={classNames} {...rest}>
+      {title && (
+        <PanelSubtitle tag={titleTag}>{title}</PanelSubtitle>
+      )}
+      {children}
+      {subtext && (
+        <PanelSubtext>{subtext}</PanelSubtext>
+      )}
+    </header>
+  );
+};
 
 PanelSubheader.defaultProps = {
   children: null,
