0b894b03182c183e9d8846e02fc5c4711bc774e5
Shane Smith <voodoogq@gmail.com>
2021-09-29 16:07:23 -0500

    fix(typescript): updating outputdir/js compilation

--- packages/sage-react/lib/Hint/Hint.tsx
+++ packages/sage-react/lib/Hint/Hint.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Icon } from '../Icon';
-import { SageTokens } from '../configs';
 
 interface Props {
-  className: any,
-  content: any,
-  icon: any,
+  className?: string,
+  content: string,
+  // This would be better served as an ENUM
+  icon: string | null | undefined,
 }
 
 export const Hint: React.FC<Props> = ({
@@ -37,15 +36,4 @@ export const Hint: React.FC<Props> = ({
       <span className="sage-hint__content">{content}</span>
     </div>
   );
-};
-
-Hint.defaultProps = {
-  className: '',
-  icon: null,
-};
-
-Hint.propTypes = {
-  className: PropTypes.string,
-  content: PropTypes.string.isRequired,
-  icon: PropTypes.oneOf(Object.values(SageTokens.ICONS)),
 };
