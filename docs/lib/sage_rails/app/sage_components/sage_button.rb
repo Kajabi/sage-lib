b9c4f6aa3bc7c5514243c921c9699fc97c0cf7a2
Monica Wheeler <monica@kajabi.com>
2021-09-29 16:21:44 -0500

    refactor(button): working on removing tertiary

--- docs/lib/sage_rails/app/sage_components/sage_button.rb
+++ docs/lib/sage_rails/app/sage_components/sage_button.rb
@@ -6,11 +6,10 @@ class SageButton < SageComponent
     disabled: [:optional, NilClass, TrueClass],
     full_width: [:optional, NilClass, TrueClass],
     icon: [:optional, { name: String, style: Set.new(["left", "right", "only"]) }],
-    no_shadow: [:optional, NilClass, TrueClass],
     raised: [:optional, NilClass, TrueClass],
     small: [:optional, NilClass, TrueClass],
     spinner_on_submit: [:optional, NilClass, String],
-    style: [:optional, NilClass, Set.new(["primary", "secondary", "tertiary", "danger"])],
+    style: [:optional, NilClass, Set.new(["primary", "secondary", "danger"])],
     subtle: [:optional, NilClass, TrueClass],
     value: [:optional, String],
   })
