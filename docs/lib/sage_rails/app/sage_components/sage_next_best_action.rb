7da15b91622e41a9a742168d9e7b6f42aa44c263
Ben Hays <ben@kajabi.com>
2021-10-12 12:49:16 -0700

    feat(nba): began simple styling and markup additions

--- docs/lib/sage_rails/app/sage_components/sage_next_best_action.rb
+++ docs/lib/sage_rails/app/sage_components/sage_next_best_action.rb
@@ -1,5 +1,7 @@
 class SageNextBestAction < SageComponent
   set_attribute_schema({
-    
+    color: [:optional, SageSchemas::STATUSES],
+    desc: [:optional, String],
+    title: [:optional, String],
   })
 end
