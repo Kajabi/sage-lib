e9139c3ad30c41836ee1bf5bdf141769d6a16a1a
Ben Hays <ben@kajabi.com>
2021-10-12 12:55:52 -0700

    feat(nba): added button options, renamed color to card_color

--- docs/lib/sage_rails/app/sage_components/sage_next_best_action.rb
+++ docs/lib/sage_rails/app/sage_components/sage_next_best_action.rb
@@ -1,6 +1,8 @@
 class SageNextBestAction < SageComponent
   set_attribute_schema({
-    color: [:optional, SageSchemas::STATUSES],
+    button_style: [:optional, NilClass, Set.new(["primary", "secondary", "danger"])],
+    button_text: [:optional, String],
+    card_color: [:optional, SageSchemas::STATUSES],
     desc: [:optional, String],
     title: [:optional, String],
   })
