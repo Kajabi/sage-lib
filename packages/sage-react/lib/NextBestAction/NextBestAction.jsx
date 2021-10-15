7ad61d73b4c1e567033f298e12628b07aea27fa3
Ben Hays <ben@kajabi.com>
2021-10-15 10:31:26 -0700

    feat(nba): added basic markup for React component

--- packages/sage-react/lib/NextBestAction/NextBestAction.jsx
+++ packages/sage-react/lib/NextBestAction/NextBestAction.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-export const NextBestAction = () => {
-  const thingy = 'Hello, world!';
-  return (
-    <div>{thingy}</div>
+export const NextBestAction = ({ className }) => {
+  const baseClass = 'sage-next-best-action';
+
+  const classNames = classnames(
+    baseClass,
+    className,
+    {}
   );
+
+  return (
+    <div className={classNames}>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+NextBestAction.defaultProps = {
+  className: '',
+};
+
+NextBestAction.propTypes = {
+  className: PropTypes.string,
 };
