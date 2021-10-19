c82c42f677127002baa115bf58772aefdd1fcbef
Ashley E <ashleyne92@gmail.com>
2021-10-19 17:07:34 -0500

    feat(copy text): adds borderless variation copy button

--- docs/lib/sage_rails/app/sage_components/sage_copy_button.rb
+++ docs/lib/sage_rails/app/sage_components/sage_copy_button.rb
@@ -1,5 +1,6 @@
 class SageCopyButton < SageComponent
   set_attribute_schema({
+    borderless: [:optional, TrueClass],
     fill_container: [:optional, TrueClass],
     semibold: [:optional, TrueClass],
     value: String,
