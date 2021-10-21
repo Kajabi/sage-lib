a7006a338f0467796b480884888f59e2f199f251
Phil Schanely <philschanely@gmail.com>
2021-10-21 17:36:39 -0400

    feat(next-best-actions): add dismiss_actions

--- docs/lib/sage_rails/app/sage_components/sage_next_best_action.rb
+++ docs/lib/sage_rails/app/sage_components/sage_next_best_action.rb
@@ -2,6 +2,7 @@ class SageNextBestAction < SageComponent
   set_attribute_schema({
     card_color: [:optional, SageSchemas::STATUSES],
     dismissable: [:optional, TrueClass],
+    dismiss_attributes: [:optional, NilClass, Hash],
     graphic: [:optional, {
       element: [:optional, String],
       on_right: [:optional, TrueClass],
