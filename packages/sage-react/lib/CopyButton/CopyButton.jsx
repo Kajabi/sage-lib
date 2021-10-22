b2a7f0bfab0ea41651ed4a9e564eac8934123889
Ashley E <ashleyne92@gmail.com>
2021-10-22 16:37:25 -0500

    feat(copy text): adds borderless React prop

--- packages/sage-react/lib/CopyButton/CopyButton.jsx
+++ packages/sage-react/lib/CopyButton/CopyButton.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Button } from '../Button';
