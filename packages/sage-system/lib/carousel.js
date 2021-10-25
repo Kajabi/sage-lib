57db06507588c9234e4d98393c41438c28943c45
Ben Hays <ben@kajabi.com>
2021-10-25 14:38:53 -0700

    feat(carousel): got the appending to slide container div working

--- packages/sage-system/lib/carousel.js
+++ packages/sage-system/lib/carousel.js
@@ -2,14 +2,32 @@ import { tns } from "tiny-slider/src/tiny-slider";
 
 Sage.carousel = (function() {
 
+  const containerClass = '.my-slider';
+
   function init(el) {
+    const slider = document.querySelector(containerClass);
+    const slides = slider.children;
+
+    const slidesArray = [...slides];
+
+    console.log('slider:', slider);
+    console.log('slides:', slides);
+    console.log('slidesArray:', slidesArray);
+    slidesArray.forEach((slide) => {
+      console.log('slide:', slide);
+      let slideContainer = document.createElement('div');
+      slideContainer.classList.add('slide');
+      slideContainer.appendChild(slide);
+      slider.appendChild(slideContainer);
+    });
+
     const options = JSON.parse(document.querySelector('.sage-carousel').getAttribute('data-js-carousel-options'));
-    let mySlider = tns(
-      {
-        ...options,
-        container: '.my-slider',
-      }
-    );
+    // let mySlider = tns(
+    //   {
+    //     ...options,
+    //     container: containerClass,
+    //   }
+    // );
   }
 
   return {
