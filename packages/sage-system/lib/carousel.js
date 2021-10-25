c0e1a8a829e30daaa64dc347aea2ec0c5b491ea6
Ben Hays <ben@kajabi.com>
2021-10-25 11:09:41 -0700

    feat(carousel): got basic markup with content_for going

--- packages/sage-system/lib/carousel.js
+++ packages/sage-system/lib/carousel.js
@@ -1,8 +1,21 @@
+import { tns } from "tiny-slider/src/tiny-slider";
+
 Sage.carousel = (function() {
 
   function init(el) {
-    console.log('carousel!');
+    console.log('carousel!', tns);
+    let mySlider = tns({
+      autoWidth: true,
+      container: '.my-slider',
+      items: 3,
+      loop: false,
+      mouseDrag: true,
+      swipeAngle: false,
+      speed: 400
+    });
   }
+
+  // if (...) speed = data-js-carousel-speed
 
   return {
     init,
