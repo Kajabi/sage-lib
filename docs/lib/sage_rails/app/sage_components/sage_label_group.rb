feea7467fe97671ac669cde7e134338340d0a273
Ashley E <ashleyne92@gmail.com>
2021-10-26 12:03:40 -0500

    feat(label): adds wrap, align, and gap props for label group

--- docs/lib/sage_rails/app/sage_components/sage_label_group.rb
+++ docs/lib/sage_rails/app/sage_components/sage_label_group.rb
@@ -1,2 +1,7 @@
 class SageLabelGroup < SageComponent
+	set_attribute_schema({
+		align: [:optional, Set.new(["center", "space-between", "end"])],
+		gap: [:optional, Set.new([:xs, :sm, :md, :lg])],
+		wrap: [:optional, TrueClass]
+	})
 end
