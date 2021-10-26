abd242a8c3bdf5fe0d93d2e411f3c1755a0ac777
Ben Hays <ben@kajabi.com>
2021-10-26 16:21:50 -0700

    feat(carousel): got carousel working with useEffect

--- packages/sage-react/lib/Carousel/Carousel.story.jsx
+++ packages/sage-react/lib/Carousel/Carousel.story.jsx
@@ -1,11 +1,73 @@
 import React from 'react';
 import { Carousel } from './Carousel';
+import { Button } from '../Button';
+import { NextBestAction } from '../NextBestAction';
 
 export default {
   title: 'Sage/Carousel',
   component: Carousel,
   args: {
-    options: 'Benzo',
+    children: (
+      <>
+        <NextBestAction
+          actions={(
+            <Button
+              color={Button.COLORS.PRIMARY}
+            >
+              Add an Upsell
+            </Button>
+          )}
+          cardColor="draft"
+          description={(
+            <p>Lorem ipsum dolor sit amet.</p>
+          )}
+          graphic={{
+            element: (<img src="//source.unsplash.com/random/272x272" alt="" />)
+          }}
+          title="Hello, world"
+        />
+        <NextBestAction
+          actions={(
+            <Button
+              color={Button.COLORS.PRIMARY}
+            >
+              Add an Upsell
+            </Button>
+          )}
+          cardColor="draft"
+          description={(
+            <p>Lorem ipsum dolor sit amet.</p>
+          )}
+          graphic={{
+            element: (<img src="//source.unsplash.com/random/272x272" alt="" />)
+          }}
+          title="Hello, world"
+        />
+        <NextBestAction
+          actions={(
+            <Button
+              color={Button.COLORS.PRIMARY}
+            >
+              Add an Upsell
+            </Button>
+          )}
+          cardColor="draft"
+          description={(
+            <p>Lorem ipsum dolor sit amet.</p>
+          )}
+          graphic={{
+            element: (<img src="//source.unsplash.com/random/272x272" alt="" />)
+          }}
+          title="Hello, world"
+        />
+      </>
+    ),
+    options: {
+      fixedWidth: 544,
+      gutter: 24,
+      loop: false,
+      mouseDrag: true,
+    },
   },
 };
 const Template = (args) => <Carousel {...args} />;
