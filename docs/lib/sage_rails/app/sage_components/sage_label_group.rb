2482ae73ac245e6b7d1dd6b1fe63afa2bff73269
Ashley E <ashleyne92@gmail.com>
2021-10-28 11:30:14 -0500

    feat(label): adds styling for new props

--- docs/lib/sage_rails/app/sage_components/sage_label_group.rb
+++ docs/lib/sage_rails/app/sage_components/sage_label_group.rb
@@ -2,6 +2,5 @@ class SageLabelGroup < SageComponent
 	set_attribute_schema({
 		align: [:optional, Set.new(["center", "space-between", "end"])],
 		gap: [:optional, Set.new([:xs, :sm, :md, :lg])],
-		wrap: [:optional, TrueClass]
 	})
 end
