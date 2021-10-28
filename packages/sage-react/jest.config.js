f549ef505da172385af17e153faf5569d63e26c2
Ben Hays <ben@kajabi.com>
2021-10-28 11:52:25 -0700

    fix(carousel): fixed yarn test with tiny-slider import

--- packages/sage-react/jest.config.js
+++ packages/sage-react/jest.config.js
@@ -176,9 +176,10 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-    'node_modules/(?!tiny-slider/)'
-  ],
+  // transformIgnorePatterns: [
+  //   "/node_modules/",
+  //   "\\.pnp\\.[^\\/]+$"
+  // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
