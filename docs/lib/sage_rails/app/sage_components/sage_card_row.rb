e75e0f73ceda466fae0064d080439bbb4d56b147
Phillip Lovelace <phillip@pixelflips.com>
2021-11-01 13:59:12 -0700

    fix(sortablecustomitem): add gap

--- docs/lib/sage_rails/app/sage_components/sage_card_row.rb
+++ docs/lib/sage_rails/app/sage_components/sage_card_row.rb
@@ -1,7 +1,7 @@
 class SageCardRow < SageComponent
   set_attribute_schema({
-    grid_template: [:optional, SageSchemas::GRID_TEMPLATE],
-    gap: [:optional, Set.new([:xs, :sm, :md, :lg])],
+    grid_template: [:optional, NilClass, SageSchemas::GRID_TEMPLATE],
+    gap: [:optional, NilClass, Set.new([:xs, :sm, :md, :lg])],
     vertical_align: [:optional, String],
   })
 end
--- docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
+++ docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
@@ -2,6 +2,7 @@ class SageSortableItemCustom < SageComponent
   set_attribute_schema({
     card: [:optional, TrueClass],
     grid_template: String,
+    gap: [:optional, String],
     id: [:optional, Integer, String],
     url_update: [:optional, String],
   })
