84dcbe8ed1aea699dbd727db19ad6fe2ad99893d
Phillip Lovelace <phillip@pixelflips.com>
2021-11-01 12:43:54 -0700

    fix(panelrow): add gap attribute rails

--- docs/lib/sage_rails/app/sage_components/sage_panel_row.rb
+++ docs/lib/sage_rails/app/sage_components/sage_panel_row.rb
@@ -1,6 +1,7 @@
 class SagePanelRow < SageComponent
   set_attribute_schema({
     grid_template: [:optional, SageSchemas::GRID_TEMPLATE],
+    gap: [:optional, Set.new([:xs, :sm, :md, :lg])],
     vertical_align: [:optional, Set.new(["start"])],
   })
 end
