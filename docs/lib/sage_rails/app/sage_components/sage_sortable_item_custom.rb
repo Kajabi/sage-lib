5965d9ae69ca00bfd2fcbf994c4089c267364bee
Phillip Lovelace <phillip@pixelflips.com>
2021-11-02 15:15:00 -0700

    fix(sortableitemcustom): adds optional to grid template

--- docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
+++ docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
@@ -1,7 +1,7 @@
 class SageSortableItemCustom < SageComponent
   set_attribute_schema({
     card: [:optional, TrueClass],
-    grid_template: String,
+    grid_template: [:optional, String],
     gap: [:optional, String],
     id: [:optional, Integer, String],
     url_update: [:optional, String],
