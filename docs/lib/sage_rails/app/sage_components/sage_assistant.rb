54ce9a07b151b60d6c02df6b3e096aa8e937f217
Court McFadzean <court@kajabi.com>
2021-11-03 10:59:07 -0700

    feat(assistant): removing Img attributes
    Not longer need images in the assistant
    
    BREAKING CHANGE: Need To remove included images

--- docs/lib/sage_rails/app/sage_components/sage_assistant.rb
+++ docs/lib/sage_rails/app/sage_components/sage_assistant.rb
@@ -1,9 +1,5 @@
 class SageAssistant < SageComponent
   set_attribute_schema({
-    logo: [:optional, {
-      path: String,
-      method: [:optional, NilClass, Set.new(["pack"])],
-    }],
     menu_button: [:optional, TrueClass],
     menu_button_controls: [:optional, String],
   })
