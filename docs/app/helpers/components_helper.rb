52e060f099d0f65eaa61fe53cd5f69f31e5cbf8c
Court McFadzean <court@kajabi.com>
2021-11-03 14:29:40 -0700

    docs(layout): removing components that are only for overall layout use
    these components are not necessary for ongoing refrence and can be better illustrated in other ways.

--- docs/app/helpers/components_helper.rb
+++ docs/app/helpers/components_helper.rb
@@ -24,16 +24,6 @@ module ComponentsHelper
         figma_embed: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F9Km09NjlZHYWsMP7EGT8tI%2F%255BWIP%255D-Sage-3-%25E2%2580%2594-Admin-Components%3Fnode-id%3D6989%253A21570",
       },
       {
-        title: "assistant",
-        description: "Page header with optional side menu toggle button.",
-        scss: "done",
-        docs: "done",
-        rails: "doing",
-        react: "todo",
-        a11y: "todo",
-        figma_embed: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F9Km09NjlZHYWsMP7EGT8tI%2F%255BWIP%255D-Sage-3-%25E2%2580%2594-Admin-Components%3Fnode-id%3D6989%253A21590",
-      },
-      {
         title: "avatar",
         description: "The Avatar component shows a user's profile image in a circular frame and allows for a few helpful modifications for different uses.",
         scss: "done",
@@ -688,16 +678,6 @@ module ComponentsHelper
         a11y: "done",
         react_component_slug: "sage-popover--default",
         figma_embed: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F9Km09NjlZHYWsMP7EGT8tI%2F%255BWIP%255D-Sage-3-%25E2%2580%2594-Admin-Components%3Fnode-id%3D6999%253A23138",
-      },
-      {
-        title: "sidebar",
-        description: "The sidebar component is a fixed, vertical panel that typica11y displays navigation and/or page hierarchy.",
-        scss: "done",
-        docs: "done",
-        rails: "done",
-        react: "no",
-        a11y: "todo",
-        figma_embed: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F9Km09NjlZHYWsMP7EGT8tI%2F%255BWIP%255D-Sage-3-%25E2%2580%2594-Admin-Components%3Fnode-id%3D6999%253A22305",
       },
       {
         title: "sortable",
