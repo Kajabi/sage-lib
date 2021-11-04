abe7f4ec0e121113ca9334c825055d5f22fe6bfb
Phil Schanely <philschanely@gmail.com>
2021-11-04 15:20:11 -0400

    feat(drawer): add React component synced to Rails one

---
+++ packages/sage-react/lib/Drawer/Drawer.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Modal } from '../Modal';
+import { Button } from '../Button';
+import { SageTokens } from '../configs';
+
+export const Drawer = ({
+  active,
+  children,
+  className,
+  customHeader,
+  onExit,
+  showClose,
+  title,
+}) => {
+
+  return (
+    <Modal
+      active={active}
+      animation={{ direction: Modal.ANIMATION_DIRECTIONS.LEFT }}
+      className={`sage-drawer ${className || ''}`}
+      disableBackgroundDismiss={true}
+    >
+      <Modal.Header
+        customHeader={customHeader ? (
+          <div class="sage-drawer__header">{customHeader}</div>
+        ) : null}
+        title={title}
+        aside={showClose && (
+          <Button
+            className="sage-drawer__close"
+            color={Button.COLORS.SECONDARY}
+            iconOnly={true}
+            icon={SageTokens.ICONS.REMOVE}
+            onClick={onExit}
+            subtle={true}
+          >
+            Close Drawer
+          </Button>
+        )}
+      />
+      <Modal.Body spacing={Modal.Body.SPACINGS.PANEL}>
+        {children}
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+Drawer.defaultProps = {
+  active: false,
+  children: null,
+  className: null,
+  customHeader: null,
+  onExit: (val) => val,
+  showClose: true,
+  title: null,
+};
+
+Drawer.propTypes = {
+
+};
--- packages/sage-react/lib/Drawer/Drawer.story.jsx
+++ packages/sage-react/lib/Drawer/Drawer.story.jsx
@@ -1,11 +1,67 @@
 import React from 'react';
 import { Drawer } from './Drawer';
+import { SageClassnames, SageTokens } from '../configs';
+import { Button } from '../Button';
+
+const drawerChildren = (
+  <>
+    <h3 className={`${SageClassnames.TYPE.HEADING_4} ${SageClassnames.TYPE_COLORS.CHARCOAL_500}`}>
+      Drawer
+    </h3>
+    <p>👋  Any content can go here.</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Esse quam assumenda a ut, architecto rerum vel quisquam, repellendus nemo quo saepe at voluptatem eveniet earum laudantium nostrum quibusdam?
+      Repellendus, repudiandae.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Eum in deleniti doloribus sequi ipsam unde pariatur perspiciatis ex molestias temporibus accusantium fuga debitis, voluptates hic eos ab perferendis ad quo.
+    </p>
+  </>
+);
+
+const drawerCustomHeader = (
+  <Button
+    color={Button.COLORS.SECONDARY}
+    subtle={true}
+    icon={SageTokens.ICONS.EXPAND}
+    iconPosition={Button.ICON_POSITIONS.LEFT}
+  >
+    Open full profile
+  </Button>
+);
 
 export default {
   title: 'Sage/Drawer',
   component: Drawer,
-  args: {}
+  args: {
+    active: true,
+    children: drawerChildren,
+    customHeader: drawerCustomHeader,
+    showClose: true,
+  }
 };
 const Template = (args) => <Drawer {...args} />;
 
 export const Default = Template.bind({});
+
+export const WiredExample = () => {
+  const [drawerActive, setDrawerActive] = React.useState(false);
+
+  return (
+    <>
+      <Button
+        color={Button.COLORS.PRIMARY}
+        onClick={() => setDrawerActive(!drawerActive)}
+      >
+        Toggle drawer
+      </Button>
+      <Drawer
+        active={drawerActive}
+        onExit={() => setDrawerActive(false)}
+        customHeader={drawerCustomHeader}
+      >
+        {drawerChildren}
+      </Drawer>
+    </>
+  )
+};
