74d94661c7c5efca1083eeb597063ab2988851a9
Quinton Jason <quinton.jason@gmail.com>
2021-11-04 13:12:57 -0500

    fix(sortable): add gap property

--- packages/sage-react/lib/Sortable/configs.js
+++ packages/sage-react/lib/Sortable/configs.js
@@ -2,3 +2,13 @@ export const SORTABLE_ITEM_TYPES = {
   DEFAULT: null,
   CARD: 'card',
 };
+
+export const SORTABLE_ROW_GAP_OPTIONS = {
+  DEFAULT: null,
+  XS: 'xs',
+  SM: 'sm',
+  MD: 'md',
+  LG: 'lg',
+};
+
+
