8ba933b7b070e3596307e3454f62608febc25e1d
Ben Hays <ben@kajabi.com>
2021-11-18 12:36:57 -0800

    feat(classnames): refactored to DRY up the validation and classname appending

--- docs/lib/sage_rails/app/helpers/sage_classnames_helper.rb
+++ docs/lib/sage_rails/app/helpers/sage_classnames_helper.rb
@@ -3,27 +3,37 @@ include SageTokens
 ALIGNMENTS = ["left", "center", "right", "justify"]
 
 module SageClassnamesHelper
+
+  def tokens
+    [
+      {
+        key_name: :align,
+        arr: ALIGNMENTS,
+        class_prefix: "t-sage--align-",
+      },
+      {
+        key_name: :color,
+        arr: COLOR_SLIDERS,
+        class_prefix: "t-sage--color-",
+      },
+      {
+        key_name: :type_spec,
+        arr: TYPE_SPECS,
+        class_prefix: "t-sage-",
+      }
+    ]
+  end
+
   def sage_classnames(obj)
     classes = ""
-    if obj.key?(:align)
-      if ALIGNMENTS.include?(obj[:align])
-        classes += "t-sage--align-#{obj[:align]} "
-      else
-        raise "align value is not valid"
-      end
-    end
-    if obj.key?(:color)
-      if COLOR_SLIDERS.include?(obj[:color])
-        classes += "t-sage--color-#{obj[:color]} "
-      else
-        raise "color value is not valid"
-      end
-    end
-    if obj.key?(:type_spec)
-      if TYPE_SPECS.include?(obj[:type_spec])
-        classes += "t-sage-#{obj[:type_spec]} "
-      else
-        raise "type_spec value is not valid"
+    tokens.each_with_index do |token, index|
+      kn = token[:key_name]
+      if obj.key?(kn)
+        if token[:arr].include?(obj[kn])
+          classes += token[:class_prefix].to_s + obj[kn] + " "
+        else
+          raise "#{kn.to_s} value is not valid"
+        end
       end
     end
     classes
