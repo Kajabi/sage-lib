87d43b7c7bb3e7c7a5602e0856bca85e7e9cecca
Ben Hays <ben@kajabi.com>
2021-11-18 13:10:51 -0800

    feat(classnames): added text_modifier and spacer options

--- docs/lib/sage_rails/app/helpers/sage_classnames_helper.rb
+++ docs/lib/sage_rails/app/helpers/sage_classnames_helper.rb
@@ -2,6 +2,20 @@ include SageTokens
 
 ALIGNMENTS = ["left", "center", "right", "justify"]
 
+SPACERS = [
+  "top", "right", "bottom", "left",
+  "top-3xs", "right-3xs", "bottom-3xs", "left-3xs",
+  "top-2xs", "right-2xs", "bottom-2xs", "left-2xs",
+  "top-xs", "right-xs", "bottom-xs", "left-xs",
+  "top-sm", "right-sm", "bottom-sm", "left-sm",
+  "top-md", "right-md", "bottom-md", "left-md",
+  "top-lg", "right-lg", "bottom-lg", "left-lg",
+  "top-xl", "right-xl", "bottom-xl", "left-xl",
+  "top-2xl", "right-2xl", "bottom-2xl", "left-2xl",
+]
+
+TEXT_MODIFIERS = ["truncate", "strikethrough"]
+
 module SageClassnamesHelper
 
   def tokens
@@ -15,6 +29,16 @@ module SageClassnamesHelper
         key_name: :color,
         arr: COLOR_SLIDERS,
         class_prefix: "t-sage--color-",
+      },
+      {
+        key_name: :spacer,
+        arr: SPACERS,
+        class_prefix: "sage-spacer-",
+      },
+      {
+        key_name: :text_modifier,
+        arr: TEXT_MODIFIERS,
+        class_prefix: "t-sage--",
       },
       {
         key_name: :type_spec,
