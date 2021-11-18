258b8bcd90f8f0e23db89662b54ba6e8e103982f
Ben Hays <ben@kajabi.com>
2021-11-18 09:46:35 -0800

    feat(classnames): added conditionals and array checking

--- docs/lib/sage_rails/app/helpers/sage_classnames_helper.rb
+++ docs/lib/sage_rails/app/helpers/sage_classnames_helper.rb
@@ -1,5 +1,27 @@
+TYPE_SPECS = [
+  "heading-1", "heading-2", "heading-3", "heading-4", "heading-5", "heading-6",
+  "nav", "nav-sub",
+  "body", "body-med", "body-semi", "body-bold",
+  "body-small", "body-small-med", "body-small-semi", "body-small-bold",
+  "body-xsmall", "body-xsmall-med", "body-xsmall-semi", "body-xsmall-bold",
+]
+
 module SageClassnamesHelper
   def sage_classnames(obj)
-    "t-sage--color-#{obj[:color]} t-sage--align-#{obj[:align]}"
+    text = ""
+    if obj.key?(:color)
+      text += "t-sage--color-#{obj[:color]} "
+    end
+    if obj.key?(:align)
+      text += "t-sage--align-#{obj[:align]} "
+    end
+    if obj.key?(:type_spec)
+      if TYPE_SPECS.include?(obj[:type_spec])
+        text += "t-sage-#{obj[:type_spec]} "
+      else
+        raise "type_spec is not valid"
+      end
+    end
+    text
   end
 end
