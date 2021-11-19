fbe2190f54c599ae193e08360df4a732a1852501
Ashley E <ashleyne92@gmail.com>
2021-11-19 10:17:44 -0600

    feat(description): removes title, data, link props for deprecation

--- docs/lib/sage_rails/app/sage_components/sage_description.rb
+++ docs/lib/sage_rails/app/sage_components/sage_description.rb
@@ -2,15 +2,12 @@ class SageDescription < SageComponent
   # TODO: simplify to items only (remove data, link, title from root)
   # once instances are converted to the lists format.
   set_attribute_schema({
-    data: [:optional, NilClass, String, Integer, Hash],
     id: [:optional, String],
     inline_spread: [:optional, TrueClass],
-    link: [:optional, String],
     items: [:optional, [[{
       data: [:optional, NilClass, String, Integer, Hash],
       link: [:optional, String],
       title: [:optional, String],
     }]]],
-    title: [:optional, String],
   })
 end
