d8953e83ac31de4713a5d978f7dcf31e9abd03ce
Monica Wheeler <monica@kajabi.com>
2021-11-29 14:50:45 -0600

    feat(type): add conditionals for headings and content

--- docs/lib/sage_rails/app/sage_components/sage_type.rb
+++ docs/lib/sage_rails/app/sage_components/sage_type.rb
@@ -1,6 +1,6 @@
 class SageType < SageComponent
   set_attribute_schema({
-    tag: String,
+    tag: [:optional, String],
     type_spec: [:optional, String, Set.new(SageTokens::TYPE_SPECS)],
     color: [:optional, SageSchemas::COLOR_SLIDER],
     alignment: [:optional, SageSchemas::HORIZONAL_ALIGNMENT],
