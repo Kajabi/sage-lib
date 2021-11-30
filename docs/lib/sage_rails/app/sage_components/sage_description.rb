14e5d47ff99aa5a4b56ce256e4e1442102de3871
Phil Schanely <philschanely@gmail.com>
2021-11-30 16:57:12 -0500

    feat(description): sync up React component

--- docs/lib/sage_rails/app/sage_components/sage_description.rb
+++ docs/lib/sage_rails/app/sage_components/sage_description.rb
@@ -3,14 +3,15 @@ class SageDescription < SageComponent
     action_width: [:optional, String],
     id: [:optional, String],
     items: [:optional, [[{
-      data: [:optional, NilClass, String, Integer, Hash],
-      title: [:optional, String],
       action: [:optional, {
         attributes: [:optional, NilClass, Hash],
         icon_only: [:optional, NilClass, TrueClass],
         test_id: [:optional, NilClass, String],
         value: [:optional, NilClass, String],
       }],
+      data: [:optional, NilClass, String, Integer, Hash],
+      hide_title: [:optional, NilClass, TrueClass],
+      title: [:optional, String],
     }]]],
     layout: [:optional, Set.new(["inline", "stacked"])],
     title_width: [:optional, String],
--- packages/sage-react/lib/Description/Description.story.jsx
+++ packages/sage-react/lib/Description/Description.story.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { selectArgs } from '../story-support/helpers';
 import { Description } from './Description';
+import { Label } from '../Label';
 
 export default {
   title: 'Sage/Description',
@@ -28,49 +29,79 @@ export const MultipleItems = Template.bind({});
 MultipleItems.args = {
   items: [
     {
-      title: 'Name',
-      data: 'John Doe'
+      title: 'Short item',
+      data: 'John Doe',
     },
     {
-      title: 'Name',
-      data: 'John Doe'
+      title: 'Wrapping item',
+      data: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim fringilla eros, at faucibus mauris aliquam at.',
     },
     {
-      title: 'Name',
-      data: 'John Doe'
+      title: 'Item with hidden title',
+      hide_title: true,
+      data: 'This item\'s title is hidden. Suspendisse potenti. Pellentesque rhoncus nisi ut ultricies facilisis. Praesent facilisis pretium posuere. Sed odio risus, dignissim et augue sed, dapibus suscipit nisl. Donec sed auctor lorem. Curabitur ac fermentum est. Donec eget ultrices nisl, non rutrum sapien. Maecenas tempus aliquet mollis. Etiam efficitur sit amet ligula nec tincidunt. Nulla faucibus nunc ac odio facilisis, sollicitudin consequat augue euismod.',
     },
     {
-      title: 'Name',
-      data: 'John Doe'
-    },
+      title: 'Item with HTML content',
+      data: (
+        <Label.Group>
+          <Label value="Lorem" color="draft" />
+          <Label value="Ipsum" color="draft" />
+          <Label value="Dolor" color="draft" />
+          <Label value="Sit" color="draft" />
+          <Label value="Amet" color="draft" />
+          <Label value="consectetur" color="draft" />
+          <Label value="adipiscing" color="draft" />
+          <Label value="elit" color="draft" />
+          <Label value="Duis" color="draft" />
+          <Label value="dignissim" color="draft" />
+          <Label value="fringilla" color="draft" />
+          <Label value="eros" color="draft" />
+        </Label.Group>
+      ),
+    }
   ]
 };
 
 export const MultipleItemsWithActionButton = Template.bind({});
 MultipleItemsWithActionButton.args = {
+  actionWidth: '108px',
   items: [
     {
-      title: 'Name',
-      data: 'John Doe',
+      title: 'No action',
+      data: 'Action buttons are not required in every row.',
+    },
+    {
+      title: 'Default action',
+      data: 'Default actions show just an arrow icon.',
       action: {
-        value: 'Button',
+        attributes: { href: '#' }
       }
     },
     {
-      title: 'Name',
-      data: 'John Doe',
+      title: 'Default action showing label',
+      data: 'Set `icon_only: false` to show the default label.',
       action: {
-        value: 'Button',
-        iconOnly: true
+        iconOnly: false,
+        attributes: { href: '#' }
       }
     },
     {
-      title: 'Name',
-      data: 'John Doe'
+      title: 'Custom labeled action',
+      data: 'Actions can be set to use a label. In such cases an custom action width may also be needed.',
+      action: {
+        value: 'Details',
+        attributes: { href: '#' }
+      }
     },
     {
-      title: 'Name',
-      data: 'John Doe'
+      title: 'Custom labeled action visually hidden',
+      data: 'Custom labels can also be made visually hidden with `value` set but `icon_only: true`',
+      action: {
+        value: 'Details',
+        iconOnly: true,
+        attributes: { href: '#' }
+      }
     },
   ]
 };
@@ -80,39 +111,54 @@ StackedLayout.args = {
   layout: Description.LAYOUT.STACKED,
   items: [
     {
-      title: 'Name',
+      title: 'Short item',
       data: 'John Doe',
-      action: {
-        value: 'Button',
-      },
     },
     {
-      title: 'Name',
-      data: 'John Doe',
+      title: 'Wrapping item',
+      data: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim fringilla eros, at faucibus mauris aliquam at.',
     },
+    {
+      title: 'Item with hidden title',
+      hide_title: true,
+      data: 'This item\'s title is hidden. Suspendisse potenti. Pellentesque rhoncus nisi ut ultricies facilisis. Praesent facilisis pretium posuere. Sed odio risus, dignissim et augue sed, dapibus suscipit nisl. Donec sed auctor lorem. Curabitur ac fermentum est. Donec eget ultrices nisl, non rutrum sapien. Maecenas tempus aliquet mollis. Etiam efficitur sit amet ligula nec tincidunt. Nulla faucibus nunc ac odio facilisis, sollicitudin consequat augue euismod.',
+    },
+    {
+      title: 'Item with HTML content',
+      data: (
+        <Label.Group>
+          <Label value="Lorem" color="draft" />
+          <Label value="Ipsum" color="draft" />
+          <Label value="Dolor" color="draft" />
+          <Label value="Sit" color="draft" />
+          <Label value="Amet" color="draft" />
+          <Label value="consectetur" color="draft" />
+          <Label value="adipiscing" color="draft" />
+          <Label value="elit" color="draft" />
+          <Label value="Duis" color="draft" />
+          <Label value="dignissim" color="draft" />
+          <Label value="fringilla" color="draft" />
+          <Label value="eros" color="draft" />
+        </Label.Group>
+      ),
+    }
   ]
 };
 
 export const CustomTitleAndActionWidths = Template.bind({});
 CustomTitleAndActionWidths.args = {
-  actionWidth: '64px',
+  actionWidth: '88px',
   titleWidth: '128px',
   items: [
     {
       title: 'Very Long Title',
       data: 'John Doe',
-      action: {
-        value: 'Button',
-        iconOnly: true,
-      }
+      action: { value: 'More' }
     },
     {
       title: 'Name',
       data: 'John Doe',
-      action: {
-        value: 'Button',
-        iconOnly: true,
-      }
+      action: { value: 'Preview' }
     },
   ]
 };
