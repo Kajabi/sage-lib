a98a7070b2500a963aede66a4a5672151b93678a
joedakroub <joe.d@kajabi.com>
2021-12-10 14:45:24 -0700

    feat(alert): added presence listener for alerts
    When dynamically adding alerts, the close button click handler does not get added. This adds a
    presence listener for alerts to address this issue.

--- packages/sage-system/lib/alert.js
+++ packages/sage-system/lib/alert.js
@@ -3,20 +3,30 @@ Sage.alert = (function () {
   // Variables
   // ==================================================
 
-  var alertCloseBtns = document.querySelectorAll(".sage-alert__close");
+  const SELECTOR_ALERT = ".sage-alert";
+  const SELECTOR_ALERT_CLOSE = ".sage-alert__close";
+
+  var alertCloseBtns = document.querySelectorAll(SELECTOR_ALERT_CLOSE);
 
   // ==================================================
   // Functions
   // ==================================================
 
   alertCloseBtns.forEach(function (btn) {
-    var alert = btn.closest(".sage-alert");
-    btn.addEventListener("click", function () {
-      alert.style.display = "none";
-    });
+    addCloseHandler(btn);
   });
 
-  function init() {}
+  function init(el) {
+    addCloseHandler(el);
+  }
+
+  function addCloseHandler(el) {
+    var alert = el.closest(SELECTOR_ALERT);
+    var closeBtn = alert.querySelector(SELECTOR_ALERT_CLOSE);
+    closeBtn.addEventListener("click", function () {
+      alert.style.display = "none";
+    });
+  }
 
   return {
     init: init,
