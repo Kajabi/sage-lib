5408fbbb526655ea45d572b10b892796411f853b
Phil Schanely <philschanely@gmail.com>
2022-01-03 15:42:57 -0500

    feat: implement grid templates using style dictionary

--- packages/sage-react/lib/configs/tokens/grid-templates.js
+++ packages/sage-react/lib/configs/tokens/grid-templates.js
@@ -1,57 +1,6 @@
-// Sage Grid Template codes.
-// See https://sage-design-system.kajabi.com/pages/foundations/grid_templates
-// Space linting disabled here to aid in visual recognition of patterns.
-/* eslint-disable no-multi-spaces, key-spacing */
-export const TOKENS_GRID_TEMPLATES = {
-  M:   'm',   // --
-  O:   'o',   // ---
-  OT:  'ot',  // ----
-  OM:  'om',  // -----
-  OO:  'oo',  // ------
-  ET:  'et', // .-
-  EM:  'em', // .--
-  EO:  'eo', // .---
-  IT:  'it', // ..-
-  IM:  'im', // ..--
-  IO:  'io', // ..---
-  SE:  'se', // ...-
-  SM:  'sm', // ...--
-  SO:  'so', // ...---
-  TE:  'te', // -.
-  TI:  'ti', // -..
-  TS:  'ts', // -...
-  ME:  'me', // --.
-  MI:  'mi', // --..
-  MS:  'ms', // --...
-  OE:  'oe', // ---.
-  OI:  'oi', // ---..
-  OS:  'os', // ---...
-  ETE:  'ete', // .-.
-  ETI:  'eti', // .-..
-  ETS:  'ets', // .-...
-  EME:  'eme', // .--.
-  EMI:  'emi', // .--..
-  EMS:  'ems', // .--...
-  EOE:  'eoe', // .---.
-  EOI:  'eoi', // .---..
-  EOS:  'eos', // .---...
-  ITE:  'ite', // ..-.
-  ITI:  'iti', // ..-..
-  ITS:  'its', // ..-...
-  IME:  'ime', // ..--.
-  IMI:  'imi', // ..--..
-  IMS:  'ims', // ..--...
-  IOE:  'ioe', // ..---.
-  IOI:  'ioi', // ..---..
-  IOS:  'ios', // ..---...
-  STE:  'ste', // ...-.
-  STI:  'sti', // ...-..
-  STS:  'sts', // ...-...
-  SME:  'sme', // ...--.
-  SMI:  'smi', // ...--..
-  SMS:  'sms', // ...--...
-  SOE:  'soe', // ...---.
-  SOI:  'soi', // ...---..
-  SOS:  'sos', // ...---...
-};
-/* eslint-enable no-multi-spaces, key-spacing */
+import { SageDictionary } from '../../dictionary';
+
+export const TOKENS_GRID_TEMPLATES = {};
+Object.keys(SageDictionary.CONTENT_GRID_TEMPLATE).forEach((template) => {
+  TOKENS_GRID_TEMPLATES[template] = SageDictionary.CONTENT_GRID_TEMPLATE.SYMBOL;
+});
