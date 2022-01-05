836b38128d2e3348e75acab3f1f295c90ef484b4
Phil Schanely <philschanely@gmail.com>
2022-01-05 11:52:54 -0500

    chore: patch for kajabi-products testing

--- packages/sage-react/lib/configs/classnames/grid-templates.js
+++ packages/sage-react/lib/configs/classnames/grid-templates.js
@@ -3,13 +3,13 @@ import { SageDictionary } from '../dictionary';
 // CLASSNAMES_GRID_TEMPLATES.ETE --> `sage-grid-template-ete`
 export const CLASSNAMES_GRID_TEMPLATES = {};
 Object.keys(SageDictionary.CONTENT_GRID_TEMPLATE).forEach((template) => {
-  CLASSNAMES_GRID_TEMPLATES[template] = SageDictionary.CONTENT_GRID_TEMPLATE.CLASSNAME;
+  CLASSNAMES_GRID_TEMPLATES[template] = SageDictionary.CONTENT_GRID_TEMPLATE[template].CLASSNAME;
 });
 
 export const lookupGridTemplateClassname = (gridTemplate) => {
   let matchingClassname = null;
   Object.keys(SageDictionary.CONTENT_GRID_TEMPLATE).forEach((gridTemplateToken) => {
-    if (SageDictionary.CONTENT_GRID_TEMPLATE[gridTemplateToken].symbol === gridTemplate) {
+    if (SageDictionary.CONTENT_GRID_TEMPLATE[gridTemplateToken].SYMBOL === gridTemplate) {
       matchingClassname = CLASSNAMES_GRID_TEMPLATES[gridTemplateToken];
     }
   });
