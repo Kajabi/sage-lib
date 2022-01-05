2cb44f95b58c65a1af83cbe0f8565117d8f6c942
Quinton Jason <quinton.jason@gmail.com>
2022-01-05 16:42:43 -0600

    fix(creation-wizard): rails cleanup

--- docs/lib/sage_rails/app/sage_components/sage_creation_wizard.rb
+++ docs/lib/sage_rails/app/sage_components/sage_creation_wizard.rb
@@ -1,9 +1,15 @@
 class SageCreationWizard < SageComponent
   set_attribute_schema({
-    indicator_current_item: [:optional, Integer],
-    indicator_label: [:optional, String],
-    indicator_num_items: [:optional, Integer],
+    active: [:optional, NilClass, TrueClass],
+    header_image: [:optional, String],
+    id: [:optional, NilClass, String],
+    indicator: [:optional, {
+      current_item: [:optional, Integer],
+      label: [:optional, String],
+      num_items: [:optional, Integer],
+    }],
     show_close: [:optional, TrueClass],
+    title: [:optional, String],
   })
   def sections
     %w(creation_wizard_body creation_wizard_body_actions creation_wizard_header creation_wizard_header_indicator creation_wizard_feature)
