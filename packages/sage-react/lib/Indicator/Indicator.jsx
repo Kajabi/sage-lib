311dc13fd0eeba2cb76290c517879c34f22efb94
Quinton Jason <quinton.jason@gmail.com>
2022-01-06 10:22:57 -0600

    fix(indicator): add custom property to react indicator

--- packages/sage-react/lib/Indicator/Indicator.jsx
+++ packages/sage-react/lib/Indicator/Indicator.jsx
@@ -34,10 +34,14 @@ export const Indicator = ({
     );
   }
 
+  const renderIndicatorProgress = () => {
+    return currentItem / numItems * 100
+  }
+
   return (
     <>
       {showText ? (
-        <p className="sage-indicator-text">{label} {currentItem} {preposition} {numItems}</p>
+        <p className="sage-indicator-text" style={{"--progress": `${renderIndicatorProgress()}%`}}>{label} {currentItem} {preposition} {numItems}</p>
       ) : (
         <ul
           className={`sage-indicator-list ${className}`}
