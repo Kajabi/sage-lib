dec3b6c79657d1a37ecd94fa43043d24c1c16cac
Shane Smith <voodoogq@gmail.com>
2022-01-06 08:49:51 -0600

    fix: converting Hint

---
+++ packages/sage-react/lib/Hint/Hint.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import classnames from 'classnames';
+import { Icon } from '../Icon';
+import { TokensIcons } from '../configs/tokens/icons'
+
+interface Props {
+  className?: string,
+  content: string,
+  icon?: TokensIcons | null,
+}
+
+export const Hint = ({
+  className = '',
+  content,
+  icon = null,
+  ...rest
+}: Props): JSX.Element => {
+  const classNames = classnames(
+    'sage-hint',
+    className,
+  );
+  return (
+    <div className={classNames} {...rest}>
+      {icon && (
+        <Icon
+          className="sage-hint__icon"
+          icon={icon}
+          adjacentType={undefined}
+          backgroundHeight={undefined}
+          backgroundWidth={undefined}
+          cardColor={undefined}
+          circular={undefined}
+          color={undefined}
+          label={undefined}
+          size={undefined}
+        />
+      )}
+      <span className="sage-hint__content">{content}</span>
+    </div>
+  );
+};
