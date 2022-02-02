894d6eddabcb3541f66273361e217ef2b6fcfde2
Quinton Jason <quinton.jason@gmail.com>
2022-02-02 09:53:22 -0600

    fix(storybook): initial commit

--- packages/sage-react/.storybook/main.js
+++ packages/sage-react/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '../lib/**/*.story.@(js|jsx|ts|tsx)'
   ],
   'addons': [
+    '@storybook/addon-a11y',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-notes',
