69117569bedf131cf519d82900a34d74c5b55892
Quinton Jason <quinton.jason@gmail.com>
2022-02-08 19:56:11 -0600

    fix(button): remove raised prop from rails

--- docs/lib/sage_rails/app/sage_components/sage_button.rb
+++ docs/lib/sage_rails/app/sage_components/sage_button.rb
@@ -1,3 +1,15 @@
 class SageButton < SageComponent
-  set_attribute_schema(SageSchemas::BUTTON)
+  set_attribute_schema({
+    align: [:optional, NilClass, String],
+    attributes: [:optional, NilClass, Hash],
+    custom_content_class: [:optional, NilClass, String],
+    disabled: [:optional, NilClass, TrueClass],
+    full_width: [:optional, NilClass, TrueClass],
+    icon: [:optional, { name: String, style: Set.new(["left", "right", "only"]) }],
+    small: [:optional, NilClass, TrueClass],
+    spinner_on_submit: [:optional, NilClass, String],
+    style: [:optional, NilClass, Set.new(["primary", "secondary", "danger"])],
+    subtle: [:optional, NilClass, TrueClass],
+    value: [:optional, String],
+  })
 end
