b22a647202b68cf1f38d0d25e628bfd693811696
Ashley E <ashleyne92@gmail.com>
2022-02-14 15:00:33 -0600

    feat(form select): adds new label prop to map to label markup

--- docs/lib/sage_rails/app/sage_components/sage_form_select.rb
+++ docs/lib/sage_rails/app/sage_components/sage_form_select.rb
@@ -2,6 +2,7 @@ class SageFormSelect < SageComponent
   set_attribute_schema({
     disabled: [:optional, TrueClass],
     has_error: [:optional, TrueClass],
+    label: [:optional, String],
     message: [:optional, String],
     multiple: [:optional, TrueClass],
     name: [:optional, String],
