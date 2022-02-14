b391a691c5a4b801c4866d05eb281fc2e0af0fb6
Ashley E <ashleyne92@gmail.com>
2022-02-14 15:31:39 -0600

    feat(form select): adds id prop for name and is attributes

--- docs/lib/sage_rails/app/sage_components/sage_form_select.rb
+++ docs/lib/sage_rails/app/sage_components/sage_form_select.rb
@@ -2,6 +2,7 @@ class SageFormSelect < SageComponent
   set_attribute_schema({
     disabled: [:optional, TrueClass],
     has_error: [:optional, TrueClass],
+    id: [:optional, String],
     label: [:optional, String],
     message: [:optional, String],
     multiple: [:optional, TrueClass],
