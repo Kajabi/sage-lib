4471367de498d44ab25976659212ea7123aeb33c
Phil Schanely <philschanely@gmail.com>
2022-02-15 10:58:13 -0500

    chore(button): move button schema to central library

--- docs/lib/sage_rails/app/sage_components/sage_schemas.rb
+++ docs/lib/sage_rails/app/sage_components/sage_schemas.rb
@@ -37,6 +37,21 @@ module SageSchemas
     size: [:optional, String],
   }
 
+  BUTTON = {
+    align: [:optional, NilClass, String],
+    attributes: [:optional, NilClass, Hash],
+    custom_content_class: [:optional, NilClass, String],
+    disabled: [:optional, NilClass, TrueClass],
+    full_width: [:optional, NilClass, TrueClass],
+    icon: [:optional, NilClass, { name: String, style: Set.new(["left", "right", "only"]) }],
+    raised: [:optional, NilClass, TrueClass],
+    small: [:optional, NilClass, TrueClass],
+    spinner_on_submit: [:optional, NilClass, String],
+    style: [:optional, NilClass, Set.new(["primary", "secondary", "danger"])],
+    subtle: [:optional, NilClass, TrueClass],
+    value: [:optional, String],
+  }
+
   CHOICE = {
     active: [:optional, NilClass, TrueClass],
     align_center: [:optional, TrueClass],
