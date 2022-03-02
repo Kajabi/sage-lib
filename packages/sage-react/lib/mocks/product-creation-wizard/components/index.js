0319e5513ba4dfde7c5120c1f9b89203ac25cdc2
Monica Wheeler <monica.wheeler@kajabi.com>
2022-03-02 08:18:51 -0600

    Revert "Add product creation wizard course mocks"

--- packages/sage-react/lib/mocks/product-creation-wizard/components/index.js
+++
@@ -1,6 +0,0 @@
-export { PaidPricingOptions } from './PaidPricingOptions';
-export { PricingOptionsDropdown } from './PricingOptionsDropdown';
-export { ProductChoice } from './ProductChoice';
-export { ProductChoiceDetails } from './ProductChoiceDetails';
-export { ProductColorPickerCard } from './ProductColorPickerCard';
-export { Root } from './Root';
