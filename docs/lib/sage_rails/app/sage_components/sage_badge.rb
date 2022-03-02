354d583604ea3f36ebcae833715834fe7a0c855f
Quinton Jason <quinton.jason@gmail.com>
2022-03-02 13:31:46 -0600

    fix(badge): eod push

--- docs/lib/sage_rails/app/sage_components/sage_badge.rb
+++ docs/lib/sage_rails/app/sage_components/sage_badge.rb
@@ -1,4 +1,13 @@
 class SageBadge < SageComponent
   set_attribute_schema({
+    container_attributes: [:optional, Hash],
+    color: [:optional, SageSchemas::STATUSES],
+    interactive_type: [:optional, Set.new([:default, :dropdown])],
+    style: [:optional, Set.new(["subtle", "bold"])],
+    value: String,
   })
+
+  def sections
+    %w(:badge_dot)
+  end
 end
