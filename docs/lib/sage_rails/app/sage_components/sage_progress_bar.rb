a14bd9682dceff37a02efa0e579bc2837702e2b1
Ashley E <ashleyne92@gmail.com>
2022-03-04 16:12:57 -0600

    style: testing solution

--- docs/lib/sage_rails/app/sage_components/sage_progress_bar.rb
+++ docs/lib/sage_rails/app/sage_components/sage_progress_bar.rb
@@ -1,6 +1,7 @@
 class SageProgressBar < SageComponent
   set_attribute_schema({
     color: [:optional, String],
+    display_percent: [:optional, TrueClass],
     label: String,
     percent: 0..100,
   })
