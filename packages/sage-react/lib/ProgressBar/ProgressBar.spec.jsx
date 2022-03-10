586b509a7ac05d1383110505ba74d37692a43d88
Quinton Jason <quinton.jason@gmail.com>
2022-03-10 12:40:17 -0600

    fix(progress-bar): add spec

---
+++ packages/sage-react/lib/ProgressBar/ProgressBar.spec.jsx
@@ -0,0 +1,24 @@
+require('../test/testHelper');
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ProgressBar } from './ProgressBar';
+
+describe('Sage ProgressCard', () => {
+  let component,
+    defaultProps;
+
+  beforeEach(() => {
+    defaultProps = {};
+
+    component = shallow(
+      <ProgressBar {...defaultProps} />
+    );
+  });
+
+  describe('construction', () => {
+    it('renders the component', () => {
+      expect(component).toHaveLength(1);
+    });
+  });
+});
