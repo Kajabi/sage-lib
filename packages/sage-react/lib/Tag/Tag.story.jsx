6fb30f9fec0405ff2ba5c571d59d63f886d3dc69
Ashley E <ashleyne92@gmail.com>
2022-03-11 17:09:04 -0600

    feat(tag): adds React Tag component

---
+++ packages/sage-react/lib/Tag/Tag.story.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SageTokens } from '../configs';
+import { selectArgs } from '../story-support/helpers';
+import { Button } from '../Button';
+import { Tag } from './Tag';
+
+export default {
+  title: 'Sage/Tag',
+  component: Tag,
+  argTypes: {
+    // As needed, use this for elaboration of token properties
+    // such as shown below for icons
+    ...selectArgs({
+      // icon: SageTokens.ICONS,
+    }),
+  },
+  args: {
+    action: (
+      <Button
+        color={Button.COLORS.SECONDARY}
+        icon={SageTokens.ICONS.REMOVE}
+        iconOnly={true}
+        subtle={true}
+      >
+        Remove
+      </Button>
+    ),
+    label: 'Label'
+  }
+};
+const Template = (args) => <Tag {...args} />;
+
+// The default story; add more as needed by duplicating this line and adjusting as needed.
+export const Default = Template.bind({});
