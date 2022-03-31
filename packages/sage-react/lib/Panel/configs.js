b62f45617442d3d2ad6c923b5964cee9a9f339b2
Quinton Jason <quinton.jason@gmail.com>
2022-03-31 18:42:17 -0500

    fix(gap): linter

--- packages/sage-react/lib/Panel/configs.js
+++ packages/sage-react/lib/Panel/configs.js
@@ -11,8 +11,6 @@ export const PANEL_ROW_ALIGNMENT_OPTIONS = {
   TOP: 'start',
 };
 
-export const PANEL_LIST_GAP_OPTIONS = PANEL_ROW_GAP_OPTIONS;
-
 export const PANEL_TILE_OPTIONS_ARRAY = [2, 3, 4];
 
 export const PANEL_LIST_BLOCK_SPACING = {
