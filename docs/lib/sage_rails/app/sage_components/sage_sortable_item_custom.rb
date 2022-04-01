0beb0728978ae1402306305e73b1c6dfc967f350
Quinton Jason <quinton.jason@gmail.com>
2022-04-01 09:23:43 -0500

    fix(gap): added missed prop

--- docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
+++ docs/lib/sage_rails/app/sage_components/sage_sortable_item_custom.rb
@@ -2,7 +2,7 @@ class SageSortableItemCustom < SageComponent
   set_attribute_schema({
     card: [:optional, TrueClass],
     grid_template: [:optional, String],
-    gap: [:optional, String],
+    gap: [:optional, NilClass, SageSchemas::GRID_GAP_OPTION],
     id: [:optional, Integer, String],
     url_update: [:optional, String],
   })
