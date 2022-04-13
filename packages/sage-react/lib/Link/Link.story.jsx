5e85773989c25a14d7243fb31fd011d74f5e405c
Quinton Jason <quinton.jason@gmail.com>
2022-04-13 15:43:29 -0500

    fix(link): updates to react

--- packages/sage-react/lib/Link/Link.story.jsx
+++ packages/sage-react/lib/Link/Link.story.jsx
@@ -1,3 +1,4 @@
+import { add } from 'lodash';
 import React from 'react';
 import { Link } from './Link';
 
@@ -13,6 +14,30 @@ Primary.args = {
   href: '#',
 };
 
+export const Plain = Template.bind({});
+Plain.args = {
+  children: 'Plain link',
+  removeUnderline: true,
+  style: Link.COLORS.SECONDARY,
+  href: '#',
+};
+
+export const LinkIconLeft = Template.bind({});
+LinkIconLeft.args = {
+  children: 'Standard link',
+  href: '#',
+  icon: "add",
+  iconPosition: "left",
+};
+
+export const LinkIconRight = Template.bind({});
+LinkIconRight.args = {
+  children: 'Standard link',
+  href: '#',
+  icon: "add",
+  iconPosition: "right",
+};
+
 export const Launch = Template.bind({});
 Launch.args = {
   children: 'Link to launch a new window',
@@ -20,13 +45,6 @@ Launch.args = {
   rel: 'noopener',
   target: '_blank',
   href: '//example.com',
-};
-
-export const Subtext = Template.bind({});
-Subtext.args = {
-  children: 'Link in subtext contenxt',
-  className: Link.CLASSNAMES.SUBTEXT,
-  href: '#',
 };
 
 export const Tooltip = Template.bind({});
