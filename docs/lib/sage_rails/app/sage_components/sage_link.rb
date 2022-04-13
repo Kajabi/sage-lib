3801f3058c7e443a46e56007ec276b3766dc2d6e
Quinton Jason <quinton.jason@gmail.com>
2022-04-13 07:43:55 -0500

    fix(link): align to foundations spec

--- docs/lib/sage_rails/app/sage_components/sage_link.rb
+++ docs/lib/sage_rails/app/sage_components/sage_link.rb
@@ -2,9 +2,12 @@ class SageLink < SageComponent
   set_attribute_schema({
     external: [:optional, TrueClass],
     help_link: [:optional, TrueClass],
+    icon: [:optional, NilClass, { name: String, style: Set.new(["left", "right"]) }],
     label: [:optional, String],
     launch: [:optional, TrueClass],
+    remove_underline: [:optional, TrueClass],
     show_label: [:optional, TrueClass],
+    style: [:optional, NilClass, Set.new(["primary", "neutral", "secondary", "danger"])],
     truncate: [:optional, TrueClass],
     url: [:optional, String],
   })
