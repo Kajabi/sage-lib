7b0b5331eb7f3f95871752c122b44efa7e0bfe49
Kevin Chang <kevin@kajabi.com>
2022-04-20 16:18:45 -0700

    feat(search): add ability to hide label

--- docs/lib/sage_rails/app/sage_components/sage_search.rb
+++ docs/lib/sage_rails/app/sage_components/sage_search.rb
@@ -2,6 +2,7 @@ class SageSearch < SageComponent
   set_attribute_schema({
     contained: [:optional, TrueClass],
     disabled: [:optional, TrueClass],
+    hide_label: [:optional, TrueClass],
     id: [:optional, String],
     label_text: [:optional, String],
     placeholder: [:optional, String],
