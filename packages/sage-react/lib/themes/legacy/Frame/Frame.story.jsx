b34ed994bf43fa9d65e9b3d8cb678ca241af1217
Phil Schanely <philschanely@gmail.com>
2022-05-02 11:42:07 -0400

    feat(frame): add more examples to documentation

--- packages/sage-react/lib/themes/legacy/Frame/Frame.story.jsx
+++ packages/sage-react/lib/themes/legacy/Frame/Frame.story.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { selectArgs } from '../story-support/helpers';
 import { SageClassnames, SageTokens } from '../configs';
-import { Button, Label } from '..';
+import { Button, Label, Property } from '..';
 import { Frame } from './Frame';
 
 export default {
@@ -46,7 +46,116 @@ export default {
 const Template = (args) => <Frame {...args} />;
 export const Default = Template.bind({});
 
-export const SampleBox = () => (
+export const SimpleStack = () => (
+  <Frame gap={Frame.GAPS.XS}>
+    <Property icon={SageTokens.ICONS.USER}>Galinti Marcus</Property>
+    <Property icon={SageTokens.ICONS.MAIL}>galinti@example.com</Property>
+    <Property icon={SageTokens.ICONS.STAR}>Member since 2022</Property>
+  </Frame>
+);
+
+export const SimpleRow = () => (
+  <Frame direction={Frame.DIRECTIONS.HORIZONTAL}>
+    <Property icon={SageTokens.ICONS.USER}>Galinti Marcus</Property>
+    <Property icon={SageTokens.ICONS.MAIL}>galinti@example.com</Property>
+    <Property icon={SageTokens.ICONS.STAR}>Member since 2022</Property>
+  </Frame>
+);
+
+export const BorderedBox = () => (
+  <Frame
+    border={Frame.BORDERS.DEFAULT}
+    borderRadius={Frame.BORDER_RADII.MD}
+    padding={Frame.PADDINGS.MD}
+  >
+    <Property icon={SageTokens.ICONS.USER}>Galinti Marcus</Property>
+    <Property icon={SageTokens.ICONS.MAIL}>galinti@example.com</Property>
+    <Property icon={SageTokens.ICONS.STAR}>Member since 2022</Property>
+  </Frame>
+);
+
+export const NestingFrames = () => (
+  <Frame
+    border={Frame.BORDERS.DEFAULT}
+    borderRadius={Frame.BORDER_RADII.MD}
+    padding={Frame.PADDINGS.MD}
+    direction={Frame.DIRECTIONS.HORIZONTAL}
+    align={Frame.ALIGNMENTS.CENTER_LEFT}
+  >
+    <Frame width="160px">
+      <img src="//source.unsplash.com/240x160" alt="" style={{ maxWidth: '100%' }} />
+    </Frame>
+    <Frame width="flex" gap={Frame.GAPS.XS}>
+      <Frame gap={Frame.GAPS.NONE}>
+        <h4 className={SageClassnames.TYPE.HEADING_3}>
+          Lorem ipsum dolor sit
+        </h4>
+        <p className={`${SageClassnames.TYPE.BODY_SMALL} ${SageClassnames.TYPE_COLORS.CHARCOAL_200}`}>
+          Consectetur adipiscing elit
+        </p>
+      </Frame>
+      <p className={SageClassnames.TYPE.BODY}>
+        Aliquam rhoncus ipsum massa, a tempor arcu elementum sed.
+        Nullam tortor odio, ullamcorper vel elementum in, luctus et tortor.
+        Nullam tellus orci, euismod ultricies odio ut, volutpat fermentum diam.
+        Proin rhoncus sit amet felis nec iaculis.
+        Fusce aliquet augue eget semper ultrices.
+      </p>
+    </Frame>
+  </Frame>
+);
+
+export const FlexibleWidthRatios = () => (
+  <Frame direction={Frame.DIRECTIONS.HORIZONTAL}>
+    <Frame
+      widthRatio="2"
+      border={Frame.BORDERS.DEFAULT}
+      borderRadius={Frame.BORDER_RADII.MD}
+      padding={Frame.PADDINGS.MD}
+    >
+      <p>Ratio of &ldquo;2&rdquo;; This side is 2 / 3</p>
+    </Frame>
+    <Frame
+      widthRatio="1"
+      border={Frame.BORDERS.DEFAULT}
+      borderRadius={Frame.BORDER_RADII.MD}
+      padding={Frame.PADDINGS.MD}
+    >
+      <p>Ratio of &ldquo;1&rdquo;; This side is 1 / 3</p>
+    </Frame>
+  </Frame>
+);
+
+export const EquallySizedFlexingItems = () => (
+  <Frame direction={Frame.DIRECTIONS.HORIZONTAL}>
+    <Frame
+      width_ratio="1"
+      border={Frame.BORDERS.DEFAULT}
+      borderRadius={Frame.BORDER_RADII.MD}
+      padding={Frame.PADDINGS.MD}
+    >
+      <p>This element is 1 / 3, flexing with the other items.</p>
+    </Frame>
+    <Frame
+      width_ratio="1"
+      border={Frame.BORDERS.DEFAULT}
+      borderRadius={Frame.BORDER_RADII.MD}
+      padding={Frame.PADDINGS.MD}
+    >
+      <p>This element is 1 / 3, flexing with the other items.</p>
+    </Frame>
+    <Frame
+      width_ratio="1"
+      border={Frame.BORDERS.DEFAULT}
+      borderRadius={Frame.BORDER_RADII.MD}
+      padding={Frame.PADDINGS.MD}
+    >
+      <p>This element is 1 / 3, flexing with the other items.</p>
+    </Frame>
+  </Frame>
+);
+
+export const CustomBlock = () => (
   <Frame
     align={Frame.ALIGNMENTS.CENTER_SPREAD}
     background="#fae2d4"
@@ -84,7 +193,7 @@ export const SampleBox = () => (
   </Frame>
 );
 
-export const SampleStatBox = () => (
+export const StatBox = () => (
   <Frame
     align={Frame.ALIGNMENTS.START_SPREAD}
     border={Frame.BORDERS.DEFAULT}
