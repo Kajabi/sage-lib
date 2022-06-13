95c871835991dca88f852c8deed0cf1e4aada375
Ashley E <ashleyne92@gmail.com>
2022-06-13 13:20:10 -0500

    chore: fix missing common

--- docs/app/helpers/components_helper.rb
+++ docs/app/helpers/components_helper.rb
@@ -842,7 +842,7 @@ module ComponentsHelper
         scss: "done",
         rails: "done",
         react: "done",
-        responsive: "done"
+        responsive: "done",
         a11y: "todo",
         react_component_slug: "sage-topbar--default",
         figma_embed: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FsMbtLUHSt2vfKgKjyQ3052%2FSage-components%3Fnode-id%3D5026%253A22583",
