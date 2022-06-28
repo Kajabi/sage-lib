0469118444653e27ce37cda5e1d8d3ec01f84a4a
Phil Schanely <philschanely@gmail.com>
2022-06-28 12:40:48 -0400

    feat(label): ensure Nil value is allowed for shared props

--- docs/lib/sage_rails/app/sage_components/sage_label.rb
+++ docs/lib/sage_rails/app/sage_components/sage_label.rb
@@ -1,10 +1,10 @@
 class SageLabel < SageComponent
   set_attribute_schema({
-    container_attributes: [:optional, Hash],
-    color: [:optional, SageSchemas::STATUSES],
+    container_attributes: [:optional, NilClass, Hash],
+    color: [:optional, NilClass, SageSchemas::STATUSES],
     custom_icon_color: [:optional, String],
     icon: [:optional, String],
-    interactive_type: [:optional, Set.new([:dropdown, :default, :secondary_button])],
+    interactive_type: [:optional, NilClass, Set.new([:dropdown, :default, :secondary_button])],
     secondary_button: [:optional, String, TrueClass, {
       attributes:       [:optional, NilClass, Hash],
       icon:             [:optional, String],
