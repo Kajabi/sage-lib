aa114fdcffa1ddfad388789a756d4468e52d330b
Quinton Jason <quinton.jason@gmail.com>
2022-07-11 09:10:58 -0500

    fix(video-thumbnail): docs update

--- docs/lib/sage_rails/app/sage_components/sage_video_thumbnail.rb
+++ docs/lib/sage_rails/app/sage_components/sage_video_thumbnail.rb
@@ -1,6 +1,6 @@
 class SageVideoThumbnail < SageComponent
   set_attribute_schema({
     cta_attributes: [:optional, Hash],
-    imageurl: [:optional, String],
+    image_url: [:optional, String],
   })
 end
