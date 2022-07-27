db09a600cd3ffc5cd5a6ae34eb582ffa3b3c310b
Monica Wheeler <monica@kajabi.com>
2022-07-27 16:17:54 -0500

    fix(listitem): add oneOfType

--- packages/sage-react/lib/List/ListItem.jsx
+++ packages/sage-react/lib/List/ListItem.jsx
@@ -49,7 +49,7 @@ ListItem.defaultProps = {
 ListItem.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  id: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   moreActions: { ...OptionsDropdown.propTypes },
   sortable: PropTypes.bool,
 };
