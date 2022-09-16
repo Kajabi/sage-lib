700e17dd7011e8c1230a247e3f2f2fa666643d49
Quinton Jason <quinton.jason@gmail.com>
2022-09-16 10:50:41 -0500

    fix(toast): updates to test

--- packages/sage-react/lib/Toast/Toast.spec.jsx
+++ packages/sage-react/lib/Toast/Toast.spec.jsx
@@ -1,7 +1,8 @@
 require('../test/testHelper');
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Toast } from './Toast';
 
 import { TOAST_TYPES } from './configs';
@@ -9,19 +10,94 @@ import { TOAST_TYPES } from './configs';
 describe('Sage Toast', () => {
   it('displays the Toast when isActive', () => {
     const props = {
-      isactive: true
+      isActive: true
     };
 
     render(<Toast {...props} />);
-    screen.findByText('sage-loader__spinner');
+    const wrapper = document.querySelector('.sage-toast');
+    expect(wrapper).not.toBeNull();
   });
 
-  it('displays the spinner while loading', () => {
+  it('displays the danger toast', () => {
     const props = {
-      loading: TOAST_TYPES.LOADING
+      isActive: true,
+      type: TOAST_TYPES.DANGER
+    };
+    render(<Toast {...props} />);
+    const wrapper = document.querySelector('.sage-toast');
+    expect(wrapper).toHaveClass('sage-toast--style-danger');
+  });
+
+  // it('displays the non dismissable toast', () => {
+  //   const props = {
+  //     isActive: true,
+  //     isDismissable: true,
+  //     timeout: 10000,
+  //     title: 'non dismissable toast title'
+  //   };
+
+  //   render(<Toast {...props} />);
+  //   let wrapper = document.querySelector('.sage-toast');
+  //   expect(wrapper).not.toBeNull();
+
+  //   waitForElementToBeRemoved(
+  //     screen.queryByText(props.title)
+  //   ).then(() =>
+  //     expect(wrapper).toBeNull()
+  //   );
+  // });
+
+  it('displays a toast win an internal link', () => {
+    const props = {
+      isActive: true,
+      link: {
+        href: '#',
+        text: 'link text'
+      }
     };
 
     render(<Toast {...props} />);
-    screen.findByText('sage-toast');
+    const wrapper = document.querySelector('.sage-toast');
+    expect(wrapper).not.toBeNull();
+
+    const link = document.querySelector('.sage-toast__button');
+    expect(link).not.toBeNull();
+  });
+
+  it('dismisses the toast when dismissed', async () => {
+    const props = {
+      isActive: true,
+      isDismissable: false
+    };
+
+    const user = userEvent.setup();
+    render(<Toast {...props} />);
+    let wrapper = document.querySelector('.sage-toast');
+    expect(wrapper).not.toBeNull();
+
+    await user.click(screen.getByRole('button'));
+    wrapper = document.querySelector('.sage-toast');
+    expect(wrapper).toBeNull();
+  });
+
+  it('dismisses the toast on timeout', () => {
+    let callbackFired = false;
+    const props = {
+      isActive: true,
+      onDismiss: () => {
+        callbackFired = true;
+      },
+      title: 'toast that has a timeout'
+    };
+
+    render(<Toast {...props} />);
+    let wrapper = document.querySelector('.sage-toast');
+    expect(wrapper).not.toBeNull();
+
+    waitForElementToBeRemoved(
+      screen.queryByText(props.title)
+    ).then(() =>
+      expect(callbackFired).toBeT()
+    );
   });
 });
