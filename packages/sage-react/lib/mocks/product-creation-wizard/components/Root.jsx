87180d5add7ded4a5835b7661d740963cdfca45c
Quinton Jason <quinton.jason@gmail.com>
2022-09-28 15:24:29 -0500

    fix(docs): align creation framework to existing build team creation wizard
    Everything that follows this commit is the attempt to algn CPF with Creation Framework

--- packages/sage-react/lib/mocks/product-creation-wizard/components/Root.jsx
+++ packages/sage-react/lib/mocks/product-creation-wizard/components/Root.jsx
@@ -80,7 +80,7 @@ export const Root = () => {
       <Button color={Button.COLORS.PRIMARY} onClick={() => setModalActive(true)}>
         Add product
       </Button>
-      <Modal active={modalActive} fullScreen={true}>
+      <Modal active={modalActive} fullScreen={true} className="creation-wizard__modal u-sage-modal--fixed">
         {/*
           TODO: Build dismiss into modal header
           https://kajabi.atlassian.net/browse/SAGE-312
@@ -110,11 +110,14 @@ export const Root = () => {
             with a footer at bottom and scroll in middle
             https://kajabi.atlassian.net/browse/SAGE-329
           */}
-          <Grid.Row>
-            <Grid.Col size={4} small={12} medium={5} large={4}>
+          <Grid.Row className="creation-wizard__container">
+            {/* <Grid.Col size={4} small={12} medium={5} large={4}> */}
+            <Grid.Col className="creation-wizard__sidebar">
               {renderStep()}
             </Grid.Col>
-            <Grid.Col size={8} small={Grid.GRID_BREAKPOINT_TOGGLES.HIDE} medium={7} large={8}>
+            {/* <Grid.Col size={8} small={Grid.GRID_BREAKPOINT_TOGGLES.HIDE}
+            medium={7} large={8}> */}
+            <Grid.Col className="creation-wizard__preview">
               {/* TODO: Dev to add actual graphic SVG here  with live edit synced */}
               <img
                 src="//source.unsplash.com/random/832x575"
