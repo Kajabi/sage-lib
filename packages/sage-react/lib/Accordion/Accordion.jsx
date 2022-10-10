1b0146a2bce514c3b0f0e45f64c3afd00af9f3d5
Quinton Jason <quinton.jason@gmail.com>
2022-10-10 11:08:15 -0500

    fix(accordion): save point adding accordion logic

--- packages/sage-react/lib/Accordion/Accordion.jsx
+++ packages/sage-react/lib/Accordion/Accordion.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { ExpandableCard } from '../ExpandableCard';
 
 export const Accordion = ({
   className,
   children,
+  id,
   onlyOnePanelExpanded,
   ...rest
 }) => {
@@ -16,9 +18,20 @@ export const Accordion = ({
     }
   );
 
+  const onChildClick = (args) => {
+    // i now have the name and index
+    // identify which child was expanded based on args
+    // args = name + index
+    // idx for example will be ExpandableCard-1
+    // iterate and be sure the others setexpanded is set to false
+  };
+
   return (
     <div className={classNames} {...rest}>
-      {children}
+      {children.props.children.map((child, idx) => {
+        console.log ('Type: ', child.type, ' Props: ', child.props);
+        return <child.type name={`${child.type.name}-${idx}`} {...child.props} onClick={onChildClick} />;
+      })}
     </div>
   );
 };
@@ -26,11 +39,13 @@ export const Accordion = ({
 Accordion.defaultProps = {
   children: null,
   className: '',
+  id: '',
   onlyOnePanelExpanded: false,
 };
 
 Accordion.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  id: PropTypes.string,
   onlyOnePanelExpanded: PropTypes.bool,
 };
