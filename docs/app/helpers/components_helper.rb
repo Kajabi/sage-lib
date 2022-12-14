6573847415c830412134a3418ba3845796de82b1
Phillip Lovelace <phillip@pixelflips.com>
2022-12-14 13:39:24 -0800

    docs: correct icon lnk path

--- docs/app/helpers/components_helper.rb
+++ docs/app/helpers/components_helper.rb
@@ -407,7 +407,7 @@ module ComponentsHelper
       },
       {
         title: "icon",
-        description: "The Icon component displays a standalone icon with customizable options. Our library of icons is available under the #{link_to("Foundation's Icons page", pages_design_path(:icon))}.".html_safe,
+        description: "The Icon component displays a standalone icon with customizable options. Our library of icons is available under the #{link_to("Foundation's Icons page", pages_foundation_path(:icon))}.".html_safe,
         scss: "todo",
         rails: "todo",
         react: "todo",
