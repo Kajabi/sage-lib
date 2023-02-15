ebe1bd6ccc4e0dbfef970465291bf58809f11dbb
Andrew McIntee <contact@andrew.mn>
2023-02-15 14:39:06 -0500

    Update Tooltip.jsx

--- packages/sage-react/lib/Tooltip/Tooltip.jsx
+++ packages/sage-react/lib/Tooltip/Tooltip.jsx
@@ -31,6 +31,7 @@ export const Tooltip = ({
         onFocus: handleActivate,
         onMouseLeave: handleDeactivate,
         onBlur: handleDeactivate,
+        ...rest,
       }))}
       {active && ReactDOM.createPortal(
         <TooltipElement parentDomRect={parentDomRect} {...rest} />,
