e28ee523742566e0dd1e869b01fc0e6d5d928a31
Phillip Lovelace <phillip@pixelflips.com>
2023-03-13 15:44:00 -0700

    docs(hero): add react slug to docs

--- docs/app/helpers/components_helper.rb
+++ docs/app/helpers/components_helper.rb
@@ -393,6 +393,7 @@ module ComponentsHelper
         react: "todo",
         responsive: "done",
         a11y: "done",
+        react_component_slug: "sage-hero--default",
         figma_embed: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FsMbtLUHSt2vfKgKjyQ3052%2F%25F0%259F%25A7%25A9-Sage-components%3Fnode-id%3D3686%253A21923",
       },
       {
