ed2daead1e7014fae019d43093ffb06a07870727
Phillip Lovelace <phillip@pixelflips.com>
2023-03-27 14:43:14 -0700

    docs(list): add examples of additional action items storybook

--- packages/sage-react/lib/List/List.story.jsx
+++ packages/sage-react/lib/List/List.story.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { selectArgs } from '../story-support/helpers';
+import { SageTokens, SageClassnames } from '../configs';
+import { Button } from '../Button';
+import { Badge } from '../Badge';
+import { Link } from '../Link';
+import { Card } from '../Card';
+import { Property } from '../Property';
 import { List } from './List';
+import { ListItem } from './ListItem';
 import {
   sampleItems,
   sampleItemRenderer,
@@ -57,5 +64,48 @@ export const FullyDraggableSortableList = () => {
       setList={setItems}
       sortable={true}
     />
+  );
+};
+
+export const otherActionItems = () => {
+  return (
+    <List>
+      {[...Array(5)].map(() => (
+        <ListItem>
+          <Card.Row gridTemplate={SageTokens.GRID_TEMPLATES.ETE}>
+            <img
+              src="https://source.unsplash.com/random/240x160"
+              width="120"
+              height="64"
+              alt=""
+            />
+            <Card.Stack>
+              <Link
+                className={`${SageClassnames.TYPE.HEADING_4}`}
+                href="#"
+                removeUnderline
+                style="secondary"
+              >
+                Plain link
+              </Link>
+              <Property.Group>
+                <Property icon="users-alt">Property</Property>
+                <Property icon="users-alt">Property</Property>
+                <Property icon="users-alt">Property</Property>
+              </Property.Group>
+            </Card.Stack>
+            <Button.Group align="end" gap="md">
+              <Badge isInteractive={false} value="Label" color="published" />
+              <Button icon="preview-on" iconOnly onClick={() => {}} subtle>
+                Preview
+              </Button>
+              <Button icon="pen" iconOnly onClick={() => {}} subtle>
+                Edit
+              </Button>
+            </Button.Group>
+          </Card.Row>
+        </ListItem>
+      ))}
+    </List>
   );
 };
