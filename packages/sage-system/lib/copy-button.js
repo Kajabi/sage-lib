05c560d7af4785f41b0d6b089db0d5c23c67f4df
Julian Skinner <dev+github-bot@kajabi.com>
2023-04-26 14:08:03 -0500

    fix(copy-button): use new clipboard functions
    we still have a fallback to use execCommand for legacy browser support
    
    fix: DSS-388

--- packages/sage-system/lib/copy-button.js
+++ packages/sage-system/lib/copy-button.js
@@ -5,19 +5,59 @@ Sage.copyButton = (function() {
   // ==================================================
 
   function handleClick(ev) {
-    const str = ev.currentTarget.dataset.jsCopyButton;
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.setAttribute('class', 'visually-hidden');
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    ev.currentTarget.focus();
-    Sage.toast.trigger({text:'Copied to clipboard'})
-  };
+    const strToCopy = ev.currentTarget.dataset.jsCopyButton;
 
+
+    if (navigator.clipboard) {
+      navigator.permissions.query({name: 'clipboard-write'}).then(result => {
+        if (result.state == 'granted' || result.state == 'prompt') {
+          navigator.clipboard.writeText(strToCopy)
+            .then(() => {
+              Sage.toast.trigger({text:'Copied to clipboard'})
+            })
+            .catch(err => {
+              Sage.toast.trigger({text:'Does not have access to clipboard'})
+            });
+        } else {
+          // Permission not granted, ask the user to grant it
+          navigator.permissions.request({name: 'clipboard-write'}).then(result => {
+            if (result.state == 'granted') {
+              navigator.clipboard.writeText(strToCopy)
+                .then(() => {
+                  Sage.toast.trigger({text:'Copied to clipboard'})
+                })
+                .catch(err => {
+                  const errMsg = 'Error writing text to clipboard: ' + err;
+                  Sage.toast.trigger({text: errMsg})
+                });
+              } else {
+                Sage.toast.trigger({text: 'Permission to write to clipboard denied'});
+              }
+            }).catch(err => {
+              const errMsg = 'Error requesting permission to write to clipboard: ' + err;
+              Sage.toast.trigger({text: errMsg})
+            });
+          }
+        }).catch(err => {
+          const errMsg = 'Error querying permission to write to clipboard: ' + err;
+          Sage.toast.trigger({text: errMsg})
+      });
+    } else {
+      // Fallback for Safari
+      const el = document.createElement('textarea');
+      el.value = strToCopy;
+      el.setAttribute('readonly', '');
+      el.setAttribute('class', 'visually-hidden');
+      document.body.appendChild(el);
+      el.select();
+
+      document.execCommand("copy");
+      document.body.removeChild(el);
+      Sage.toast.trigger({text:'Copied to clipboard'})
+    }
+
+    ev.currentTarget.focus();
+  };
 
   // ==================================================
   // Functions
