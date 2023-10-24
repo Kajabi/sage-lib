a9b68bb6681ae7bb6f0518639d4bacf73ba4541b
Ashley E <ashleyne92@gmail.com>
2023-10-24 16:24:46 -0500

    docs(panel): updates Panel docs for React

--- packages/sage-react/lib/Panel/Panel.jsx
+++ packages/sage-react/lib/Panel/Panel.jsx
@@ -72,10 +72,16 @@ Panel.defaultProps = {
 };
 
 Panel.propTypes = {
+  /** Renders any content passed within. */
   children: PropTypes.node,
+  /** CSS class that will be applied to the outermost container. */
   className: PropTypes.string,
+  /** Removes padding on both top and bottom of the Panel */
   clearPaddingBoth: PropTypes.bool,
+  /** Removes padding on the bottom of the Panel */
   clearPaddingBottom: PropTypes.bool,
+  /** Removes padding on the top of the Panel */
   clearPaddingTop: PropTypes.bool,
+  /** Setting this to true will display a loading spinner within the Panel */
   loading: PropTypes.bool,
 };
