28657a4c80065f5f1be6516712662a91cb636a2b
Phillip Lovelace <phillip@pixelflips.com>
2024-01-04 15:23:07 -0800

    fix(tooltip): remove size and theme props

--- packages/sage-react/lib/Tooltip/configs.js
+++ packages/sage-react/lib/Tooltip/configs.js
@@ -1,18 +1,7 @@
-export const TOOLTIP_SIZES = {
-  DEFAULT: null,
-  SMALL: 'small',
-  LARGE: 'large',
-};
-
 export const TOOLTIP_POSITIONS = {
   DEFAULT: 'top',
   TOP: 'top',
   RIGHT: 'right',
   BOTTOM: 'bottom',
   LEFT: 'left',
-};
-
-export const TOOLTIP_THEMES = {
-  DEFAULT: null,
-  LIGHT: 'light',
 };
