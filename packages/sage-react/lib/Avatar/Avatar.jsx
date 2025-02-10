8e242f4175a979eeee38d0d9d15675f44d86a028
Ashley Echols <ashley.echols@kajabi.com>
2025-02-10 15:18:12 -0600

    chore: swaps out React stencil component for web component version

--- packages/sage-react/lib/Avatar/Avatar.jsx
+++ packages/sage-react/lib/Avatar/Avatar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { PdsIcon } from '@pine-ds/react';
 import { SageTokens } from '../configs';
 import { Icon } from '../Icon';
 import { AVATAR_COLORS } from './configs';
@@ -83,7 +82,7 @@ export const Avatar = ({
         <img alt={image.alt || ''} className="sage-avatar__image" src={image.src} id={image.id} />
       )}
       {(!image.src || useFallbackGraphic) && (
-        <PdsIcon name="user-filled" color="var(--pine-color-mercury-500)" className="sage-avatar__graphic" />
+        <pds-icon name="user-filled" color="var(--pine-color-mercury-500)" className="sage-avatar__graphic" />
       )}
     </div>
   );
